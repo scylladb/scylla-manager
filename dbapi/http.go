101d2fa3436663bc1dad5962b6f473d073fe94cd
Michał Matczuk <michal@scylladb.com>
2017-09-08 09:55:06 +0200

    dbapi: clone request

--- dbapi/http.go
+++ dbapi/http.go
@@ -2,6 +2,7 @@ package dbapi
 
 import (
 	"net/http"
+	"net/url"
 
 	"github.com/hailocab/go-hostpool"
 	"github.com/scylladb/mermaid/log"
@@ -15,8 +16,8 @@ type transport struct {
 	logger log.Logger
 }
 
-func (t transport) RoundTrip(r *http.Request) (*http.Response, error) {
-	ctx := r.Context()
+func (t transport) RoundTrip(req *http.Request) (*http.Response, error) {
+	ctx := req.Context()
 
 	var (
 		h   string
@@ -32,11 +33,14 @@ func (t transport) RoundTrip(r *http.Request) (*http.Response, error) {
 		h = hpr.Host()
 	}
 
+	// clone request
+	r := cloneRequest(req)
+
+	// set host
 	r.Host = h
 	r.URL.Host = h
 
 	t.logger.Debug(ctx, "Request", "URL", r.URL)
-
 	resp, err := t.parent.RoundTrip(r)
 	if resp != nil {
 		t.logger.Debug(ctx, "Response",
@@ -54,4 +58,33 @@ func (t transport) RoundTrip(r *http.Request) (*http.Response, error) {
 	}
 
 	return resp, err
+}
+
+// cloneRequest creates a shallow copy of the request along with a deep copy
+// of the Headers and URL.
+func cloneRequest(req *http.Request) *http.Request {
+	r := new(http.Request)
+
+	// shallow clone
+	*r = *req
+
+	// deep copy headers
+	r.Header = cloneHeader(req.Header)
+
+	// deep copy URL
+	r.URL = new(url.URL)
+	*r.URL = *req.URL
+
+	return r
+}
+
+// cloneHeader creates a deep copy of an http.Header.
+func cloneHeader(in http.Header) http.Header {
+	out := make(http.Header, len(in))
+	for key, values := range in {
+		newValues := make([]string, len(values))
+		copy(newValues, values)
+		out[key] = newValues
+	}
+	return out
 }
