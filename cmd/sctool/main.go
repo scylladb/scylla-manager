8ab29c91f431212a9225822f567e0d1c912b8c18
Michał Matczuk <michal@scylladb.com>
2017-11-10 16:09:00 +0100

    sctool: use cobra cli lib

--- cmd/sctool/main.go
+++ cmd/sctool/main.go
@@ -3,51 +3,17 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
-	"os/signal"
-	"syscall"
-
-	"github.com/mitchellh/cli"
-	"github.com/scylladb/mermaid/command/client"
 )
 
 func main() {
-	os.Exit(realMain())
-}
-
-func realMain() int {
 	log.SetOutput(ioutil.Discard)
 
-	// cancel on signal
-	ctx, cancel := context.WithCancel(context.Background())
-
-	signalCh := make(chan os.Signal, 1)
-	signal.Notify(signalCh, syscall.SIGINT, syscall.SIGTERM)
-	go func() {
-		_, ok := <-signalCh
-		if ok {
-			cancel()
-		}
-	}()
-	defer close(signalCh)
-
-	cli := cli.NewCLI("sctool", version)
-	cli.Args = os.Args[1:]
-	cli.Commands = client.Commands(ctx)
-
-	// disable auto complete installation
-	cli.AutocompleteInstall = "\n"
-	cli.AutocompleteUninstall = "\n"
-
-	exitCode, err := cli.Run()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error executing CLI: %s\n", err.Error())
-		return 1
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(1)
 	}
 
-	return exitCode
+	os.Exit(0)
 }
---
+++ cmd/sctool/table.go
@@ -0,0 +1,42 @@
+// Copyright (C) 2017 ScyllaDB
+
+package main
+
+import (
+	"bytes"
+	"fmt"
+
+	"github.com/mmatczuk/tablewriter"
+)
+
+type table struct {
+	table *tablewriter.Table
+	buf   *bytes.Buffer
+}
+
+func newTable(header ...string) table {
+	b := new(bytes.Buffer)
+	t := tablewriter.NewWriter(b)
+	t.SetHeader(header)
+	t.SetAutoFormatHeaders(false)
+	t.SetBorder(false)
+
+	return table{
+		table: t,
+		buf:   b,
+	}
+}
+
+func (t table) append(row ...interface{}) {
+	s := make([]string, len(row))
+	for i, e := range row {
+		s[i] = fmt.Sprint(e)
+	}
+	t.table.Append(s)
+}
+
+func (t table) String() string {
+	defer t.buf.Reset()
+	t.table.Render()
+	return t.buf.String()
+}
