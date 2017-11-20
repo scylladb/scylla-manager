b688ba6c10d2e0630228a747ab4a2bb849b8e110
Michał Matczuk <michal@scylladb.com>
2017-11-20 17:14:28 +0100

    cluster: CRUD service...

---
+++ cluster/integration_test.go
@@ -0,0 +1,190 @@
+// Copyright (C) 2017 ScyllaDB
+
+// +build all integration
+
+package cluster_test
+
+import (
+	"context"
+	"strconv"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/scylladb/mermaid"
+	"github.com/scylladb/mermaid/cluster"
+	"github.com/scylladb/mermaid/log"
+	"github.com/scylladb/mermaid/mermaidtest"
+	"github.com/scylladb/mermaid/uuid"
+)
+
+func TestServiceStorageIntegration(t *testing.T) {
+	session := mermaidtest.CreateSession(t)
+
+	s, err := cluster.NewService(
+		session,
+		log.NewDevelopment().Named("cluster"),
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ctx := context.Background()
+
+	cleanup := func(t *testing.T) {
+		q := session.Query("TRUNCATE cluster")
+		defer q.Release()
+		if err := q.Exec(); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	t.Run("list empty clusters", func(t *testing.T) {
+		cleanup(t)
+
+		clusters, err := s.ListClusters(ctx, &cluster.Filter{})
+		if err != nil {
+			t.Fatal(err)
+		}
+		if len(clusters) != 0 {
+			t.Fatal("expected 0 len result")
+		}
+	})
+
+	t.Run("list clusters", func(t *testing.T) {
+		cleanup(t)
+
+		expected := make([]*cluster.Cluster, 3)
+		for i := range expected {
+			u := &cluster.Cluster{
+				ID:   uuid.NewTime(),
+				Name: "name" + strconv.Itoa(i),
+			}
+			if err := s.PutCluster(ctx, u); err != nil {
+				t.Fatal(err)
+			}
+			expected[i] = u
+		}
+
+		clusters, err := s.ListClusters(ctx, &cluster.Filter{})
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if diff := cmp.Diff(clusters, expected, mermaidtest.UUIDComparer()); diff != "" {
+			t.Fatal(diff)
+		}
+	})
+
+	t.Run("get missing cluster", func(t *testing.T) {
+		cleanup(t)
+
+		c, err := s.GetClusterByID(ctx, uuid.MustRandom())
+		if err != mermaid.ErrNotFound {
+			t.Fatal("expected not found")
+		}
+		if c != nil {
+			t.Fatal("expected nil")
+		}
+	})
+
+	t.Run("get cluster", func(t *testing.T) {
+		cleanup(t)
+
+		c0 := validCluster()
+		c0.ID = uuid.Nil
+
+		if err := s.PutCluster(ctx, c0); err != nil {
+			t.Fatal(err)
+		}
+		if c0.ID == uuid.Nil {
+			t.Fatal("ID not updated")
+		}
+		c1, err := s.GetClusterByID(ctx, c0.ID)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if diff := cmp.Diff(c0, c1, mermaidtest.UUIDComparer()); diff != "" {
+			t.Fatal("read write mismatch", diff)
+		}
+
+		c2, err := s.GetClusterByName(ctx, c0.Name)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if diff := cmp.Diff(c0, c2, mermaidtest.UUIDComparer()); diff != "" {
+			t.Fatal("read write mismatch", diff)
+		}
+	})
+
+	t.Run("put nil cluster", func(t *testing.T) {
+		cleanup(t)
+
+		if err := s.PutCluster(ctx, nil); err == nil {
+			t.Fatal("expected validation error")
+		}
+	})
+
+	t.Run("put conflicting cluster", func(t *testing.T) {
+		cleanup(t)
+
+		c0 := validCluster()
+
+		if err := s.PutCluster(ctx, c0); err != nil {
+			t.Fatal(err)
+		}
+
+		c1 := c0
+		c1.ID = uuid.Nil
+
+		if err := s.PutCluster(ctx, c0); err == nil {
+			t.Fatal("expected validation error")
+		}
+	})
+
+	t.Run("put new cluster", func(t *testing.T) {
+		cleanup(t)
+
+		c := validCluster()
+		c.ID = uuid.Nil
+
+		if err := s.PutCluster(ctx, c); err != nil {
+			t.Fatal(err)
+		}
+		if c.ID == uuid.Nil {
+			t.Fatal("id not set")
+		}
+	})
+
+	t.Run("delete missing cluster", func(t *testing.T) {
+		cleanup(t)
+
+		err := s.DeleteCluster(ctx, uuid.MustRandom())
+		if err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	t.Run("delete cluster", func(t *testing.T) {
+		cleanup(t)
+
+		c := validCluster()
+
+		if err := s.PutCluster(ctx, c); err != nil {
+			t.Fatal(err)
+		}
+		if err := s.DeleteCluster(ctx, c.ID); err != nil {
+			t.Fatal(err)
+		}
+		_, err := s.GetClusterByID(ctx, c.ID)
+		if err != mermaid.ErrNotFound {
+			t.Fatal(err)
+		}
+	})
+}
+
+func validCluster() *cluster.Cluster {
+	return &cluster.Cluster{
+		ID:   uuid.MustRandom(),
+		Name: "name_" + uuid.MustRandom().String(),
+	}
+}
--- cluster/model.go
+++ cluster/model.go
@@ -2,7 +2,10 @@
 
 package cluster
 
-import "github.com/scylladb/mermaid/uuid"
+import (
+	"github.com/pkg/errors"
+	"github.com/scylladb/mermaid/uuid"
+)
 
 // Cluster specifies a cluster properties.
 type Cluster struct {
@@ -10,4 +13,26 @@ type Cluster struct {
 	Name       string
 	Hosts      []string
 	ShardCount int64
+}
+
+// Validate checks if all the fields are properly set.
+func (c *Cluster) Validate() error {
+	if c == nil {
+		return errors.New("nil")
+	}
+	return nil
+}
+
+// Filter filters Clusters.
+type Filter struct {
+	Name string
+}
+
+// Validate checks if all the fields are properly set.
+func (f *Filter) Validate() error {
+	if f == nil {
+		return errors.New("nil")
+	}
+
+	return nil
 }
