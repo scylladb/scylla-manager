6094a181c5e1fefe53f7b67f085e6b646e865671
Michał Matczuk <michal@scylladb.com>
2017-12-11 14:46:29 +0100

    testing: ssh options param fix

--- testing/scylla_cluster/install_keys.sh
+++ testing/scylla_cluster/install_keys.sh
@@ -3,5 +3,5 @@
 set -e
 
 for name in `docker ps -f name=mermaid_dc* --format {{.Names}}`; do
-	sshpass -p "test" ssh-copy-id -f -oStrictHostKeyChecking=no scylla-mgmt@`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${name}` &>/dev/null;
+	sshpass -p "test" ssh-copy-id -f -o StrictHostKeyChecking=no scylla-mgmt@`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${name}` &>/dev/null;
 done
