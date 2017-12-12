9f27bdecf080a27b41cc5d8f092d6fbc4f303f3a
Michał Matczuk <michal@scylladb.com>
2017-12-12 14:04:07 +0100

    testing: use custom key and known_hosts file

--- testing/scylla_cluster/install_keys.sh
+++ testing/scylla_cluster/install_keys.sh
@@ -2,6 +2,19 @@
 
 set -e
 
+echo "==> Generating keys"
+[ -f "scylla_mgmt" ] || ssh-keygen -t rsa -b 2048 -N "" -f "scylla_mgmt"
+
+pem=`cat "scylla_mgmt.pub"`
+
+echo "==> Installing keys"
 for name in `docker ps -f name=mermaid_dc* --format {{.Names}}`; do
-	sshpass -p "test" ssh-copy-id -f -o StrictHostKeyChecking=no scylla-mgmt@`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${name}` &>/dev/null;
+	ip=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${name}`
+	echo " ${ip}"
+	sshpass -p "test" ssh-copy-id -f -o StrictHostKeyChecking=no -o UserKnownHostsFile=scylla_mgmt_known_hosts scylla-mgmt@${ip} &> /dev/null
 done
+
+echo "==> Linking files"
+ln -f scylla_mgmt scylla_mgmt.pub scylla_mgmt_known_hosts ~/.ssh/
+
+echo "try: ssh -i ~/.ssh/scylla_mgmt -o UserKnownHostsFile=~/.ssh/scylla_mgmt_known_hosts scylla-mgmt@172.16.1.10"
