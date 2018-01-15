7953af411511487d7bba89697279b934b7219c74
Yuval Pavel Zholkover <paulzhol@gmail.com>
2018-01-15 13:11:59 +0200

    dist,testing: use scylla_manager.pem as the default private SSH key name
    Drop the .ssh dir, it's not needed.
    
    Fixes #252, #254.

--- testing/scylla_cluster/install_keys.sh
+++ testing/scylla_cluster/install_keys.sh
@@ -3,13 +3,14 @@
 set -e
 
 SSH_OPTS="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
+KEYFILE=scylla_manager.pem
 
 echo "==> Generating keys"
-[ -f "id_rsa" ] || ssh-keygen -t rsa -b 2048 -N "" -f "id_rsa" > /dev/null && chmod 0400 id_rsa
+[ -f "${KEYFILE}" ] || ssh-keygen -t rsa -b 2048 -N "" -f "${KEYFILE}" > /dev/null && chmod 0400 "${KEYFILE}"
 
 echo "==> Installing keys"
 for name in `docker ps -f name=mermaid_dc* --format {{.Names}}`; do
 	ip=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${name}`
 	echo " ${ip}"
-	sshpass -p "test" ssh-copy-id -i id_rsa -f ${SSH_OPTS} scylla-manager@${ip} &> /dev/null
+	sshpass -p "test" ssh-copy-id -i "${KEYFILE}" -f ${SSH_OPTS} scylla-manager@${ip} &> /dev/null
 done
