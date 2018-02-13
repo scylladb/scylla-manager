e07f56e394fe194033c9c9d0016722f9ecf716c5
Michał Matczuk <michal@scylladb.com>
2018-02-13 11:46:47 +0100

    server/dist: stop on error

--- cmd/scylla-manager/config_test.go
+++ cmd/scylla-manager/config_test.go
@@ -44,6 +44,7 @@ func TestNewConfigFromFile(t *testing.T) {
 			SegmentSizeLimit:  10,
 			SegmentsPerRepair: 7,
 			SegmentErrorLimit: 0,
+			StopOnError:       true,
 			ErrorBackoff:      10 * time.Second,
 			PollInterval:      500 * time.Millisecond,
 			AutoScheduleDelay: 100 * time.Second,
