scylladb/scylla-manager

commit ccc1f216dc3d5f7b9dd198faf4ad9e4673f0bdb0

sched: preventing access to a closed service internal state

sched/service.go
@@ -56,6 +56,7 @@ type Service struct {
 	tasks    map[uuid.UUID]cancelableTrigger
 	wg       sync.WaitGroup
 	taskLock sync.Mutex
+	closed   bool
 }
 
 type cancelableTrigger struct {
@@ -89,6 +90,7 @@ func NewService(session *gocql.Session, cp cluster.ProviderFunc, l log.Logger) (
 		cronCtx: log.WithTraceID(context.Background()),
 		runners: make(map[TaskType]runner.Runner),
 		tasks:   make(map[uuid.UUID]cancelableTrigger),
+		closed:  false,
 	}, nil
 }
 
@@ -169,6 +171,12 @@ func (s *Service) schedTask(ctx context.Context, now time.Time, t *Task) {
 
 	doneCh := make(chan struct{})
 	s.taskLock.Lock()
+	defer s.taskLock.Unlock()
+
+	if s.closed {
+		s.logger.Debug(ctx, "Service closed, not re-scheduling", "task", t)
+		return
+	}
 	timer := time.AfterFunc(activation.Sub(now), func() { s.execTrigger(triggerCtx, t, doneCh) })
 	s.tasks[t.ID] = cancelableTrigger{
 		timer: timer,
@@ -180,13 +188,18 @@ func (s *Service) schedTask(ctx context.Context, now time.Time, t *Task) {
 		},
 		done: doneCh,
 	}
-	s.taskLock.Unlock()
 }
 
 func (s *Service) reschedTask(ctx context.Context, t *Task, run *Run, done chan struct{}) {
 	defer close(done)
 
 	s.taskLock.Lock()
+	if s.closed {
+		s.taskLock.Unlock()
+		s.logger.Debug(context.Background(), "Service closed, not re-scheduling", "task", t)
+		return
+	}
+
 	if prevTrigger, ok := s.tasks[t.ID]; ok {
 		delete(s.tasks, t.ID)
 		defer prevTrigger.cancel()
@@ -358,6 +371,11 @@ func (s *Service) StartTask(ctx context.Context, t *Task, opts runner.Opts) erro
 	doneCh := make(chan struct{})
 
 	s.taskLock.Lock()
+	if s.closed {
+		s.taskLock.Unlock()
+		s.logger.Debug(context.Background(), "Service closed, not re-scheduling", "task", t)
+		return errors.New("scheduler closed, please check the server status and logs")
+	}
 	s.tasks[t.ID] = cancelableTrigger{
 		cancel: cancel,
 		done:   doneCh,
@@ -371,6 +389,12 @@ func (s *Service) StartTask(ctx context.Context, t *Task, opts runner.Opts) erro
 func (s *Service) cancelTask(t *Task) {
 	var doneCh chan struct{}
 	s.taskLock.Lock()
+
+	if s.closed {
+		s.taskLock.Unlock()
+		s.logger.Debug(context.Background(), "Service closed, not re-scheduling", "task", t)
+		return
+	}
 	if trigger, ok := s.tasks[t.ID]; ok {
 		delete(s.tasks, t.ID)
 		doneCh = trigger.done
@@ -628,6 +652,7 @@ func (s *Service) Close() {
 		t.cancel()
 	}
 	s.tasks = nil
+	s.closed = true
 	s.taskLock.Unlock()
 	s.wg.Wait()
 }

commit 0511e89341e2cf3063fe271c21fc68a8cf1d047b

sched: make check fixes
Fixes make check issues introduced in ccc1f216

```
sched/service.go:172:2: the cancel function is not used on all paths (possible context leak) (govet)
        triggerCtx, cancel := context.WithCancel(log.WithTraceID(s.cronCtx))
        ^
sched/service.go:180:3: this return statement may be reached without using the cancel var defined on line 172 (govet)
                return
                ^
sched/service.go:371:2: the cancel function is not used on all paths (possible context leak) (govet)
        triggerCtx, cancel := context.WithCancel(log.WithTraceID(s.cronCtx))
        ^
sched/service.go:379:3: this return statement may be reached without using the cancel var defined on line 371 (govet)
                return errors.New("scheduler closed, please check the server status and logs")
                ^
sched/service.go:48:14: struct of size 96 bytes could be of size 88 bytes (maligned)
type Service struct {
             ^
```

sched/service.go
@@ -169,17 +169,17 @@ func (s *Service) schedTask(ctx context.Context, now time.Time, t *Task) {
 		"activation", activation,
 	)
 
-	triggerCtx, cancel := context.WithCancel(log.WithTraceID(s.cronCtx))
-
-	doneCh := make(chan struct{})
 	s.tasksMu.Lock()
-	defer s.tasksMu.Unlock()
-
 	if s.closed {
+		s.tasksMu.Unlock()
 		s.logger.Debug(ctx, "Service closed, not re-scheduling", "task", t)
 		return
 	}
+
+	triggerCtx, cancel := context.WithCancel(log.WithTraceID(s.cronCtx))
+	doneCh := make(chan struct{})
 	timer := time.AfterFunc(activation.Sub(now), func() { s.execTrigger(triggerCtx, t, doneCh) })
+
 	s.tasks[t.ID] = cancelableTrigger{
 		timer: timer,
 		cancel: func() {
@@ -190,6 +190,7 @@ func (s *Service) schedTask(ctx context.Context, now time.Time, t *Task) {
 		},
 		done: doneCh,
 	}
+	s.tasksMu.Unlock()
 }
 
 func (s *Service) reschedTask(ctx context.Context, t *Task, run *Run, done chan struct{}) {
@@ -368,41 +369,43 @@ func (s *Service) StartTask(ctx context.Context, t *Task, opts runner.Opts) erro
 	}
 	s.cancelTask(t)
 
+	s.tasksMu.Lock()
+	if s.closed {
+		s.tasksMu.Unlock()
+		return errors.New("scheduler closed, please check the server status and logs")
+	}
+
 	triggerCtx, cancel := context.WithCancel(log.WithTraceID(s.cronCtx))
 	triggerCtx = runner.WithOpts(triggerCtx, opts)
 	doneCh := make(chan struct{})
 
-	s.tasksMu.Lock()
-	if s.closed {
-		s.tasksMu.Unlock()
-		s.logger.Debug(context.Background(), "Service closed, not re-scheduling", "task", t)
-		return errors.New("scheduler closed, please check the server status and logs")
-	}
 	s.tasks[t.ID] = cancelableTrigger{
 		cancel: cancel,
 		done:   doneCh,
 	}
-	go s.execTrigger(triggerCtx, t, doneCh)
 	s.tasksMu.Unlock()
+
+	go s.execTrigger(triggerCtx, t, doneCh)
 
 	return nil
 }
 
 func (s *Service) cancelTask(t *Task) {
-	var doneCh chan struct{}
 	s.tasksMu.Lock()
-
 	if s.closed {
 		s.tasksMu.Unlock()
 		s.logger.Debug(context.Background(), "Service closed, not re-scheduling", "task", t)
 		return
 	}
+
+	var doneCh chan struct{}
 	if trigger, ok := s.tasks[t.ID]; ok {
 		delete(s.tasks, t.ID)
 		doneCh = trigger.done
 		trigger.cancel()
 	}
 	s.tasksMu.Unlock()
+
 	if doneCh != nil {
 		<-doneCh
 	}