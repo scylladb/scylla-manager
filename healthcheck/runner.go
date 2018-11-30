532c0cfbc99e91da26cec9b44559ab52d9cc3070
Michał Matczuk <michal@scylladb.com>
2018-11-30 10:02:53 +0100

    healthcheck: remove metrics for decommissioned hosts
    Fixes #804
    
    Signed-off-by: Michał Matczuk <michal@scylladb.com>

--- healthcheck/runner.go
+++ healthcheck/runner.go
@@ -9,12 +9,14 @@ import (
 
 	"github.com/pkg/errors"
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/scylladb/go-set/strset"
 	"github.com/scylladb/mermaid/cluster"
 	"github.com/scylladb/mermaid/internal/cqlping"
 	"github.com/scylladb/mermaid/sched/runner"
 	"github.com/scylladb/mermaid/scyllaclient"
-	"github.com/scylladb/mermaid/uuid"
 )
+
+const pingTimeout = 250 * time.Millisecond
 
 type healthCheckRunner struct {
 	cluster cluster.ProviderFunc
@@ -31,24 +33,31 @@ const pingTimeout = 250 * time.Millisecond
 
 // Run implements runner.Runner.
 func (r healthCheckRunner) Run(ctx context.Context, d runner.Descriptor, p runner.Properties) error {
-	// get cluster name
-	c, hosts, err := r.getClusterAndHosts(ctx, d.ClusterID)
+	c, err := r.cluster(ctx, d.ClusterID)
 	if err != nil {
-		return errors.Wrap(err, "invalid cluster")
+		return errors.Wrap(err, "failed to get cluster")
 	}
+
+	client, err := r.client(ctx, d.ClusterID)
+	if err != nil {
+		return errors.Wrap(err, "failed to get client")
+	}
+
+	hosts, err := client.Hosts(ctx)
+	if err != nil {
+		return errors.Wrap(err, "failed to get hosts")
+	}
+
+	r.removeDecommissionedHosts(c, hosts)
 
 	out := make(chan hostRTT, runtime.NumCPU()+1)
 	for _, h := range hosts {
 		v := hostRTT{host: h}
 		go func() {
-			const pingTimeout = 250 * time.Millisecond
 			v.rtt, v.err = cqlping.Ping(ctx, pingTimeout, v.host)
 			out <- v
 		}()
 	}
-
-	cqlStatus.Reset()
-	cqlRTT.Reset()
 
 	for range hosts {
 		v := <-out
@@ -70,6 +79,26 @@ func (r healthCheckRunner) Run(ctx context.Context, d runner.Descriptor, p runne
 	return nil
 }
 
+func (r healthCheckRunner) removeDecommissionedHosts(c *cluster.Cluster, hosts []string) {
+	m := strset.New(hosts...)
+
+	apply(collect(cqlStatus), func(cluster, host string, v float64) {
+		if c.String() != cluster {
+			return
+		}
+		if m.Has(host) {
+			return
+		}
+
+		l := prometheus.Labels{
+			clusterKey: c.String(),
+			hostKey:    host,
+		}
+		cqlStatus.Delete(l)
+		cqlRTT.Delete(l)
+	})
+}
+
 // Stop implements runner.Runner.
 func (r healthCheckRunner) Stop(ctx context.Context, d runner.Descriptor) error {
 	return nil
@@ -78,23 +107,4 @@ func (r healthCheckRunner) Stop(ctx context.Context, d runner.Descriptor) error 
 // Status implements runner.Runner.
 func (r healthCheckRunner) Status(ctx context.Context, d runner.Descriptor) (runner.Status, string, error) {
 	return runner.StatusDone, "", nil
-}
-
-func (r healthCheckRunner) getClusterAndHosts(ctx context.Context, clusterID uuid.UUID) (*cluster.Cluster, []string, error) {
-	c, err := r.cluster(ctx, clusterID)
-	if err != nil {
-		return nil, nil, errors.Wrap(err, "invalid cluster")
-	}
-
-	client, err := r.client(ctx, clusterID)
-	if err != nil {
-		return nil, nil, errors.Wrap(err, "failed to get client proxy")
-	}
-
-	hosts, err := client.Hosts(ctx)
-	if err != nil {
-		return nil, nil, errors.Wrap(err, "failed to get hosts")
-	}
-
-	return c, hosts, nil
 }
