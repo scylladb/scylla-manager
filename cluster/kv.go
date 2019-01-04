7a3cf1443219d3a71dd02ec23725846c5ac56dd0
Michał Matczuk <michal@scylladb.com>
2019-01-04 15:01:04 +0100

    cluster: added putWithRollback function
    Signed-off-by: Michał Matczuk <michal@scylladb.com>

---
+++ cluster/kv.go
@@ -0,0 +1,24 @@
+// Copyright (C) 2017 ScyllaDB
+
+package cluster
+
+import (
+	"github.com/pkg/errors"
+	"github.com/scylladb/mermaid"
+	"github.com/scylladb/mermaid/internal/kv"
+	"github.com/scylladb/mermaid/uuid"
+)
+
+func putWithRollback(s kv.Store, id uuid.UUID, value []byte) (rollback func(), err error) {
+	var old []byte
+	old, err = s.Get(id)
+	if err != nil && err != mermaid.ErrNotFound {
+		return nil, errors.Wrap(err, "failed to read old value")
+	}
+	if err := s.Put(id, value); err != nil {
+		return nil, errors.Wrap(err, "failed to write value")
+	}
+	return func() {
+		s.Put(id, old) // nolint:errcheck
+	}, nil
+}
