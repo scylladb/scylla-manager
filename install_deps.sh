13c83a9c6d556fd3e0e18ebdba54ad70b68707fa
Michał Matczuk <michal@scylladb.com>
2020-01-16 14:49:09 +0100

    golangci-lint: Updated to v1.22.2
    Signed-off-by: Michał Matczuk <michal@scylladb.com>

--- install_deps.sh
+++ install_deps.sh
@@ -8,7 +8,7 @@ UBUNTU_PKGS="jq make moreutils sshpass python python-pip"
 PYTHON_PKGS="cqlsh"
 
 GO_PKGS="
-golangci-lint       https://github.com/golangci/golangci-lint/releases/download/v1.21.0/golangci-lint-1.21.0-linux-amd64.tar.gz \
+golangci-lint       https://github.com/golangci/golangci-lint/releases/download/v1.22.2/golangci-lint-1.22.2-linux-amd64.tar.gz \
 sup                 https://github.com/pressly/sup/releases/download/v0.5.3/sup-linux64 \
 swagger             https://github.com/go-swagger/go-swagger/releases/download/v0.20.1/swagger_linux_amd64 \
 license-detector    https://github.com/src-d/go-license-detector/releases/download/2.0.2/license-detector.linux_amd64.gz \
