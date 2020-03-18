aaa05546503b43292d7d8db6db534f101d777739
Michał Matczuk <michal@scylladb.com>
2020-03-18 16:05:03 +0100

    service/secrets: Add PutWithRollback a rollback helper function (#1694)
    Signed-off-by: Michał Matczuk <michal@scylladb.com>

---
+++ pkg/service/secrets/rollback.go
@@ -0,0 +1,44 @@
+// Copyright (C) 2017 ScyllaDB
+
+package secrets
+
+import (
+	"github.com/pkg/errors"
+	"github.com/scylladb/mermaid/pkg/service"
+)
+
+type keyValueHolder struct {
+	KeyValue
+	data []byte
+}
+
+func (v *keyValueHolder) MarshalBinary() (data []byte, err error) {
+	return v.data, nil
+}
+
+func (v *keyValueHolder) UnmarshalBinary(data []byte) error {
+	v.data = data
+	return nil
+}
+
+// PutWithRollback gets former value of secret and returns a function to restore
+// it if put was successful.
+func PutWithRollback(store Store, secret KeyValue) (func(), error) {
+	old := &keyValueHolder{
+		KeyValue: secret,
+	}
+
+	if err := store.Get(old); err != nil && err != service.ErrNotFound {
+		return nil, errors.Wrap(err, "read")
+	}
+	if err := store.Put(secret); err != nil {
+		return nil, errors.Wrap(err, "put")
+	}
+	return func() {
+		if old.data == nil {
+			store.Delete(old) // nolint:errcheck
+		} else {
+			store.Put(old) // nolint:errcheck
+		}
+	}, nil
+}
---
+++ pkg/service/secrets/rollback_test.go
@@ -0,0 +1,106 @@
+// Copyright (C) 2017 ScyllaDB
+
+package secrets
+
+import (
+	"testing"
+
+	"github.com/scylladb/mermaid/pkg/service"
+	"github.com/scylladb/mermaid/pkg/util/uuid"
+)
+
+type testSecret []byte
+
+func newTestSecret(version byte) *testSecret {
+	return &testSecret{version}
+}
+
+func (v *testSecret) Key() (clusterID uuid.UUID, key string) {
+	return uuid.Nil, "test"
+}
+
+func (v *testSecret) MarshalBinary() (data []byte, err error) {
+	return *v, nil
+}
+
+func (v *testSecret) UnmarshalBinary(data []byte) error {
+	*v = data
+	return nil
+}
+
+type testStore []byte
+
+var _ Store = &testStore{}
+
+func (t *testStore) Put(secret KeyValue) error {
+	v, err := secret.MarshalBinary()
+	if err != nil {
+		return err
+	}
+	*t = v
+	return nil
+}
+
+func (t *testStore) Get(secret KeyValue) error {
+	if len(*t) == 0 {
+		return service.ErrNotFound
+	}
+	return secret.UnmarshalBinary(*t)
+}
+
+const deleted = 0xf
+
+func (t *testStore) Delete(secret KeyValue) error {
+	*t = testStore{deleted}
+	return nil
+}
+
+func (t *testStore) DeleteAll(clusterID uuid.UUID) error {
+	*t = testStore{deleted}
+	return nil
+}
+
+func (t *testStore) version() byte {
+	if len(*t) == 0 {
+		return 0
+	}
+	return []byte(*t)[0]
+}
+
+func TestPutWithRollback(t *testing.T) {
+	t.Parallel()
+
+	t.Run("delete", func(t *testing.T) {
+		s := &testStore{}
+		r, err := PutWithRollback(s, newTestSecret(1))
+		if err != nil {
+			t.Fatal("PutWithRollback() error ", err)
+		}
+		if s.version() != 1 {
+			t.Fatal("Wrong version", s.version())
+		}
+		r()
+		if s.version() != deleted {
+			t.Fatalf("Got version %d, expected deleted", s.version())
+		}
+	})
+
+	t.Run("update", func(t *testing.T) {
+		s := &testStore{}
+		r, err := PutWithRollback(s, newTestSecret(1))
+		if err != nil {
+			t.Fatal("PutWithRollback() error ", err)
+		}
+		r, err = PutWithRollback(s, newTestSecret(2))
+		if err != nil {
+			t.Fatal("PutWithRollback() error ", err)
+		}
+		if s.version() != 2 {
+			t.Fatal("Wrong version", s.version())
+		}
+		r()
+		if s.version() != 1 {
+			t.Fatal("Wrong version", s.version())
+		}
+	})
+}
