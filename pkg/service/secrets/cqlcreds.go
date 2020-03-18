afa71338742b90cc2bd0b51910076289915c4cbd
Michał Matczuk <michal@scylladb.com>
2020-03-18 14:38:16 +0100

    service/secrets: Add CQLCreds (#1694)
    CQLCreds is a new secret type that can encode cql user and password.
    
    Signed-off-by: Michał Matczuk <michal@scylladb.com>

---
+++ pkg/service/secrets/cqlcreds.go
@@ -0,0 +1,38 @@
+// Copyright (C) 2017 ScyllaDB
+
+package secrets
+
+import (
+	"encoding/json"
+
+	"github.com/scylladb/mermaid/pkg/util/uuid"
+)
+
+const (
+	cqlCredsKey = "cql_creds"
+)
+
+// CQLCreds specifies CQL credentials to cluster.
+type CQLCreds struct {
+	ClusterID uuid.UUID `json:"-"`
+	User      string    `json:"user"`
+	Password  string    `json:"password"`
+}
+
+// Key returns pair of `clusterID` and `key` used for identifying credentials.
+func (v *CQLCreds) Key() (clusterID uuid.UUID, key string) {
+	return v.ClusterID, cqlCredsKey
+}
+
+// MarshalBinary implements encoding.BinaryMarshaler.
+func (v *CQLCreds) MarshalBinary() (data []byte, err error) {
+	if v.User == "" {
+		return nil, nil
+	}
+	return json.Marshal(v)
+}
+
+// UnmarshalBinary implements encoding.BinaryUnmarshaler.
+func (v *CQLCreds) UnmarshalBinary(data []byte) error {
+	return json.Unmarshal(data, v)
+}
