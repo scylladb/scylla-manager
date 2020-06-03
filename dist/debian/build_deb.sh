41e38f02a14a44acf91487aec06e5fdd1d6bf5ae
Aleksandar Jankovic <aleksandar@scylladb.com>
2020-06-03 18:11:25 +0200

    dist/debian: fix version string embedding to the binary
    Execution of debian rules that are used by pdebuild didn't had proper environment variables.
    Release string vars needed to be explicitly.

--- dist/debian/build_deb.sh
+++ dist/debian/build_deb.sh
@@ -146,4 +146,5 @@ sudo rm -fv /var/cache/pbuilder/scylla-manager-$TARGET.tgz
 sudo DIST=$TARGET /usr/sbin/pbuilder clean --configfile ./dist/debian/pbuilderrc
 sudo DIST=$TARGET /usr/sbin/pbuilder create --configfile ./dist/debian/pbuilderrc
 sudo DIST=$TARGET /usr/sbin/pbuilder update --configfile ./dist/debian/pbuilderrc
-sudo DIST=$TARGET GO_VERSION="$GO_VERSION" CURL="/usr/bin/curl" pdebuild  --configfile ./dist/debian/pbuilderrc --buildresult dist/release/deb
+sudo DIST=$TARGET GO_VERSION="$GO_VERSION" CURL="/usr/bin/curl" VERSION="$MERMAID_VERSION" RELEASE="$MERMAID_RELEASE"\
+ pdebuild  --configfile ./dist/debian/pbuilderrc --buildresult dist/release/deb
