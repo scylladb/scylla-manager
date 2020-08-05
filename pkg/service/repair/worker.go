626df2db17e10140d74271b8a607d52c480dbdc4
Michał Matczuk <michal@scylladb.com>
2020-08-05 11:50:17 +0200

    service/repair: Moved sharding code from worker.go to shards.go

--- pkg/service/repair/worker.go
+++ pkg/service/repair/worker.go
@@ -10,7 +10,6 @@ import (
 	"github.com/pkg/errors"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/scylladb/go-log"
-	"github.com/scylladb/go-set/i64set"
 	"github.com/scylladb/go-set/strset"
 	"github.com/scylladb/mermaid/pkg/dht"
 	"github.com/scylladb/mermaid/pkg/scyllaclient"
@@ -207,103 +206,4 @@ func (w *worker) waitRepairStatus(ctx context.Context, id int32, host, keyspace 
 			}
 		}
 	}
-}
-
-func splitToShards(ttrs []*tableTokenRange, p *dht.Murmur3Partitioner) [][]*tableTokenRange {
-	res := make([][]*tableTokenRange, p.ShardCount())
-
-	for _, ttr := range ttrs {
-		start := ttr.StartToken
-		end := ttr.EndToken
-		shard := p.ShardOf(end - 1)
-
-		for start < end {
-			prev := p.PrevShard(shard)
-			token := p.TokenForPrevShard(end, shard)
-
-			if token > start {
-				res[shard] = append(res[shard], &tableTokenRange{
-					Keyspace:   ttr.Keyspace,
-					Table:      ttr.Table,
-					Pos:        ttr.Pos,
-					Replicas:   ttr.Replicas,
-					StartToken: token,
-					EndToken:   end,
-				})
-			} else {
-				res[shard] = append(res[shard], &tableTokenRange{
-					Keyspace:   ttr.Keyspace,
-					Table:      ttr.Table,
-					Pos:        ttr.Pos,
-					Replicas:   ttr.Replicas,
-					StartToken: start,
-					EndToken:   end,
-				})
-			}
-
-			end = token
-			shard = prev
-		}
-	}
-
-	return res
-}
-
-// validateShards checks that the shard split of segments is sound.
-func validateShards(ttrs []*tableTokenRange, shards [][]*tableTokenRange, p *dht.Murmur3Partitioner) error {
-	startTokens := i64set.New()
-	endTokens := i64set.New()
-
-	// Check that the s belong to the correct shards
-	for shard, s := range shards {
-		for _, r := range s {
-			if p.ShardOf(r.StartToken) != uint(shard) {
-				return errors.Errorf("wrong shard of a start token %d, expected %d, got %d", r.StartToken, p.ShardOf(r.StartToken), shard)
-			}
-			if p.ShardOf(r.EndToken-1) != uint(shard) {
-				return errors.Errorf("wrong shard of an end token %d, expected %d, got %d", r.EndToken-1, p.ShardOf(r.EndToken-1), shard)
-			}
-
-			// Extract tokens
-			startTokens.Add(r.StartToken)
-			endTokens.Add(r.EndToken)
-		}
-	}
-
-	// Check that shards contain the original start and end tokens
-	for _, ttr := range ttrs {
-		if !startTokens.Has(ttr.StartToken) {
-			return errors.Errorf("no start token %d", ttr.StartToken)
-		}
-		if !endTokens.Has(ttr.EndToken) {
-			return errors.Errorf("no end token %d", ttr.StartToken)
-		}
-
-		startTokens.Remove(ttr.StartToken)
-		endTokens.Remove(ttr.EndToken)
-	}
-
-	// Check that the range is continuous
-	var err error
-
-	startTokens.Each(func(item int64) bool {
-		if !endTokens.Has(item) {
-			err = errors.Errorf("missing end token for start token %d", item)
-			return false
-		}
-		return true
-	})
-	if err != nil {
-		return err
-	}
-
-	endTokens.Each(func(item int64) bool {
-		if !startTokens.Has(item) {
-			err = errors.Errorf("missing start token end token %d", item)
-			return false
-		}
-		return true
-	})
-
-	return err
 }
--- pkg/service/repair/worker_test.go
+++ pkg/service/repair/worker_test.go
@@ -401,30 +401,3 @@ func newTestClient(t *testing.T, hrt *HackableRoundTripper, logger log.Logger) *
 
 	return c
 }
-
-func TestSplitSegmentsToShards(t *testing.T) {
-	t.Parallel()
-
-	for _, shardCount := range []uint{1, 2, 3, 5, 8} {
-		p := dht.NewMurmur3Partitioner(shardCount, 12)
-		ttrs := []*tableTokenRange{
-			{
-				StartToken: dht.Murmur3MinToken,
-				EndToken:   dht.Murmur3MinToken + 1<<50,
-			},
-			{
-				StartToken: 9165301526494284802,
-				EndToken:   9190445181212206709,
-			},
-			{
-				StartToken: 9142565851149460331,
-				EndToken:   9143747749498840635,
-			},
-		}
-		v := splitToShards(ttrs, p)
-
-		if err := validateShards(ttrs, v, p); err != nil {
-			t.Fatal(err)
-		}
-	}
-}
