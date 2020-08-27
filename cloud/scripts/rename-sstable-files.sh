3fc396d8df3952b8362effda3b5f02e5f5d771ff
Maciej Zimnoch <maciej@scylladb.com>
2020-08-27 15:11:11 +0200

    cloud: change regexp used in rename-sstable script
    SSTable files weren't found by regexp used in script.
    Now it works fine.
    
    ```
    [maciej@maciej-scylla-test-1 ~]$ find /var/lib/scylla/data/keyspace1/standard1-8b945b70e7a411ea80c200000000000d -regextype posix-extended -regex '.*/[:alnum:]+\-([0-9]+)\-.*\-.*\..*$' -type f
    [maciej@maciej-scylla-test-1 ~]$ find /var/lib/scylla/data/keyspace1/standard1-8b945b70e7a411ea80c200000000000d -regextype posix-extended -regex '.*/[a-zA-Z]+-([0-9]+)-[a-zA-Z]+-[a-zA-Z]+\..*$' -type f | wc -l
    35514
    
    ```

--- cloud/scripts/rename-sstable-files.sh
+++ cloud/scripts/rename-sstable-files.sh
@@ -15,7 +15,7 @@ if [[ $1 == "" ]]; then
     exit 1
 fi
 
-for f in $(find "$1" -regextype posix-extended -regex '.*/[:alnum:]+\-([0-9]+)\-.*\-.*\..*$' -type f)
+for f in $(find "$1" -regextype posix-extended -regex '.*/[a-zA-Z]+-([0-9]+)-[a-zA-Z]+-[a-zA-Z]+\..*$' -type f)
 do
     mv "$f" $(sed -rn 's|-([[:digit:]]+)-|-\10-|p' <<< "$f")
 done
