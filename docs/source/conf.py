c652aa96951151a65d519ca4ded4da917d843676
Michał Matczuk <michal@scylladb.com>
2020-10-07 12:42:47 +0200

    docs: Generation and deployment adjustments
    - Deploy only if docs content has changes
    - Work only on a current branch
    - Remove "branch-" prefix from release branches

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -153,9 +153,9 @@ redirects_file = "_utils/redirections.yaml"
 # -- Options for multiversion --------------------------------------------
 
 # Whitelist pattern for tags (set to None to ignore all tags)
-smv_tag_whitelist = r'^.*$'
+smv_tag_whitelist = ""
 # Whitelist pattern for branches (set to None to ignore all branches)
-smv_branch_whitelist = r"^master$"
+smv_branch_whitelist = ""
 # Whitelist pattern for remotes (set to None to use local branches only)
 smv_remote_whitelist = r"^origin$"
 # Pattern for released versions
