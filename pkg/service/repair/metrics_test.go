385568b563afd928d833fd8409df0664369fa329
Aleksandar Jankovic <aleksandar@scylladb.com>
2020-10-14 16:22:16 +0200

    service/repair: fix metrics watcher (#2297)
    - Monitoring was broken because cluster name was not properly set.
    - Fixed issue with progress not getting to 100 because update was
    missing on stop.
    
    Fixes #2297

---
+++ pkg/service/repair/metrics_test.go
@@ -0,0 +1,57 @@
+// Copyright (C) 2017 ScyllaDB
+
+package repair
+
+import (
+	"bytes"
+	"fmt"
+	"testing"
+
+	"github.com/prometheus/client_golang/prometheus/testutil"
+	"github.com/scylladb/mermaid/pkg/util/uuid"
+)
+
+func TestRepairUpdateMetrics(t *testing.T) {
+	t.Parallel()
+
+	clusterID := uuid.NewTime()
+	taskID := uuid.NewTime()
+	runID := uuid.NewTime()
+	run := &Run{
+		ID:          runID,
+		ClusterID:   clusterID,
+		TaskID:      taskID,
+		clusterName: "my-cluster",
+	}
+	prog := Progress{
+		progress: progress{
+			TokenRanges: 160,
+			Success:     68,
+			Error:       0,
+		},
+		Hosts: []HostProgress{
+			{Host: "h1", progress: progress{TokenRanges: 100, Success: 40}, Tables: []TableProgress{
+				{Keyspace: "k1", Table: "t1", progress: progress{TokenRanges: 60, Success: 20}},
+				{Keyspace: "k1", Table: "t2", progress: progress{TokenRanges: 40, Success: 20}},
+			}},
+			{Host: "h2", progress: progress{TokenRanges: 60, Success: 28}, Tables: []TableProgress{
+				{Keyspace: "k1", Table: "t1", progress: progress{TokenRanges: 50, Success: 25}},
+				{Keyspace: "k2", Table: "t2", progress: progress{TokenRanges: 10, Success: 3}},
+			}},
+		},
+	}
+
+	updateMetrics(run, prog)
+
+	golden := fmt.Sprintf(`# HELP scylla_manager_repair_progress Current repair progress.
+        # TYPE scylla_manager_repair_progress gauge
+        scylla_manager_repair_progress{cluster="my-cluster",host="",keyspace="",task="%s"} 42
+        scylla_manager_repair_progress{cluster="my-cluster",host="h1",keyspace="k1",task="%s"} 41
+        scylla_manager_repair_progress{cluster="my-cluster",host="h2",keyspace="k1",task="%s"} 50
+        scylla_manager_repair_progress{cluster="my-cluster",host="h2",keyspace="k2",task="%s"} 30
+`, taskID, taskID, taskID, taskID)
+	if err := testutil.CollectAndCompare(repairProgress, bytes.NewBufferString(golden)); err != nil {
+		t.Fatal(err)
+	}
+
+}
--- pkg/util/prom/metricswatcher_test.go
+++ pkg/util/prom/metricswatcher_test.go
@@ -15,7 +15,7 @@ import (
 func TestMetricsWatcherCallbacks(t *testing.T) {
 	mw := NewMetricsWatcher(log.NewDevelopmentWithLevel(zapcore.DebugLevel))
 	wrapCalls := make(chan struct{}, 2)
-	calls := make(chan struct{}, 3)
+	calls := make(chan struct{}, 5)
 	stop1 := mw.RegisterCallback(func() {
 		calls <- struct{}{}
 	})
@@ -23,26 +23,29 @@ func TestMetricsWatcherCallbacks(t *testing.T) {
 		calls <- struct{}{}
 	})
 	defer stop2()
+
 	rh := mw.WrapHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		wrapCalls <- struct{}{}
 	}))
 	r := httptest.NewRequest("GET", "/foo", nil)
 	w := httptest.NewRecorder()
+
 	rh.ServeHTTP(w, r)
-	if len(calls) != 2 {
-		t.Fatalf("Expected two callback calls, got %d", len(calls))
+	firstStepCallCount := len(calls)
+	if firstStepCallCount != 2 {
+		t.Fatalf("Expected two request callback calls, got %d", len(calls))
 	}
 	stop1()
 	rh.ServeHTTP(w, r)
-	if len(calls) != 3 {
-		t.Fatalf("Expected three callback calls, got %d", len(calls))
+	if len(calls) != firstStepCallCount+2 {
+		t.Fatalf("Expected one stop call and one request callback, got %d", len(calls)-firstStepCallCount)
 	}
 	if len(wrapCalls) != 2 {
 		t.Fatalf("Expected two calls to wrapped handler, got %d", len(wrapCalls))
 	}
 }
 
-func TestMetricsWatcherSlowExecution(t *testing.T) {
+func TestMetricsWatcherCallbackTimeout(t *testing.T) {
 	defer func(old time.Duration) {
 		callbackTimeout = old
 	}(callbackTimeout)
@@ -50,12 +53,16 @@ func TestMetricsWatcherSlowExecution(t *testing.T) {
 
 	mw := NewMetricsWatcher(log.NewDevelopmentWithLevel(zapcore.DebugLevel))
 	wrapCalls := make(chan struct{}, 1)
-	calls := make(chan struct{}, 2)
+	calls := make(chan struct{}, 6)
+	wait := make(chan struct{})
 	stop1 := mw.RegisterCallback(func() {
-		time.Sleep(3 * callbackTimeout)
+		<-wait
 		calls <- struct{}{}
 	})
-	defer stop1()
+	defer func() {
+		close(wait)
+		stop1()
+	}()
 	stop2 := mw.RegisterCallback(func() {
 		calls <- struct{}{}
 	})
@@ -68,5 +75,9 @@ func TestMetricsWatcherSlowExecution(t *testing.T) {
 	rh.ServeHTTP(w, r)
 	if len(wrapCalls) != 1 {
 		t.Fatalf("Expected one call to wrapped handler, got %d", len(wrapCalls))
+	}
+	callCount := len(calls)
+	if callCount != 1 {
+		t.Fatalf("Expected one callback call, got %d", callCount)
 	}
 }
