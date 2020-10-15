21972fff04f438f99aeeaa776d08783c4631253b
Michał Matczuk <michal@scylladb.com>
2020-10-15 16:54:26 +0200

    cmd/sctool: Use docs.scylladb.com/operating-scylla/manager as base URL

--- pkg/cmd/sctool/docs.go
+++ pkg/cmd/sctool/docs.go
@@ -48,7 +48,7 @@ func commandDocsURL(cmd *cobra.Command) string {
 	return docsURL(link)
 }
 
-const docsBaseURL = "https://scylladb.github.io/mermaid/2.2"
+const docsBaseURL = "https://docs.scylladb.com/operating-scylla/manager/2.2"
 
 func docsURL(link string) string {
 	return docsBaseURL + "/sctool/" + link
