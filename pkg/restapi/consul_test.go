ee3287dcd4f0ab6b683ae63311991bf959f66b27
Aleksandar Jankovic <aleksandar@scylladb.com>
2020-11-06 14:41:36 +0100

    restapi: updated prometheus consul discovery (#2325)
    Prometheus Consul discovery module changed discovery endpoint with the release of Prometheus 2.17.0.
    New endpoint /v1/health/service/scylla is added to fix this.
    
    Fixes #2325

--- pkg/restapi/consul_test.go
+++ pkg/restapi/consul_test.go
@@ -43,7 +43,7 @@ func TestConsulAPI(t *testing.T) {
 		}
 
 		expected := map[string][]string{
-			"scylla": []string{"cluster1"},
+			"scylla": {"cluster1"},
 		}
 		got := map[string][]string{}
 		if err := json.Unmarshal(w.Body.Bytes(), &got); err != nil {
@@ -112,7 +112,92 @@ func TestConsulAPI(t *testing.T) {
 				},
 			},
 		}
-		got := []consulNode{}
+		var got []consulNode
+		if err := json.Unmarshal(w.Body.Bytes(), &got); err != nil {
+			t.Fatal(err.Error())
+		}
+		if diff := cmp.Diff(got, expected, testutils.UUIDComparer()); diff != "" {
+			t.Fatal(diff)
+		}
+		if h, ok := w.HeaderMap["X-Consul-Index"]; !ok && h[0] != "1" {
+			t.Fatalf("Wrong X-Consul-Index header %+v", w.HeaderMap)
+		}
+	})
+
+	t.Run("list health nodes", func(t *testing.T) {
+		nodes := []cluster.Node{
+			{
+				"dc1",
+				"127.0.0.1",
+				2,
+			},
+			{
+				"dc2",
+				"127.0.0.1",
+				3,
+			},
+		}
+		m := NewMockClusterService(ctrl)
+		m.EXPECT().ListClusters(gomock.Any(), &cluster.Filter{}).Return(clusters, nil)
+		m.EXPECT().ListNodes(gomock.Any(), clusters[0].ID).Return(nodes, nil)
+		h := NewPrometheus(m, &prom.MetricsWatcher{})
+
+		r := httptest.NewRequest(http.MethodGet, "/v1/health/service/scylla", nil)
+		w := httptest.NewRecorder()
+		h.ServeHTTP(w, r)
+
+		if w.Code != http.StatusOK {
+			t.Fatal(w.Code)
+		}
+		expected := []consulHealthServiceItem{
+			{
+				Node: consulHealthNode{
+					Datacenter: "dc1",
+					Node:       "127.0.0.1",
+					Address:    "127.0.0.1",
+					Meta: map[string]string{
+						"shard_num":    "2",
+						"dc":           "dc1",
+						"cluster_name": "cluster1",
+					},
+				},
+				Service: consulService{
+					Address: "127.0.0.1",
+					ID:      "scylla",
+					Service: "scylla",
+					Port:    9180,
+					Tags:    []string{"cluster1"},
+					Meta: map[string]string{
+						"dc":           "dc1",
+						"cluster_name": "cluster1",
+					},
+				},
+			},
+			{
+				Node: consulHealthNode{
+					Datacenter: "dc2",
+					Node:       "127.0.0.1",
+					Address:    "127.0.0.1",
+					Meta: map[string]string{
+						"shard_num":    "3",
+						"dc":           "dc2",
+						"cluster_name": "cluster1",
+					},
+				},
+				Service: consulService{
+					Address: "127.0.0.1",
+					ID:      "scylla",
+					Service: "scylla",
+					Port:    9180,
+					Tags:    []string{"cluster1"},
+					Meta: map[string]string{
+						"dc":           "dc2",
+						"cluster_name": "cluster1",
+					},
+				},
+			},
+		}
+		var got []consulHealthServiceItem
 		if err := json.Unmarshal(w.Body.Bytes(), &got); err != nil {
 			t.Fatal(err.Error())
 		}
