4f88b2ecd576b9df0b129700f2403d936ad98d54
Michał Matczuk <michal@scylladb.com>
2020-11-09 17:23:39 +0100

    Resolve top level package name problem (#2263)

--- docs.go
+++
@@ -1,5 +0,0 @@
-// Copyright (C) 2017 ScyllaDB
-
-// Package scylla-manager implements Scylla Manager, check user docs at
-// https://docs.scylladb.com/operating-scylla/manager.
-package scylla-manager
