33ecbb3c5d3712f0c5e8ab73f07b25dde0fd2655
Michał Matczuk <michal@scylladb.com>
2020-11-09 17:23:39 +0100

    Resolve top level package name problem (#2263)

--- mod/mod.go
+++ mod/mod.go
@@ -2,7 +2,7 @@
 
 // +build modules
 
-package scylla-manager
+package mod
 
 // This file exists only to trick modules into getting extra packages,
 // typically tools that are part of other packages that are otherwise wiped by
