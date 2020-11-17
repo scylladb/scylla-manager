bfae1a205699af1e685876ae64813c18c7006e63
Michał Matczuk <michal@scylladb.com>
2020-11-17 14:18:31 +0100

    docs: Cache poetry

--- docs/_utils/setup.sh
+++ docs/_utils/setup.sh
@@ -10,7 +10,7 @@ if pwd | egrep -q '\s'; then
 	exit 1
 fi
 
-which python3 || { echo "Failed to find python3. Try installing Python for your operative system: https://www.python.org/downloads/" && exit 1; }
-which poetry || curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 - && source ${HOME}/.poetry/env
-poetry install
-poetry update
+POETRY=${HOME}/.poetry/bin/poetry
+[ -x  "${POETRY}" ] || curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
+${POETRY} install
+${POETRY} update
