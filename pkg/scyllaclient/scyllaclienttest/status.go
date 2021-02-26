4dfef14746ef03cabe1994c0ebaf868c95b82091
Michał Matczuk <michal@scylladb.com>
2021-02-26 13:37:11 +0100

    scyllaclient/scyllaclienttest: add MakeAgentError

---
+++ pkg/scyllaclient/scyllaclienttest/status.go
@@ -0,0 +1,29 @@
+// Copyright (C) 2017 ScyllaDB
+
+package scyllaclienttest
+
+import (
+	"fmt"
+
+	agentModels "github.com/scylladb/scylla-manager/swagger/gen/agent/models"
+)
+
+type agentError struct {
+	payload *agentModels.ErrorResponse
+}
+
+// MakeAgentError creates synthetic errors that can be used with scyllaclient
+// Status and StatusAndMessage functions.
+func MakeAgentError(payload agentModels.ErrorResponse) error {
+	return agentError{
+		payload: &payload,
+	}
+}
+
+func (ae agentError) GetPayload() *agentModels.ErrorResponse {
+	return ae.payload
+}
+
+func (ae agentError) Error() string {
+	return fmt.Sprintf("agent [HTTP %d] %s", ae.payload.Status, ae.payload.Message)
+}
