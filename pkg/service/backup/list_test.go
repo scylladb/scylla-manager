097e1062bc5e11e4b7c6945bc3ed15e7f1bc854c
Michał Matczuk <michal@scylladb.com>
2021-06-01 12:25:52 +0200

    service/backup: list, extract listing code from purger.go to list.go

---
+++ pkg/service/backup/list_test.go
@@ -0,0 +1,47 @@
+// Copyright (C) 2017 ScyllaDB
+
+package backup
+
+import (
+	"context"
+	"strings"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+	"github.com/scylladb/scylla-manager/pkg/scyllaclient/scyllaclienttest"
+	. "github.com/scylladb/scylla-manager/pkg/service/backup/backupspec"
+	"github.com/scylladb/scylla-manager/pkg/testutils"
+	"github.com/scylladb/scylla-manager/pkg/util/uuid"
+)
+
+func TestListAllManifests(t *testing.T) {
+	client, closeServer := scyllaclienttest.NewFakeRcloneServer(t)
+	defer closeServer()
+
+	ctx := context.Background()
+
+	t.Run("one cluster", func(t *testing.T) {
+		manifests, error := listManifests(ctx, client, scyllaclienttest.TestHost,
+			Location{Provider: "testdata", Path: "list"},
+			uuid.MustParse("2e4ac82f-a7b5-4b6d-ab5e-0a1553a50a21"),
+		)
+		if error != nil {
+			t.Fatal("listManifests() error", error)
+		}
+		testutils.SaveGoldenJSONFileIfNeeded(t, manifests)
+		var golden []*RemoteManifest
+		testutils.LoadGoldenJSONFile(t, &golden)
+		if diff := cmp.Diff(manifests, golden, testutils.UUIDComparer(), cmpopts.SortSlices(func(a, b *RemoteManifest) bool {
+			if v := strings.Compare(a.NodeID, b.NodeID); v != 0 {
+				return v < 0
+			}
+			if v := strings.Compare(a.SnapshotTag, b.SnapshotTag); v != 0 {
+				return v < 0
+			}
+			return false
+		})); diff != "" {
+			t.Fatalf("listManifests() diff\n%s", diff)
+		}
+	})
+}
--- pkg/service/backup/purger.go
+++ pkg/service/backup/purger.go
@@ -7,7 +7,6 @@ import (
 	"net/http"
 	"path"
 	"sort"
-	"sync"
 	"time"
 
 	"github.com/pkg/errors"
@@ -20,98 +19,6 @@ import (
 	"github.com/scylladb/scylla-manager/pkg/util/uuid"
 	"go.uber.org/atomic"
 )
-
-// listManifestsInAllLocations returns manifests for all nodes of a in all
-// locations specified in hosts.
-func listManifestsInAllLocations(ctx context.Context, client *scyllaclient.Client, hosts []hostInfo, clusterID uuid.UUID) ([]*RemoteManifest, error) {
-	var (
-		locations = make(map[Location]struct{})
-		manifests []*RemoteManifest
-	)
-
-	for _, hi := range hosts {
-		if _, ok := locations[hi.Location]; ok {
-			continue
-		}
-		locations[hi.Location] = struct{}{}
-
-		lm, err := listManifests(ctx, client, hi.IP, hi.Location, clusterID)
-		if err != nil {
-			return nil, err
-		}
-		manifests = append(manifests, lm...)
-	}
-
-	return manifests, nil
-}
-
-// listManifests returns manifests for all nodes of a given cluster in the location.
-func listManifests(ctx context.Context, client *scyllaclient.Client, host string, location Location, clusterID uuid.UUID) ([]*RemoteManifest, error) {
-	baseDir := RemoteMetaClusterDCDir(clusterID)
-	opts := scyllaclient.RcloneListDirOpts{
-		FilesOnly: true,
-		Recurse:   true,
-	}
-	items, err := client.RcloneListDir(ctx, host, location.RemotePath(baseDir), &opts)
-	if err != nil {
-		return nil, err
-	}
-
-	var manifests []*RemoteManifest
-	for _, item := range items {
-		p := path.Join(baseDir, item.Path)
-		m := &RemoteManifest{}
-		if err := m.ParsePartialPath(p); err != nil {
-			continue
-		}
-		m.Location = location
-		manifests = append(manifests, m)
-	}
-	return manifests, nil
-}
-
-func groupManifestsByNode(manifests []*RemoteManifest) map[string][]*RemoteManifest {
-	v := map[string][]*RemoteManifest{}
-	for _, m := range manifests {
-		v[m.NodeID] = append(v[m.NodeID], m)
-	}
-	return v
-}
-
-func groupManifestsByTask(manifests []*RemoteManifest) map[uuid.UUID][]*RemoteManifest {
-	v := map[uuid.UUID][]*RemoteManifest{}
-	for _, m := range manifests {
-		v[m.TaskID] = append(v[m.TaskID], m)
-	}
-	return v
-}
-
-// popNodeIDManifestsForLocation returns a function that for a given location
-// finds next node and it's manifests from that location.
-func popNodeIDManifestsForLocation(manifests []*RemoteManifest) func(h hostInfo) (string, []*RemoteManifest) {
-	var mu sync.Mutex
-	nodeIDManifests := groupManifestsByNode(manifests)
-	return func(h hostInfo) (string, []*RemoteManifest) {
-		mu.Lock()
-		defer mu.Unlock()
-
-		// Fast path, get manifests for the current node
-		if manifests, ok := nodeIDManifests[h.ID]; ok {
-			delete(nodeIDManifests, h.ID)
-			return h.ID, manifests
-		}
-
-		// Look for other nodes in the same location
-		for nodeID, manifests := range nodeIDManifests {
-			if manifests[0].Location == h.Location {
-				delete(nodeIDManifests, nodeID)
-				return nodeID, manifests
-			}
-		}
-
-		return "", nil
-	}
-}
 
 // staleTags returns collection of snapshot tags for manifests that are "stale".
 // That is:
