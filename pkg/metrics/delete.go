2129c32e96d96d9d9d3aec30d2d47a0a54e56a40
Michał Matczuk <michal@scylladb.com>
2021-06-07 17:59:57 +0200

    metrics: move matcher to separate file

--- pkg/metrics/delete.go
+++ pkg/metrics/delete.go
@@ -5,24 +5,7 @@ package metrics
 import (
 	"github.com/prometheus/client_golang/prometheus"
 	dto "github.com/prometheus/client_model/go"
-	"github.com/scylladb/scylla-manager/pkg/util/uuid"
 )
-
-func clusterMatcher(clusterID uuid.UUID) func(m *dto.Metric) bool {
-	return LabelMatcher("cluster", clusterID.String())
-}
-
-// LabelMatcher returns a matcher checking only single label.
-func LabelMatcher(name, value string) func(m *dto.Metric) bool {
-	return func(m *dto.Metric) bool {
-		for _, l := range m.GetLabel() {
-			if l.GetName() == name && l.GetValue() == value {
-				return true
-			}
-		}
-		return false
-	}
-}
 
 // CollectorDeleter extends prometheus.Collector with Delete.
 type CollectorDeleter interface {
