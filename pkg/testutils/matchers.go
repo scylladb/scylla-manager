4258a1c7f914921e817f80b44127dbe47bbf9332
Michał Matczuk <michal@scylladb.com>
2021-09-21 11:27:41 +0200

    testutils: remove dependency on service/scheduler

--- pkg/testutils/matchers.go
+++ pkg/testutils/matchers.go
@@ -6,34 +6,8 @@ import (
 	"fmt"
 
 	"github.com/google/go-cmp/cmp"
-	"github.com/scylladb/scylla-manager/pkg/service/scheduler"
 	"github.com/scylladb/scylla-manager/pkg/util/uuid"
 )
-
-// TaskMatcher gomock.Matcher interface implementation for scheduler.Task.
-type TaskMatcher struct {
-	expected *scheduler.Task
-}
-
-// NewTaskMatcher returns gomock.Matcher for tasks. It compares only ID field.
-func NewTaskMatcher(expected *scheduler.Task) *TaskMatcher {
-	return &TaskMatcher{
-		expected: expected,
-	}
-}
-
-// Matches returns whether v is a match.
-func (m TaskMatcher) Matches(v interface{}) bool {
-	task, ok := v.(*scheduler.Task)
-	if !ok {
-		return false
-	}
-	return cmp.Equal(m.expected.ID, task.ID, UUIDComparer())
-}
-
-func (m TaskMatcher) String() string {
-	return fmt.Sprintf("is equal to task with ID: %s", m.expected.ID.String())
-}
 
 // UUIDMatcher gomock.Matcher interface implementation for UUID.
 type UUIDMatcher struct {
