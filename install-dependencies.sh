2bea80234c6d2a1cad0803c8c976f69844bd120a
Michał Matczuk <michal@scylladb.com>
2021-10-20 11:44:11 +0200

    bump github.com/goreleaser/goreleaser to v0.182.1

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -10,7 +10,7 @@ UBUNTU_PKGS="docker-compose jq make moreutils openssl sshpass"
 
 GO_PKGS="
 golangci-lint       https://github.com/golangci/golangci-lint/releases/download/v1.39.0/golangci-lint-1.39.0-linux-amd64.tar.gz
-goreleaser          https://github.com/goreleaser/goreleaser/releases/download/v0.168.0/goreleaser_Linux_x86_64.tar.gz
+goreleaser          https://github.com/goreleaser/goreleaser/releases/download/v0.182.1/goreleaser_Linux_x86_64.tar.gz
 sup                 https://github.com/pressly/sup/releases/download/v0.5.3/sup-linux64
 swagger             https://github.com/go-swagger/go-swagger/releases/download/v0.25.0/swagger_linux_amd64
 license-detector    https://github.com/src-d/go-license-detector/releases/download/2.0.2/license-detector.linux_amd64.gz
