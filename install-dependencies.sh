ebbc2f5ec8b659dbe02732aa5c4b32c4b49e8835
Michał Matczuk <michal@scylladb.com>
2021-12-06 11:31:08 +0100

    install-dependencies.sh: move to mod managed dependencies installed from source

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -8,15 +8,16 @@ set -eu -o pipefail
 LINUX_PKGS="docker-compose jq make moreutils openssl"
 
 GO_PKGS="
-golangci-lint       https://github.com/golangci/golangci-lint/releases/download/v1.39.0/golangci-lint-1.39.0-linux-amd64.tar.gz
-goreleaser          https://github.com/goreleaser/goreleaser/releases/download/v0.182.1/goreleaser_Linux_x86_64.tar.gz
-sup                 https://github.com/pressly/sup/releases/download/v0.5.3/sup-linux64
-swagger             https://github.com/go-swagger/go-swagger/releases/download/v0.25.0/swagger_linux_amd64
-license-detector    https://github.com/src-d/go-license-detector/releases/download/2.0.2/license-detector.linux_amd64.gz
+golangci-lint       github.com/golangci/golangci-lint/cmd/golangci-lint
+goreleaser          github.com/goreleaser/goreleaser/cmd
+license-detector    github.com/go-enry/go-license-detector/v4/cmd/license-detector
 mockgen             github.com/golang/mock/mockgen
 schemagen           github.com/scylladb/gocqlx/v2/cmd/schemagen
 stress              golang.org/x/tools/cmd/stress
-yq                  https://github.com/mikefarah/yq/releases/download/3.4.1/yq_linux_amd64"
+sup                 github.com/pressly/sup/cmd/sup
+swagger             github.com/go-swagger/go-swagger/cmd/swagger
+yq                  github.com/mikefarah/yq/v3/cmd
+"
 
 source ./env
 mkdir -p ${LOCAL_BIN}
@@ -45,38 +46,12 @@ rm -f "${LOCAL_BIN}"/*
 
 echo "==> Installing Go packages at ${LOCAL_BIN}"
 
-function download() {
-    case $2 in
-        *goreleaser*.tar.gz)
-          curl -sSq -L $2 | tar zxf - -C ${LOCAL_BIN} $1
-          ;;
-
-        *.tar.gz)
-            ;&
-        *.tgz)
-            curl -sSq -L $2 | tar zxf - --strip 1 -C ${LOCAL_BIN} --wildcards "*/$1"
-            ;;
-        *.gz)
-            curl -sSq -L $2 | gzip -d - > "${LOCAL_BIN}/$1"
-            ;;
-        *)
-            curl -sSq -L $2 -o "${LOCAL_BIN}/$1"
-            ;;
-    esac
-    chmod u+x "${LOCAL_BIN}/$1"
-}
-
-function install_from_vendor() {
-    GO111MODULE=on go build -mod=vendor -o "${LOCAL_BIN}/$1" ${2}
-}
-
 function install() {
-    echo "$1 $2"
-    if [[ $2 =~ http* ]]; then
-        download $1 $2
-    else
-        install_from_vendor $1 $2
-    fi
+    echo "$1 ($2)"
+
+    pushd mod > /dev/null
+    go build -mod=readonly -o "${LOCAL_BIN}/$1" ${2}
+    popd > /dev/null
 }
 
 pkgs=($(echo "${GO_PKGS}" | sed 's/\s+/\n/g'))
--- mod/mod.go
+++ mod/mod.go
@@ -1,5 +1,6 @@
 // Copyright (C) 2017 ScyllaDB
 
+//go:build modules
 // +build modules
 
 package mod
@@ -9,8 +10,14 @@ package mod
 // go mod tidy.
 
 import (
+	_ "github.com/go-enry/go-license-detector/v4/cmd/license-detector"
 	_ "github.com/go-openapi/runtime"
+	_ "github.com/go-swagger/go-swagger/cmd/swagger"
 	_ "github.com/golang/mock/mockgen"
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "github.com/goreleaser/goreleaser/cmd"
+	_ "github.com/mikefarah/yq/v3/cmd"
+	_ "github.com/pressly/sup/cmd/sup"
 	_ "github.com/scylladb/gocqlx/v2/cmd/schemagen"
 	_ "golang.org/x/tools/cmd/stress"
 )
