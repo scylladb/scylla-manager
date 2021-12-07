2cc47d8bd2c2c0a1d7ee0550ecc574f62b219831
Michał Matczuk <michal@scylladb.com>
2021-12-07 11:42:48 +0100

    testing: upgrade Minio and Prometheus
    - Versions are upgraded to provide arm builds
    - Minio is modernized (new envvars and flag)
    - Prometheus is put to the "public" network
    - Ports are mapped to localhost

--- testing/minio/add_user.sh
+++ testing/minio/add_user.sh
@@ -11,7 +11,7 @@ function mc() {
   docker run -it --rm --network=host -v $(pwd)/minio/mc:/root/.mc/ -v $(pwd)/minio/user-policy.json:/user-policy.json  minio/mc:"$MINIO_MC_VERSION" "$@"
 }
 
-mc config host add minio-test "$MINIO_ENDPOINT" "$MINIO_ACCESS_KEY" "$MINIO_SECRET_KEY"
+mc config host add minio-test "$MINIO_ENDPOINT" "$MINIO_ROOT_USER" "$MINIO_ROOT_PASSWORD"
 mc admin policy add minio-test user /user-policy.json
 mc admin user add minio-test "$MINIO_USER_ACCESS_KEY" "$MINIO_USER_SECRET_KEY"
 mc admin policy set minio-test user user="$MINIO_USER_ACCESS_KEY"
