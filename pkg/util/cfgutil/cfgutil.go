4024172f4a21aee15502e5bff52026b8e3eb745e
Michał Matczuk <michal@scylladb.com>
2021-12-13 17:06:07 +0100

    util/cfgutil: add PermissiveParseYAML to enable parsing parts of a file

--- pkg/util/cfgutil/cfgutil.go
+++ pkg/util/cfgutil/cfgutil.go
@@ -13,21 +13,45 @@ import (
 // It will overwrite any conflicting keys by the keys in the subsequent files.
 // Missing files will not cause an error but will just be skipped.
 func ParseYAML(target interface{}, files ...string) error {
-	var opts []config.YAMLOption
-	for _, f := range files {
-		exists, err := fileExists(f)
-		if err != nil {
-			return err
-		}
-		if exists {
-			opts = append(opts, config.File(f))
-		}
+	opts, err := fileOpts(files)
+	if err != nil {
+		return err
 	}
+	return parseYaml(target, opts)
+}
+
+// PermissiveParseYAML is a variant of ParseYAML that disables gopkg.in/yaml.v2's strict mode
+//
+// DO NOT USE.
+func PermissiveParseYAML(target interface{}, files ...string) error {
+	opts, err := fileOpts(files)
+	if err != nil {
+		return err
+	}
+	opts = append(opts, config.Permissive())
+	return parseYaml(target, opts)
+}
+
+func parseYaml(target interface{}, opts []config.YAMLOption) error {
 	cfg, err := config.NewYAML(opts...)
 	if err != nil {
 		return err
 	}
 	return cfg.Get(config.Root).Populate(target)
+}
+
+func fileOpts(files []string) ([]config.YAMLOption, error) {
+	var opts []config.YAMLOption
+	for _, f := range files {
+		exists, err := fileExists(f)
+		if err != nil {
+			return nil, err
+		}
+		if exists {
+			opts = append(opts, config.File(f))
+		}
+	}
+	return opts, nil
 }
 
 func fileExists(filename string) (bool, error) {
