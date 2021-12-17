3636e520f47db6bdb0a0439300d48e1f09b57f37
Michał Matczuk <michal@scylladb.com>
2021-12-17 13:51:57 +0100

    service/scheduler: resolver, dumbify resolver
    The resolver was built with a trie to be able to match prefixes of task IDs.
    This is replaced with two maps, cluster ID + task name to task ID map, and task ID to task info map.
    The resolver API remains more or less the same as the prefix functionality was not used.
    
    The rationale behind it is that it's better to have your own short name instead of a prefix or a random ID.
    When working with ID you would copy it anyway.

--- pkg/service/scheduler/resolver_test.go
+++ pkg/service/scheduler/resolver_test.go
@@ -8,98 +8,6 @@ import (
 	"github.com/scylladb/scylla-manager/pkg/util/uuid"
 )
 
-func TestResolverFind(t *testing.T) {
-	setup := []string{
-		"02b3bc0a-9973-408e-828d-c29fe4292edd",
-		"0d257f4b-25a5-4e49-b26e-418383eb200b",
-		"4aa2a2c1-8fed-47db-96e7-4ffad1007a0d",
-		"9c961ba1-0b98-44d5-aff1-e1bfd53f0f43",
-		"9ea60b8a-00b1-4662-8293-4bec9311fbda",
-		"c0db1e4d-6aae-4a08-9aa7-8863afca16f6",
-		"eb831839-4860-40e4-a35d-e4847f03d9cb",
-	}
-
-	table := []struct {
-		Pre   string
-		Match bool
-	}{
-		{
-			Pre:   "99999999",
-			Match: false,
-		},
-		{
-			Pre:   "4aa2a2c1",
-			Match: true,
-		},
-		{
-			Pre:   "eb831839-4860-40e4-a35d-e4847f03d9cb",
-			Match: true,
-		},
-		{
-			Pre:   "eb831839-4860-40e4-a35d-e4847f03d9cbXXXXX",
-			Match: false,
-		},
-	}
-
-	r := newResolver()
-	for _, s := range setup {
-		r.Put(taskInfo{TaskID: uuid.MustParse(s)})
-	}
-
-	for i := range table {
-		test := table[i]
-		t.Run(test.Pre, func(t *testing.T) {
-			if _, ok := r.Find(test.Pre); ok != test.Match {
-				t.Fatalf("Find() %v, expected %v", ok, test.Match)
-			}
-		})
-	}
-}
-
-func TestResolverFindByName(t *testing.T) {
-	setup := []string{
-		"foo",
-		"bar",
-		"foobar",
-	}
-
-	table := []struct {
-		Pre   string
-		Match bool
-	}{
-		{
-			Pre:   "foo",
-			Match: true,
-		},
-		{
-			Pre:   "foob",
-			Match: true,
-		},
-		{
-			Pre:   "bar",
-			Match: true,
-		},
-		{
-			Pre:   "f",
-			Match: false,
-		},
-	}
-
-	r := newResolver()
-	for _, s := range setup {
-		r.Put(taskInfo{TaskID: uuid.MustRandom(), TaskName: s})
-	}
-
-	for i := range table {
-		test := table[i]
-		t.Run(test.Pre, func(t *testing.T) {
-			if _, ok := r.Find(test.Pre); ok != test.Match {
-				t.Fatalf("Find() %v, expected %v", ok, test.Match)
-			}
-		})
-	}
-}
-
 func TestResolverChangeName(t *testing.T) {
 	ti := taskInfo{
 		ClusterID: uuid.MustRandom(),
@@ -111,11 +19,14 @@ func TestResolverChangeName(t *testing.T) {
 	ti.TaskName = "new name"
 	r.Put(ti)
 
-	if _, ok := r.Find("name"); ok {
+	v := ti
+	v.TaskName = "name"
+	if r.FillTaskID(&v) {
 		t.Fatalf("Name not removed")
 	}
-	if v, _ := r.Find("new"); v != ti {
-		t.Fatalf("Find() = %v, expected %v", v, ti)
+	v.TaskName = "new name"
+	if !r.FillTaskID(&v) {
+		t.Fatalf("FillTaskID() = %v, expected %v", v, ti)
 	}
 }
 
@@ -133,7 +44,7 @@ func TestResolverNameUUIDHijacking(t *testing.T) {
 	r.Put(ti0)
 	r.Put(ti1)
 
-	if eid, _ := r.Find(ti0.TaskID.String()); eid != ti0 {
-		t.Fatalf("Find() = %v, expected %v", eid, ti0)
+	if eid, _ := r.FindByID(ti0.TaskID); eid != ti0 {
+		t.Fatalf("FindByID() = %v, expected %v", eid, ti0)
 	}
 }
