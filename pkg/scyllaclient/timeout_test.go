113a9ad7e1825e7fcb8e75b846d2b79a75938a5f
Michał Matczuk <michal@scylladb.com>
2022-02-22 15:08:45 +0100

    scyllaclient: timeout, add support for timeout while streaming response body
    Relates to scylladb/scylla-enterprise#2083
    
    Without this patch request can error with `context deadline exceeded` and the timeout increase logic would not kick in like in the example below
    
    ```
    Feb 17 02:20:33 ip-172-29-11-98.eu-central-1.compute.internal scylla-manager[20171]: {"L":"INFO","T":"2022-02-17T02:20:33.898Z","N":"cluster.client","M":"HTTP retry backoff","operation":"OperationsList","wait":"1.167087508s","error":"context deadline exceeded","_trace_id":"H4gGm0hTQJWwOTApzHjTww"}
    Feb 17 02:20:50 ip-172-29-11-98.eu-central-1.compute.internal scylla-manager[20171]: {"L":"INFO","T":"2022-02-17T02:20:50.067Z","N":"cluster.client","M":"HTTP retry backoff","operation":"OperationsList","wait":"2.240297671s","error":"context deadline exceeded","_trace_id":"H4gGm0hTQJWwOTApzHjTww"}
    Feb 17 02:21:07 ip-172-29-11-98.eu-central-1.compute.internal scylla-manager[20171]: {"L":"INFO","T":"2022-02-17T02:21:07.312Z","N":"cluster.client","M":"HTTP retry backoff","operation":"OperationsList","wait":"4.57509025s","error":"context deadline exceeded","_trace_id":"H4gGm0hTQJWwOTApzHjTww"}
    Feb 17 02:21:26 ip-172-29-11-98.eu-central-1.compute.internal scylla-manager[20171]: {"L":"INFO","T":"2022-02-17T02:21:26.889Z","N":"cluster.client","M":"HTTP retry backoff","operation":"OperationsList","wait":"6.619364793s","error":"context deadline exceeded","_trace_id":"H4gGm0hTQJWwOTApzHjTww"}
    Feb 17 02:21:48 ip-172-29-11-98.eu-central-1.compute.internal scylla-manager[20171]: {"L":"INFO","T":"2022-02-17T02:21:48.511Z","N":"cluster.client","M":"HTTP retry backoff","operation":"OperationsList","wait":"15.85745673s","error":"context deadline exceeded","_trace_id":"H4gGm0hTQJWwOTApzHjTww"}
    Feb 17 02:22:19 ip-172-29-11-98.eu-central-1.compute.internal scylla-manager[20171]: {"L":"INFO","T":"2022-02-17T02:22:19.369Z","N":"cluster.client","M":"HTTP retry backoff","operation":"OperationsList","wait":"29.786856468s","error":"context deadline exceeded","_trace_id":"H4gGm0hTQJWwOTApzHjTww"}
    Feb 17 02:23:04 ip-172-29-11-98.eu-central-1.compute.internal scylla-manager[20171]: {"L":"INFO","T":"2022-02-17T02:23:04.157Z","N":"cluster.client","M":"HTTP retry backoff","operation":"OperationsList","wait":"27.526159426s","error":"context deadline exceeded","_trace_id":"H4gGm0hTQJWwOTApzHjTww"}
    Feb 17 02:23:46 ip-172-29-11-98.eu-central-1.compute.internal scylla-manager[20171]: {"L":"INFO","T":"2022-02-17T02:23:46.686Z","N":"cluster.client","M":"HTTP retry backoff","operation":"OperationsList","wait":"27.489310051s","error":"context deadline exceeded","_trace_id":"H4gGm0hTQJWwOTApzHjTww"}
    Feb 17 02:24:29 ip-172-29-11-98.eu-central-1.compute.internal scylla-manager[20171]: {"L":"INFO","T":"2022-02-17T02:24:29.177Z","N":"cluster.client","M":"HTTP retry backoff","operation":"OperationsList","wait":"26.769972031s","error":"context deadline exceeded","_trace_id":"H4gGm0hTQJWwOTApzHjTww"}
    ```
    
    This patch removes scyllaclient.ErrTimeout and switches to context.DeadlineExceeded.
    
    Fixes #3048
    
    (cherry picked from commit dea020e2265afcfbf35ca1f126cf1d4e02fd9562)

--- pkg/scyllaclient/timeout_test.go
+++ pkg/scyllaclient/timeout_test.go
@@ -4,13 +4,16 @@ package scyllaclient_test
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"testing"
 	"time"
 
+	"github.com/go-openapi/strfmt"
 	"github.com/scylladb/scylla-manager/pkg/scyllaclient"
 	"github.com/scylladb/scylla-manager/pkg/scyllaclient/scyllaclienttest"
+	"github.com/scylladb/scylla-manager/swagger/gen/agent/models"
 )
 
 func TestCustomTimeout(t *testing.T) {
@@ -64,10 +67,52 @@ func TestCustomTimeout(t *testing.T) {
 			_, err := client.Ping(test.Context, host, test.Timeout)
 			close(done)
 
-			golden := fmt.Sprintf("after %s: timeout", test.ExpectedTimeout)
+			golden := fmt.Sprintf("after %s: context deadline exceeded", test.ExpectedTimeout)
 			if err == nil || err.Error() != golden {
 				t.Fatalf("Ping() error = %v, expected %s", err, golden)
 			}
 		})
 	}
+}
+
+func TestTimeoutWhileStreaming(t *testing.T) {
+	s := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, `{"list":[`)
+		b, err := json.Marshal(models.ListItem{
+			IsDir:   false,
+			ModTime: strfmt.DateTime{},
+			Name:    "foo",
+			Path:    "/bar/foo",
+			Size:    42,
+		})
+		if err != nil {
+			panic(err)
+		}
+		var (
+			d = 150 * time.Millisecond
+			s = 10 * time.Millisecond
+		)
+		for i := 0; i < int(d/s); i++ {
+			w.Write(b)
+			w.Write([]byte(","))
+			if f, ok := w.(http.Flusher); ok {
+				f.Flush()
+			}
+			time.Sleep(s)
+		}
+		w.Write(b)
+		w.Write([]byte("]}"))
+	})
+
+	host, port, closeServer := scyllaclienttest.MakeServer(t, s)
+	defer closeServer()
+	client := scyllaclienttest.MakeClient(t, host, port, func(config *scyllaclient.Config) {
+		config.Timeout = 50 * time.Millisecond
+	})
+
+	l, err := client.RcloneListDir(context.Background(), host, "s3:foo", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("got %d items", len(l))
 }
