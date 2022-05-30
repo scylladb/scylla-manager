9fef405d78a695bba9be52351be283fb3d8b06a2
Michał Matczuk <michal@scylladb.com>
2022-05-30 11:26:23 +0200

    docs: update docs to 3.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,9 +9,9 @@ from sphinx_scylladb_theme.utils import multiversion_regex_builder
 
 # Build documentation for the following tags and branches
 TAGS = []
-BRANCHES = ['master', 'branch-2.2', 'branch-2.3', 'branch-2.4', 'branch-2.5', 'branch-2.6']
+BRANCHES = ['master', 'branch-2.2', 'branch-2.3', 'branch-2.4', 'branch-2.5', 'branch-2.6', 'branch-3.0']
 # Set the latest version.
-LATEST_VERSION = 'branch-2.6'
+LATEST_VERSION = 'branch-3.0'
 # Set which versions are not released yet.
 UNSTABLE_VERSIONS = ['master']
 # Set which versions are deprecated
