2ea9c7121ecd9d8d34d34917bcf58cfe11082dd4
Karol Kokoszka <karol.kokoszka@scylladb.com>
2022-08-08 12:26:54 +0200

    service/backup: integration-test for universal restore foundation WIP

--- pkg/service/backup/worker_upload.go
+++ pkg/service/backup/worker_upload.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/pkg/errors"
+
 	"github.com/scylladb/scylla-manager/v3/pkg/scyllaclient"
 	. "github.com/scylladb/scylla-manager/v3/pkg/service/backup/backupspec"
 	"github.com/scylladb/scylla-manager/v3/pkg/util/parallel"
