128b44e3c44cd5f304657214ba096b386ad71e44
Michał Leszczyński <2000michal@wp.pl>
2023-03-22 20:26:26 +0100

    scyllaclient_test: added test for rclone copy dir suffix option

--- pkg/scyllaclient/client_rclone_agent_integration_test.go
+++ pkg/scyllaclient/client_rclone_agent_integration_test.go
@@ -6,11 +6,15 @@
 package scyllaclient_test
 
 import (
+	"bytes"
 	"context"
+	"fmt"
+	"path"
 	"strings"
 	"testing"
 	"time"
 
+	"github.com/pkg/errors"
 	"github.com/scylladb/go-log"
 	"github.com/scylladb/scylla-manager/v3/pkg/scyllaclient"
 	. "github.com/scylladb/scylla-manager/v3/pkg/testutils"
@@ -167,6 +171,210 @@ func TestRcloneStoppingTransferIntegration(t *testing.T) {
 	if job.Transferred[0].Error == "" {
 		t.Fatal("Expected error but got empty")
 	}
+}
+
+func TestRcloneSuffixOptionIntegration(t *testing.T) {
+	var (
+		// "backup/meta" prefix in bucket path allows for using rclone cat
+		srcPath = "data:tmp/backup/meta"
+		srcFile = path.Join(srcPath, "file")
+		dstPath = remotePath("backup/meta")
+	)
+
+	const (
+		firstContents  = "First file!"
+		secondContents = "Second file!"
+		thirdContents  = "Third file!"
+		firstSuffix    = ".suffix_1"
+		secondSuffix   = ".suffix_2"
+	)
+
+	config := scyllaclient.TestConfig(ManagedClusterHosts(), AgentAuthToken())
+	client, err := scyllaclient.NewClient(config, log.NewDevelopment())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	testHost := ManagedClusterHost()
+	S3InitBucket(t, testBucket)
+	ctx := context.Background()
+	// This test also uses data from local data dir, so it needs to perform cleanup at the end
+	defer func() {
+		cmd := injectDataDir("rm -rf %s/tmp")
+		_, _, err := ExecOnHost(testHost, cmd)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}()
+
+	Print("Put first file in src dir")
+
+	b := bytes.NewBuffer([]byte(firstContents))
+	if err = client.RclonePut(ctx, testHost, srcFile, b); err != nil {
+		t.Fatal(err)
+	}
+	// Validate contents of src and dst dirs after each step
+	srcM := map[string]string{
+		"file": firstContents,
+	}
+	dstM := map[string]string{}
+	if err := validateDirContents(ctx, client, testHost, srcPath, srcM); err != nil {
+		t.Fatal(err)
+	}
+	if err := validateDirContents(ctx, client, testHost, dstPath, dstM); err != nil {
+		t.Fatal(err)
+	}
+
+	Print("Copy src into dst")
+
+	id, err := client.RcloneCopyDir(ctx, testHost, dstPath, srcPath, "")
+	if err != nil {
+		t.Fatal(err)
+	}
+	WaitCond(t, func() bool {
+		job, err := client.RcloneJobProgress(ctx, testHost, id, longPollingTimeoutSeconds)
+		if err != nil {
+			t.Fatal(err)
+		}
+		return scyllaclient.RcloneJobStatus(job.Status) == scyllaclient.JobSuccess
+	}, time.Second, 3*time.Second)
+
+	srcM = map[string]string{
+		"file": firstContents,
+	}
+	dstM = map[string]string{
+		"file": firstContents,
+	}
+	if err := validateDirContents(ctx, client, testHost, srcPath, srcM); err != nil {
+		t.Fatal(err)
+	}
+	if err := validateDirContents(ctx, client, testHost, dstPath, dstM); err != nil {
+		t.Fatal(err)
+	}
+
+	Print("Modify src file")
+
+	b = bytes.NewBuffer([]byte(secondContents))
+	if err = client.RclonePut(ctx, testHost, srcFile, b); err != nil {
+		t.Fatal(err)
+	}
+
+	srcM = map[string]string{
+		"file": secondContents,
+	}
+	dstM = map[string]string{
+		"file": firstContents,
+	}
+	if err := validateDirContents(ctx, client, testHost, srcPath, srcM); err != nil {
+		t.Fatal(err)
+	}
+	if err := validateDirContents(ctx, client, testHost, dstPath, dstM); err != nil {
+		t.Fatal(err)
+	}
+
+	Print("Copy src into dst after file modification")
+
+	id, err = client.RcloneCopyDir(ctx, testHost, dstPath, srcPath, firstSuffix)
+	if err != nil {
+		t.Fatal(err)
+	}
+	WaitCond(t, func() bool {
+		job, err := client.RcloneJobProgress(ctx, testHost, id, longPollingTimeoutSeconds)
+		if err != nil {
+			t.Fatal(err)
+		}
+		return scyllaclient.RcloneJobStatus(job.Status) == scyllaclient.JobSuccess
+	}, time.Second, 3*time.Second)
+
+	srcM = map[string]string{
+		"file": secondContents,
+	}
+	dstM = map[string]string{
+		"file" + firstSuffix: firstContents,
+		"file":               secondContents,
+	}
+	if err := validateDirContents(ctx, client, testHost, srcPath, srcM); err != nil {
+		t.Fatal(err)
+	}
+	if err := validateDirContents(ctx, client, testHost, dstPath, dstM); err != nil {
+		t.Fatal(err)
+	}
+
+	Print("Modify src file again")
+
+	b = bytes.NewBuffer([]byte(thirdContents))
+	if err = client.RclonePut(ctx, testHost, srcFile, b); err != nil {
+		t.Fatal(err)
+	}
+
+	srcM = map[string]string{
+		"file": thirdContents,
+	}
+	dstM = map[string]string{
+		"file" + firstSuffix: firstContents,
+		"file":               secondContents,
+	}
+	if err := validateDirContents(ctx, client, testHost, srcPath, srcM); err != nil {
+		t.Fatal(err)
+	}
+	if err := validateDirContents(ctx, client, testHost, dstPath, dstM); err != nil {
+		t.Fatal(err)
+	}
+
+	Print("Copy src into dst after another file modification")
+
+	id, err = client.RcloneCopyDir(ctx, testHost, dstPath, srcPath, secondSuffix)
+	if err != nil {
+		t.Fatal(err)
+	}
+	WaitCond(t, func() bool {
+		job, err := client.RcloneJobProgress(ctx, testHost, id, longPollingTimeoutSeconds)
+		if err != nil {
+			t.Fatal(err)
+		}
+		return scyllaclient.RcloneJobStatus(job.Status) == scyllaclient.JobSuccess
+	}, time.Second, 3*time.Second)
+
+	srcM = map[string]string{
+		"file": thirdContents,
+	}
+	dstM = map[string]string{
+		"file" + firstSuffix:  firstContents,
+		"file" + secondSuffix: secondContents,
+		"file":                thirdContents,
+	}
+	if err := validateDirContents(ctx, client, testHost, srcPath, srcM); err != nil {
+		t.Fatal(err)
+	}
+	if err := validateDirContents(ctx, client, testHost, dstPath, dstM); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func validateDirContents(ctx context.Context, client *scyllaclient.Client, host, remotePath string, files map[string]string) error {
+	// Check if all specified files with given contents are present
+	for f, expected := range files {
+		gotBytes, err := client.RcloneCat(ctx, host, path.Join(remotePath, f))
+		if err != nil {
+			return err
+		}
+		got := string(gotBytes)
+		if got != expected {
+			return errors.New(fmt.Sprintf("checking file %s, expected: %s, got: %s", f, expected, got))
+		}
+	}
+	// Check if these are the only files present
+	var iterErr error
+	err := client.RcloneListDirIter(ctx, host, remotePath, nil, func(item *scyllaclient.RcloneListDirItem) {
+		_, ok := files[item.Name]
+		if !ok {
+			iterErr = errors.New(fmt.Sprintf("didn't expect file: %s to be present", item.Name))
+		}
+	})
+	if err != nil {
+		return err
+	}
+	return iterErr
 }
 
 const scyllaDataDir = "/var/lib/scylla/data"
