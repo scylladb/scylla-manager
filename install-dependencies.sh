64041551bab4a2f7308ea37a4a30a902bc41df28
Karol Kokoszka <karol.kokoszka@scylladb.com>
2023-07-14 14:38:55 +0200

    deps: remove mod/go.mod and update install-dependencies.sh

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -7,18 +7,6 @@ set -eu -o pipefail
 
 LINUX_PKGS="docker-compose jq make moreutils openssl"
 
-GO_PKGS="
-golangci-lint       github.com/golangci/golangci-lint/cmd/golangci-lint
-goreleaser          github.com/goreleaser/goreleaser
-license-detector    github.com/go-enry/go-license-detector/v4/cmd/license-detector
-mockgen             github.com/golang/mock/mockgen
-schemagen           github.com/scylladb/gocqlx/v2/cmd/schemagen
-stress              golang.org/x/tools/cmd/stress
-sup                 github.com/pressly/sup/cmd/sup
-swagger             github.com/go-swagger/go-swagger/cmd/swagger
-yq                  github.com/mikefarah/yq/v3/cmd
-"
-
 source ./env
 mkdir -p ${LOCAL_BIN}
 
@@ -46,19 +34,15 @@ rm -f "${LOCAL_BIN}"/*
 
 echo "==> Installing Go packages at ${LOCAL_BIN}"
 
-function install() {
-    echo "$1 ($2)"
-
-    pushd mod > /dev/null
-    go build -mod=readonly -o "${LOCAL_BIN}/$1" ${2}
-    popd > /dev/null
-}
-
-pkgs=($(echo "${GO_PKGS}" | sed 's/\s+/\n/g'))
-for i in "${!pkgs[@]}"; do
-    if [[ $(($i % 2)) == 0 ]]; then
-        install ${pkgs[$i]} ${pkgs[$((i+1))]}
-    fi
-done
+export GOBIN=${LOCAL_BIN}
+go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.50.1
+go install github.com/goreleaser/goreleaser@v1.14.1
+go install github.com/go-enry/go-license-detector/v4/cmd/license-detector@latest
+go install github.com/golang/mock/mockgen@v1.6.0
+go install github.com/scylladb/gocqlx/v2/cmd/schemagen@v2.6.0
+go install golang.org/x/tools/cmd/stress@v0.2.0
+go install github.com/pressly/sup/cmd/sup@v0.5.3
+go install github.com/go-swagger/go-swagger/cmd/swagger@v0.25.0
+go install github.com/mikefarah/yq/v3@v3.0.0-20201202084205-8846255d1c37
 
 echo "==> Complete!"
