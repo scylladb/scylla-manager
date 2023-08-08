2326f696317ce72df3b938eeb1cf2b3014c6ab49
Karol Kokoszka <karol.kokoszka@scylladb.com>
2023-08-08 17:18:20 +0200

    repair: remove force_repair_type from config

--- pkg/service/repair/config.go
+++ pkg/service/repair/config.go
@@ -10,23 +10,12 @@ import (
 	"go.uber.org/multierr"
 )
 
-// Type represents type of the repair algorithm.
-type Type string
-
-const (
-	// TypeAuto auto detects repair algo.
-	TypeAuto Type = "auto"
-	// TypeRowLevel row level repair.
-	TypeRowLevel Type = "row_level"
-)
-
 // Config specifies the repair service configuration.
 type Config struct {
 	PollInterval                    time.Duration `yaml:"poll_interval"`
 	LongPollingTimeoutSeconds       int           `yaml:"long_polling_timeout_seconds"`
 	AgeMax                          time.Duration `yaml:"age_max"`
 	GracefulStopTimeout             time.Duration `yaml:"graceful_stop_timeout"`
-	ForceRepairType                 Type          `yaml:"force_repair_type"`
 	Murmur3PartitionerIgnoreMSBBits int           `yaml:"murmur3_partitioner_ignore_msb_bits"`
 }
 
@@ -35,7 +24,6 @@ func DefaultConfig() Config {
 		PollInterval:                    50 * time.Millisecond,
 		LongPollingTimeoutSeconds:       10,
 		GracefulStopTimeout:             30 * time.Second,
-		ForceRepairType:                 TypeAuto,
 		Murmur3PartitionerIgnoreMSBBits: 12,
 	}
 }
@@ -54,11 +42,6 @@ func (c *Config) Validate() error {
 	}
 	if c.GracefulStopTimeout <= 0 {
 		err = multierr.Append(err, errors.New("invalid graceful_stop_timeout, must be > 0"))
-	}
-	switch c.ForceRepairType {
-	case TypeAuto, TypeRowLevel:
-	default:
-		err = multierr.Append(err, errors.Errorf("invalid force_repair_type value %s", c.ForceRepairType))
 	}
 	if c.Murmur3PartitionerIgnoreMSBBits < 0 {
 		err = multierr.Append(err, errors.New("invalid murmur3_partitioner_ignore_msb_bits, must be >= 0"))
