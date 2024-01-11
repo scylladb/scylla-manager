57012bb770d4d929919f7f274df5366b17c0bbda
Michał Leszczyński <2000michal@wp.pl>
2024-01-11 11:44:32 +0100

    fix(restore): restore-schema, don't hide issues with raft schema
    Removing filtering was done so that our tests can pass with Scylla 5.4 and raft enabled, but it didn't improve the real life situations where agents don't have cross region remote location access.

--- pkg/service/restore/worker.go
+++ pkg/service/restore/worker.go
@@ -121,7 +121,7 @@ func (w *worker) initTarget(ctx context.Context, properties json.RawMessage) err
 			remotePath     = l.RemotePath("")
 			locationStatus = status
 		)
-		// In case location does not have specified dc, use nodes from all dcs
+
 		if l.DC == "" {
 			w.logger.Info(ctx, "No datacenter specified for location - using all nodes for this location", "location", l)
 		} else {
