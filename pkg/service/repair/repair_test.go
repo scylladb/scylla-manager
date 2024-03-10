f48e64b6450c935c803cb8df7c9e7e3de0231135
Michał Leszczyński <2000michal@wp.pl>
2024-03-11 00:36:29 +0100

    feat(repair): add ShouldRepairRing
    ShouldRepairRing is a cleaner and more comprehensible way of checking if Ring should be repaired.

--- pkg/service/repair/repair_test.go
+++ pkg/service/repair/repair_test.go
@@ -106,3 +106,186 @@ func TestMaxRingParallel(t *testing.T) {
 		})
 	}
 }
+
+func TestShouldRepairRing(t *testing.T) {
+	hostDC := map[string]string{
+		// dc1 -> 1
+		"h1": "dc1",
+		// dc2 -> 1
+		"h2": "dc2",
+		// dc3 -> 3
+		"h3": "dc3",
+		"h4": "dc3",
+		"h5": "dc3",
+		// dc3 -> 4
+		"h6": "dc4",
+		"h7": "dc4",
+		"h8": "dc4",
+		"h9": "dc4",
+	}
+
+	testCases := []struct {
+		Ring     scyllaclient.Ring
+		DCs      []string
+		Host     string
+		Expected bool
+	}{
+		{
+			Ring: scyllaclient.Ring{
+				HostDC:      hostDC,
+				Replication: scyllaclient.LocalStrategy,
+				RF:          1,
+			},
+			DCs:      []string{"dc1", "dc2", "dc3", "dc4"},
+			Expected: false,
+		},
+		{
+			Ring: scyllaclient.Ring{
+				HostDC:      hostDC,
+				Replication: scyllaclient.SimpleStrategy,
+				RF:          1,
+			},
+			DCs:      []string{"dc1", "dc2", "dc3", "dc4"},
+			Expected: false,
+		},
+		{
+			Ring: scyllaclient.Ring{
+				HostDC:      hostDC,
+				Replication: scyllaclient.SimpleStrategy,
+				RF:          2,
+			},
+			DCs:      []string{"dc1", "dc2", "dc3", "dc4"},
+			Expected: true,
+		},
+		{
+			Ring: scyllaclient.Ring{
+				HostDC:      hostDC,
+				Replication: scyllaclient.SimpleStrategy,
+				RF:          3,
+			},
+			DCs:      []string{"dc3", "dc4"},
+			Expected: false,
+		},
+		{
+			Ring: scyllaclient.Ring{
+				HostDC:      hostDC,
+				Replication: scyllaclient.SimpleStrategy,
+				RF:          4,
+			},
+			DCs:      []string{"dc3", "dc4"},
+			Expected: true,
+		},
+		{
+			Ring: scyllaclient.Ring{
+				HostDC:      hostDC,
+				Replication: scyllaclient.SimpleStrategy,
+				RF:          4,
+			},
+			DCs:      []string{"dc4"},
+			Expected: false,
+		},
+		{
+			Ring: scyllaclient.Ring{
+				HostDC:      hostDC,
+				Replication: scyllaclient.NetworkTopologyStrategy,
+				RF:          4,
+				DCrf: map[string]int{
+					"dc1": 1,
+					"dc2": 1,
+					"dc3": 1,
+					"dc4": 1,
+				},
+			},
+			DCs:      []string{"dc1", "dc2", "dc3", "dc4"},
+			Expected: true,
+		},
+		{
+			Ring: scyllaclient.Ring{
+				HostDC:      hostDC,
+				Replication: scyllaclient.NetworkTopologyStrategy,
+				RF:          4,
+				DCrf: map[string]int{
+					"dc1": 1,
+					"dc2": 1,
+					"dc3": 1,
+					"dc4": 1,
+				},
+			},
+			DCs:      []string{"dc1"},
+			Expected: false,
+		},
+		{
+			Ring: scyllaclient.Ring{
+				HostDC:      hostDC,
+				Replication: scyllaclient.NetworkTopologyStrategy,
+				RF:          8,
+				DCrf: map[string]int{
+					"dc1": 1,
+					"dc2": 1,
+					"dc3": 2,
+					"dc4": 2,
+				},
+			},
+			DCs:      []string{"dc3"},
+			Expected: true,
+		},
+		{
+			Ring: scyllaclient.Ring{
+				HostDC:      hostDC,
+				Replication: scyllaclient.NetworkTopologyStrategy,
+				RF:          8,
+				DCrf: map[string]int{
+					"dc1": 1,
+					"dc2": 1,
+					"dc3": 3,
+					"dc4": 4,
+				},
+			},
+			DCs:      []string{"dc4"},
+			Host:     "h6",
+			Expected: true,
+		},
+		{
+			Ring: scyllaclient.Ring{
+				HostDC:      hostDC,
+				Replication: scyllaclient.NetworkTopologyStrategy,
+				RF:          8,
+				DCrf: map[string]int{
+					"dc1": 1,
+					"dc2": 1,
+					"dc3": 3,
+					"dc4": 4,
+				},
+			},
+			DCs:      []string{"dc4"},
+			Host:     "h2",
+			Expected: false,
+		},
+		{
+			Ring: scyllaclient.Ring{
+				HostDC:      hostDC,
+				Replication: scyllaclient.NetworkTopologyStrategy,
+				RF:          8,
+				DCrf: map[string]int{
+					"dc1": 1,
+					"dc2": 1,
+					"dc3": 3,
+					"dc4": 4,
+				},
+			},
+			DCs:      []string{"dc1", "dc2", "dc3", "dc4"},
+			Host:     "h1",
+			Expected: true,
+		},
+	}
+
+	for i := range testCases {
+		tc := testCases[i]
+		t.Run("test "+fmt.Sprint(i), func(t *testing.T) {
+			t.Parallel()
+			if out := repair.ShouldRepairRing(tc.Ring, tc.DCs, tc.Host); out != tc.Expected {
+				t.Fatalf("Expected %v, got %v", tc.Expected, out)
+			}
+		})
+	}
+}
