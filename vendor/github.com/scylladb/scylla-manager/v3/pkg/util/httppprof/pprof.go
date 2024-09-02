106ae6f98e53ad81a8c82fdb7c8c58e243b1a562
Karol Kokoszka <karol.kokoszka@scylladb.com>
2024-09-02 11:00:34 +0200

    modules: import github.com/scylladb/scylla-manager/v3/util

---
+++ vendor/github.com/scylladb/scylla-manager/v3/pkg/util/httppprof/pprof.go
@@ -0,0 +1,19 @@
+// Copyright (C) 2017 ScyllaDB
+
+package httppprof
+
+import (
+	"net/http"
+	"net/http/pprof"
+)
+
+// Handler returns http.Handler serving pprof endpoints.
+func Handler() http.Handler {
+	m := http.NewServeMux()
+	m.HandleFunc("/debug/pprof/", pprof.Index)
+	m.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
+	m.HandleFunc("/debug/pprof/profile", pprof.Profile)
+	m.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+	m.HandleFunc("/debug/pprof/trace", pprof.Trace)
+	return m
+}
