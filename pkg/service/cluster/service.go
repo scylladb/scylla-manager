scylladb/scylla-manager

commit 5bf6b35699c0645ba8598a53516e03ba7e01383f

fix(cluster): don't mix DNS and IPs
Scylla operates on host IDs or IPs, but it doesn't handle DNS names well:
"N":"cluster.client","M":"HTTP retry now","operation":"SnitchDatacenterGet",
"error":"agent [HTTP 500] std::invalid_argument (scylla-manager-cluster-client.scylla-manager.svc)"

That's why SM also operates mostly on IPs.
The only place where SM uses DNS name is the initial cluster contact point ('sctool cluster add --host').
So Cluster.Host should be used only for discover purposes. After that we should use its discovered IP.

pkg/service/cluster/service.go
@@ -137,17 +137,8 @@ func (s *Service) CreateClientNoCache(ctx context.Context, clusterID uuid.UUID) 
 		return nil, err
 	}
 
-	knownHosts, err := s.filterKnownHosts(ctx, c)
-	if err != nil {
-		if errors.Is(err, ErrNoValidKnownHost) {
-			s.logger.Error(ctx, "There is no single valid known host for the cluster."+
-				"Please update it with 'sctool cluster update -h <host>'.", "cluster", c.ID, "coordinatorHost", c.Host,
-				"knownHosts", c.KnownHosts)
-		}
-		return nil, err
-	}
-	if err := s.setKnownHosts(c, knownHosts); err != nil {
-		return nil, errors.Wrap(err, "update known_hosts on cluster")
+	if err := s.discoverAndSetClusterHosts(ctx, c); err != nil {
+		return nil, errors.Wrap(err, "discover and set cluster hosts")
 	}
 
 	config := s.clientConfig(c)
@@ -160,26 +151,54 @@ func (s *Service) clientConfig(c *Cluster) scyllaclient.Config {
 		config.Port = fmt.Sprint(c.Port)
 	}
 	config.AuthToken = c.AuthToken
-	config.Hosts = c.AllHosts()
+	config.Hosts = c.KnownHosts
 	return config
 }
 
-func (s *Service) filterKnownHosts(ctx context.Context, c *Cluster) ([]string, error) {
-	config := s.clientConfig(c)
-	client, err := scyllaclient.NewClient(config, s.logger.Named("client"))
+func (s *Service) discoverAndSetClusterHosts(ctx context.Context, c *Cluster) error {
+	knownHosts, err := s.discoverClusterHosts(ctx, c)
 	if err != nil {
-		return nil, err
+		if errors.Is(err, ErrNoValidKnownHost) {
+			s.logger.Error(ctx, "There is no single valid known host for the cluster. "+
+				"Please update it with 'sctool cluster update -h <host>'",
+				"cluster", c.ID,
+				"contact point", c.Host,
+				"discovered hosts", c.KnownHosts,
+			)
+		}
+		return err
 	}
-	defer logutil.LogOnError(ctx, s.logger, client.Close, "Couldn't close scylla client")
+	return errors.Wrap(s.setKnownHosts(c, knownHosts), "update known_hosts in SM DB")
+}
 
-	for _, host := range config.Hosts {
-		knownHosts, err := s.discoverHosts(scyllaclient.ClientContextWithSelectedHost(ctx, host), client)
+func (s *Service) discoverClusterHosts(ctx context.Context, c *Cluster) ([]string, error) {
+	var contactPoints []string
+	contactPoints = append(contactPoints, c.Host)          // Go with the designated contact point first
+	contactPoints = append(contactPoints, c.KnownHosts...) // In case it failed, try to contact previously discovered hosts
+
+	for _, cp := range contactPoints {
+		config := scyllaclient.DefaultConfigWithTimeout(s.timeoutConfig)
+		if c.Port != 0 {
+			config.Port = fmt.Sprint(c.Port)
+		}
+		config.AuthToken = c.AuthToken
+		config.Hosts = []string{cp}
+
+		client, err := scyllaclient.NewClient(config, s.logger.Named("client"))
 		if err != nil {
-			s.logger.Error(ctx, "Cannot find known hosts using coordinator host", "host", host, "error", err)
+			s.logger.Error(ctx, "Couldn't connect to contact point", "contact point", cp, "error", err)
+			continue
+		}
+
+		knownHosts, err := s.discoverHosts(ctx, client)
+		logutil.LogOnError(ctx, s.logger, client.Close, "Couldn't close scylla client")
+		if err != nil {
+			s.logger.Error(ctx, "Couldn't discover hosts", "host", cp, "error", err)
 			continue
 		}
 		return knownHosts, nil
 	}
+
 	return nil, ErrNoValidKnownHost
 }
 
@@ -463,18 +482,10 @@ func (s *Service) validateHostsConnectivity(ctx context.Context, c *Cluster) err
 	if err := s.loadKnownHosts(c); err != nil && !errors.Is(err, gocql.ErrNotFound) {
 		return errors.Wrap(err, "load known hosts")
 	}
-	c.KnownHosts = c.AllHosts()
 
-	knownHosts, err := s.filterKnownHosts(ctx, c)
-	if err != nil {
-		if errors.Is(err, ErrNoValidKnownHost) {
-			s.logger.Error(ctx, "There is no single valid known host for the cluster."+
-				"Please update it with 'sctool cluster update -h <host>'.", "cluster", c.ID, "coordinatorHost", c.Host,
-				"knownHosts", c.KnownHosts)
-		}
-		return err
+	if err := s.discoverAndSetClusterHosts(ctx, c); err != nil {
+		return errors.Wrap(err, "discover and set cluster hosts")
 	}
-	c.KnownHosts = knownHosts
 
 	config := s.clientConfig(c)
 	client, err := scyllaclient.NewClient(config, s.logger.Named("client"))

pkg/service/cluster/model.go
@@ -6,7 +6,6 @@ import (
 	"crypto/tls"
 
 	"github.com/pkg/errors"
-	"github.com/scylladb/go-set/strset"
 	"github.com/scylladb/scylla-manager/v3/pkg/util"
 	"github.com/scylladb/scylla-manager/v3/pkg/util/uuid"
 	"go.uber.org/multierr"
@@ -17,8 +16,8 @@ type Cluster struct {
 	ID         uuid.UUID         `json:"id"`
 	Name       string            `json:"name"`
 	Labels     map[string]string `json:"labels"`
-	Host       string            `json:"host"`
-	KnownHosts []string          `json:"-"`
+	Host       string            `json:"host"` // The initial contact point for SM (DNS or IP)
+	KnownHosts []string          `json:"-"`    // Hosts discovered by connecting to Host (IPs)
 	Port       int               `json:"port,omitempty"`
 	AuthToken  string            `json:"auth_token"`
 
@@ -70,16 +69,6 @@ func (c *Cluster) Validate() error {
 	}
 
 	return util.ErrValidate(errors.Wrap(errs, "invalid cluster"))
-}
-
-// AllHosts returns KnownHosts with Host at the front and without duplicates.
-func (c *Cluster) AllHosts() []string {
-	hostSet := strset.New(c.KnownHosts...)
-	if c.Host == "" {
-		return hostSet.List()
-	}
-	hostSet.Remove(c.Host)
-	return append([]string{c.Host}, hostSet.List()...)
 }
 
 // Filter filters Clusters.

commit 55bfde57d42fb4e20e7ec9cf426c438cf7bf272d

fix(cluster): don't insert known hosts before validation finishes
Commit 5bf6b356 introduced a bug that when adding cluster to SM finished with error,
cluster was still saved in SM DB, but only with the ID and known hosts fields.
That's because method validateHostsConnectivity used discoverAndSetClusterHosts
instead of discoverClusterHosts, which happened before inserting cluster to SM DB.

Fixes #4028

pkg/service/cluster/service.go
@@ -483,9 +483,11 @@ func (s *Service) validateHostsConnectivity(ctx context.Context, c *Cluster) err
 		return errors.Wrap(err, "load known hosts")
 	}
 
-	if err := s.discoverAndSetClusterHosts(ctx, c); err != nil {
-		return errors.Wrap(err, "discover and set cluster hosts")
+	knownHosts, err := s.discoverClusterHosts(ctx, c)
+	if err != nil {
+		return errors.Wrap(err, "discover cluster hosts")
 	}
+	c.KnownHosts = knownHosts
 
 	config := s.clientConfig(c)
 	client, err := scyllaclient.NewClient(config, s.logger.Named("client"))