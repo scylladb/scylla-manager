f3baac6b0daa5dc698d9693113be653694f61f85
Michał Leszczyński <2000michal@wp.pl>
2024-10-16 11:58:35 +0200

    feat(restore): extend batch test with host retry
    This commit extends TestBatchDispatcher to include
    failures in its scenario and to validate host retry
    in a different datacenter.

--- pkg/service/restore/batch_test.go
+++ pkg/service/restore/batch_test.go
@@ -22,6 +22,7 @@ func TestBatchDispatcher(t *testing.T) {
 		{
 			ManifestInfo: &backupspec.ManifestInfo{
 				Location: l1,
+				DC:       "dc1",
 			},
 			TableName: TableName{
 				Keyspace: "ks1",
@@ -37,6 +38,7 @@ func TestBatchDispatcher(t *testing.T) {
 		{
 			ManifestInfo: &backupspec.ManifestInfo{
 				Location: l1,
+				DC:       "dc1",
 			},
 			TableName: TableName{
 				Keyspace: "ks1",
@@ -53,6 +55,7 @@ func TestBatchDispatcher(t *testing.T) {
 		{
 			ManifestInfo: &backupspec.ManifestInfo{
 				Location: l1,
+				DC:       "dc2",
 			},
 			TableName: TableName{
 				Keyspace: "ks1",
@@ -68,6 +71,7 @@ func TestBatchDispatcher(t *testing.T) {
 		{
 			ManifestInfo: &backupspec.ManifestInfo{
 				Location: l1,
+				DC:       "dc1",
 			},
 			TableName: TableName{
 				Keyspace: "ks1",
@@ -83,6 +87,7 @@ func TestBatchDispatcher(t *testing.T) {
 		{
 			ManifestInfo: &backupspec.ManifestInfo{
 				Location: l2,
+				DC:       "dc1",
 			},
 			TableName: TableName{
 				Keyspace: "ks1",
@@ -117,19 +122,27 @@ func TestBatchDispatcher(t *testing.T) {
 		dir   string
 		size  int64
 		count int
+		err   bool
 	}{
 		{host: "h1", ok: true, dir: "c", size: 60, count: 1},
-		{host: "h1", ok: true, dir: "c", size: 50, count: 1},
+		{host: "h1", ok: true, dir: "c", size: 50, count: 1, err: true},
+		{host: "h1", ok: true, dir: "b", size: 20, count: 1}, // host retry in different dc
+		{host: "h2", ok: true, dir: "c", size: 50, count: 1}, // batch retry
+		{host: "h1", ok: true, dir: "b", size: 10, count: 1, err: true},
+		{host: "h1"}, // already failed in all dcs
+		{host: "h2", ok: true, dir: "b", size: 10, count: 1}, // batch retry
 		{host: "h2", ok: true, dir: "b", size: 30, count: 2},
 		{host: "h3", ok: true, dir: "d", size: 200, count: 2},
-		{host: "h3", ok: false},
+		{host: "h3"},
 		{host: "h2", ok: true, dir: "a", size: 20, count: 2},
 		{host: "h2", ok: true, dir: "e", size: 10, count: 3}, // batch extended with leftovers < shard_cnt
-		{host: "h1", ok: false},
-		{host: "h2", ok: false},
+		{host: "h1"},
+		{host: "h2"},
 	}
 
 	for _, step := range scenario {
+		// use dispatchBatch instead of DispatchBatch because
+		// we don't want to hang here.
 		b, ok := bd.dispatchBatch(step.host)
 		if ok != step.ok {
 			t.Errorf("Expected %v, got %#v", step, b)
@@ -140,7 +153,13 @@ func TestBatchDispatcher(t *testing.T) {
 		if b.RemoteSSTableDir != step.dir || b.Size != step.size || len(b.SSTables) != step.count {
 			t.Errorf("Expected %v, got %#v", step, b)
 		}
-		bd.ReportSuccess(b)
+		if step.err {
+			if err := bd.ReportFailure(step.host, b); err != nil {
+				t.Fatal(err)
+			}
+		} else {
+			bd.ReportSuccess(b)
+		}
 	}
 
 	if err := bd.ValidateAllDispatched(); err != nil {
