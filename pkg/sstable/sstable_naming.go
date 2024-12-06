a15433dfbf1658f410721a5c4c7a18a63c67ec66
Michał Leszczyński <2000michal@wp.pl>
2024-12-06 10:55:05 +0100

    feat(sstable): add SSTable component description

--- pkg/sstable/sstable_naming.go
+++ pkg/sstable/sstable_naming.go
@@ -139,3 +139,56 @@ func RandomSSTableUUID() string {
 		encodeBase36(uint64(nsecs/100)),
 		encodeBase36(msb))
 }
+
+// Component defines SSTable components by their suffixes.
+// Definitions and documentation were taken from:
+// https://github.com/scylladb/scylladb/blob/master/docs/dev/sstables-directory-structure.md#sstable-files.
+//
+// Note that SSTables of different format version might consist
+// of different set of component files.
+type Component string
+
+const (
+	// ComponentData is the SSTable data file,
+	// containing a part of the actual data stored in the database.
+	ComponentData Component = "Data.db"
+
+	// ComponentIndex of the row keys with pointers to their positions in the data file.
+	ComponentIndex Component = "Index.db"
+
+	// ComponentFilter is a structure stored in memory that checks
+	// if row data exists in the memtable before accessing SSTables on disk.
+	ComponentFilter Component = "Filter.db"
+
+	// ComponentCompressionInfo is a file holding information about
+	// uncompressed data length, chunk offsets and other compression information.
+	ComponentCompressionInfo Component = "CompressionInfo.db"
+
+	// ComponentStatistics is a statistical metadata about the content of the SSTable
+	// and encoding statistics for the data file, starting with the mc format.
+	ComponentStatistics Component = "Statistics.db"
+
+	// ComponentSummary holds a sample of the partition index stored in memory.
+	ComponentSummary Component = "Summary.db"
+
+	// ComponentTOC is a file that stores the list of all components for the SSTable TOC.
+	// See details below regarding the use of a temporary TOC name during creation and deletion of SSTables.
+	ComponentTOC Component = "TOC.txt"
+
+	// ComponentScylla holds scylla-specific metadata about the SSTable,
+	// such as sharding information, extended features support, and sstabe-run identifier.
+	ComponentScylla Component = "Scylla.db"
+
+	// ComponentCRC holds the CRC32 for chunks in an uncompressed file.
+	ComponentCRC Component = "CRC.db"
+
+	// ComponentDigest are files holding the checksum of the data file.
+	// The method used for checksum is specific to the SSTable format version.
+
+	// ComponentDigestCRC holds crc32 checksum.
+	ComponentDigestCRC Component = "Digest.crc32"
+	// ComponentDigestADLER holds adler32 checksum.
+	ComponentDigestADLER Component = "Digest.adler32"
+	// ComponentDigestSHA1 holds sha1 checksum.
+	ComponentDigestSHA1 Component = "Digest.sha1"
+)
