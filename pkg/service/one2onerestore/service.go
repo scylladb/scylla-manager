b3acdddb5b97c9bc30f49f3122c1abf2da9d6ba2
Vasil Averyanau <VAveryanov8@users.noreply.github.com>
2025-02-07 09:28:15 +0100

    feat(1-1-restore): adds 1-1-restore cli and service stub (#4221)
    * feat(1-1-restore): adds new 1-1-restore sub command
    
    This introduces new cli sub command for the 1-1-restore
    
    Relates: #4200

---
+++ pkg/service/one2onerestore/service.go
@@ -0,0 +1,86 @@
+// Copyright (C) 2025 ScyllaDB
+
+package one2onerestore
+
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/pkg/errors"
+	"github.com/scylladb/go-log"
+	"github.com/scylladb/gocqlx/v2"
+
+	"github.com/scylladb/scylla-manager/v3/pkg/scyllaclient"
+	"github.com/scylladb/scylla-manager/v3/pkg/service/cluster"
+	"github.com/scylladb/scylla-manager/v3/pkg/service/configcache"
+	"github.com/scylladb/scylla-manager/v3/pkg/util/uuid"
+)
+
+// Servicer is an interface that defines one2onerestore service API contract.
+type Servicer interface {
+	// One2OneRestore restores data (tables) from the source cluster backup to the target cluster if they have equal topology:
+	// nodes count, shards count, token ownership should be exactly the same.
+	One2OneRestore(ctx context.Context, clusterID, taskID, runID uuid.UUID, properties json.RawMessage) error
+	// Runner creates a Runner that handles 1-1-restore operations.
+	Runner() Runner
+}
+
+// Service for the 1-1-restore.
+type Service struct {
+	session gocqlx.Session
+
+	scyllaClient   scyllaclient.ProviderFunc
+	clusterSession cluster.SessionFunc
+	configCache    configcache.ConfigCacher
+	logger         log.Logger
+}
+
+func NewService(session gocqlx.Session, scyllaClient scyllaclient.ProviderFunc, clusterSession cluster.SessionFunc, configCache configcache.ConfigCacher,
+	logger log.Logger,
+) (Servicer, error) {
+	if session.Session == nil || session.Closed() {
+		return nil, errors.New("invalid session")
+	}
+
+	if scyllaClient == nil {
+		return nil, errors.New("invalid scylla provider")
+	}
+
+	return &Service{
+		session: session,
+
+		scyllaClient:   scyllaClient,
+		clusterSession: clusterSession,
+		configCache:    configCache,
+
+		logger: logger,
+	}, nil
+}
+
+// One2OneRestore creates and initializes worker performing 1-1-restore with given properties.
+func (s *Service) One2OneRestore(ctx context.Context, clusterID, taskID, runID uuid.UUID, properties json.RawMessage) error {
+	s.logger.Info(ctx, "1-1-restore",
+		"cluster_id", clusterID,
+		"task_id", taskID,
+		"run_id", runID,
+	)
+
+	target, err := s.parseTarget(properties)
+	if err != nil {
+		return errors.Wrap(err, "parse target")
+	}
+
+	s.logger.Info(ctx, "Not yet implemented", "target", target)
+	return nil
+}
+
+func (s *Service) parseTarget(properties json.RawMessage) (Target, error) {
+	target := defaultTarget()
+	if err := json.Unmarshal(properties, &target); err != nil {
+		return Target{}, errors.Wrap(err, "unmarshal json")
+	}
+	if err := target.validateProperties(); err != nil {
+		return Target{}, errors.Wrap(err, "invalid target")
+	}
+	return target, nil
+}
