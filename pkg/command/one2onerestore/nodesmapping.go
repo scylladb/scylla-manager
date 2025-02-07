68796c08af28fd7f411a20ac114ee32709a268b0
Vasil Averyanau <VAveryanov8@users.noreply.github.com>
2025-02-07 09:28:15 +0100

    feat(1-1-restore): adds 1-1-restore cli and service stub (#4221)
    * feat(1-1-restore): adds new 1-1-restore sub command
    
    This introduces new cli sub command for the 1-1-restore
    
    Relates: #4200
    (cherry picked from commit 8436957aa44a644a6bae21d8198b57a4efce7bc6)

---
+++ pkg/command/one2onerestore/nodesmapping.go
@@ -0,0 +1,76 @@
+// Copyright (C) 2025 ScyllaDB
+
+package one2onerestore
+
+import (
+	"bufio"
+	"bytes"
+	"os"
+
+	"github.com/pkg/errors"
+)
+
+// implements pflag.Value interface.
+type nodesMapping []nodeMapping
+
+type nodeMapping struct {
+	Source node `json:"source"`
+	Target node `json:"target"`
+}
+
+type node struct {
+	DC     string `json:"dc"`
+	Rack   string `json:"rack_id"`
+	HostID string `json:"host_id"`
+}
+
+func (m *nodesMapping) Set(filePath string) error {
+	fd, err := os.Open(filePath)
+	if err != nil {
+		return errors.Wrap(err, "open a file")
+	}
+
+	scanner := bufio.NewScanner(fd)
+	scanner.Split(bufio.ScanLines)
+	for scanner.Scan() {
+		lineBytes := scanner.Bytes()
+		mappingParts := bytes.Split(lineBytes, []byte("="))
+		if len(mappingParts) != 2 {
+			return errors.New("source and target mappings must be split by '='")
+		}
+		sourceNode, err := parseNode(mappingParts[0])
+		if err != nil {
+			return err
+		}
+		targetNode, err := parseNode(mappingParts[1])
+		if err != nil {
+			return err
+		}
+		*m = append(*m, nodeMapping{
+			Source: sourceNode,
+			Target: targetNode,
+		})
+	}
+
+	return errors.Wrap(scanner.Err(), "scan lines")
+}
+
+func parseNode(rawNode []byte) (node, error) {
+	nodeParts := bytes.Split(rawNode, []byte(":"))
+	if len(nodeParts) != 3 {
+		return node{}, errors.New("invalid node format, must be <dc>:<rack>:<host_id>")
+	}
+	return node{
+		DC:     string(bytes.TrimSpace(nodeParts[0])),
+		Rack:   string(bytes.TrimSpace(nodeParts[1])),
+		HostID: string(bytes.TrimSpace(nodeParts[2])),
+	}, nil
+}
+
+func (m *nodesMapping) String() string {
+	return ""
+}
+
+func (m *nodesMapping) Type() string {
+	return "nodes-mapping"
+}
