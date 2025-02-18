51b7e6f6c97bf21188924d02c81d9dce9cc813fa
Vasil Averyanau <VAveryanov8@users.noreply.github.com>
2025-02-18 08:34:32 +0100

    feat(1-1-restore): adds copy data stage + loadSStables call (#4235)
    feat(1-1-restore): adds copy data stage + loadSStables call
    
    This adds copy data stage for the 1-1-restore with simple integration
    test. Data is copied by each node, table by table and after each table
    loadSStables is called.
    Note: integration tests with TABLETS=enabled will be skipped as 1-1-restore should only
    work with v-nodes.
    
    Refs:  #4202 #4203

---
+++ pkg/service/one2onerestore/service_integration_test.go
@@ -0,0 +1,95 @@
+// Copyright (C) 2025 ScyllaDB
+
+//go:build all || integration
+// +build all integration
+
+package one2onerestore
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/pkg/errors"
+	"github.com/scylladb/gocqlx/v2"
+	"github.com/scylladb/gocqlx/v2/qb"
+	. "github.com/scylladb/scylla-manager/v3/pkg/service/backup/backupspec"
+	. "github.com/scylladb/scylla-manager/v3/pkg/testutils"
+	. "github.com/scylladb/scylla-manager/v3/pkg/testutils/db"
+	. "github.com/scylladb/scylla-manager/v3/pkg/testutils/testconfig"
+)
+
+func TestOne2OneRestoreServiceIntegration(t *testing.T) {
+	if tablets := os.Getenv("TABLETS"); tablets == "enabled" {
+		t.Skip("1-1-restore is available only for v-nodes")
+	}
+	h := newTestHelper(t, ManagedClusterHosts())
+
+	clusterSession := CreateSessionAndDropAllKeyspaces(t, h.client)
+
+	ksName := "testrestore"
+	WriteData(t, clusterSession, ksName, 10)
+
+	srcCnt := rowCount(t, clusterSession, ksName, BigTableName)
+	if srcCnt == 0 {
+		t.Fatalf("Unexpected row count: 0")
+	}
+
+	Print("Run backup")
+	loc := []Location{testLocation("1-1-restore", "")}
+	S3InitBucket(t, loc[0].Path)
+	tag := h.runBackup(t, map[string]any{
+		"location": loc,
+	})
+
+	Print("Truncate tables")
+	truncateAllTablesInKeyspace(t, clusterSession, ksName)
+	if cnt := rowCount(t, clusterSession, ksName, BigTableName); cnt != 0 {
+		t.Fatalf("Unexpected row count: %d", cnt)
+	}
+
+	Print("Run 1-1-restore")
+	h.runRestore(t, map[string]any{
+		"location":          loc,
+		"snapshot_tag":      tag,
+		"source_cluster_id": h.clusterID,
+		"nodes_mapping":     getNodeMappings(t, h.client),
+	})
+
+	Print("Validate data")
+	dstCnt := rowCount(t, clusterSession, ksName, BigTableName)
+	if srcCnt != dstCnt {
+		t.Fatalf("Expected row count %d, but got %d", srcCnt, dstCnt)
+	}
+}
+
+func truncateAllTablesInKeyspace(tb testing.TB, session gocqlx.Session, ks string) {
+	tb.Helper()
+
+	q := qb.Select("system_schema.tables").Columns("table_name").Where(qb.Eq("keyspace_name")).Query(session).Bind(ks)
+	defer q.Release()
+
+	var all []string
+	if err := q.Select(&all); err != nil {
+		tb.Fatal(err)
+	}
+
+	for _, t := range all {
+		truncateTable(tb, session, ks, t)
+	}
+}
+
+func truncateTable(tb testing.TB, session gocqlx.Session, keyspace, table string) {
+	tb.Helper()
+
+	ExecStmt(tb, session, fmt.Sprintf("TRUNCATE TABLE %q.%q", keyspace, table))
+}
+
+func rowCount(t *testing.T, s gocqlx.Session, ks, tab string) int {
+	var cnt int
+	if err := s.Session.Query(fmt.Sprintf("SELECT COUNT(*) FROM %q.%q USING TIMEOUT 300s", ks, tab)).Scan(&cnt); err != nil {
+		t.Fatal(errors.Wrapf(err, "get table %s.%s row count", ks, tab))
+	}
+	Printf("%s.%s row count: %v", ks, tab, cnt)
+	return cnt
+}
---
+++ pkg/service/one2onerestore/worker_tables.go
@@ -0,0 +1,151 @@
+// Copyright (C) 2025 ScyllaDB
+
+package one2onerestore
+
+import (
+	"context"
+	"strings"
+	"time"
+
+	"github.com/pkg/errors"
+	"github.com/scylladb/scylla-manager/v3/pkg/scyllaclient"
+	"github.com/scylladb/scylla-manager/v3/pkg/service/backup/backupspec"
+	"github.com/scylladb/scylla-manager/v3/pkg/util/parallel"
+)
+
+func (w *worker) restoreTables(ctx context.Context, manifests []*backupspec.ManifestInfo, hosts []Host, nodeMappings []nodeMapping, keyspaces []string) error {
+	targetBySourceHostID, err := mapTargetHostToSource(hosts, nodeMappings)
+	if err != nil {
+		return errors.Wrap(err, "invalid node mapping")
+	}
+
+	logError := func(i int, err error) {
+		w.logger.Error(ctx, "Restore data", "err", err, "node_id", manifests[i].NodeID)
+	}
+	return parallel.Run(len(manifests), len(manifests), func(i int) error {
+		m := manifests[i]
+		h := targetBySourceHostID[m.NodeID]
+
+		mc, err := w.getManifestContent(ctx, h.Addr, m)
+		if err != nil {
+			return errors.Wrap(err, "manifest content")
+		}
+
+		const (
+			repeatInterval  = 10 * time.Second
+			pollIntervalSec = 10
+		)
+		return mc.ForEachIndexIterWithError(keyspaces, func(table backupspec.FilesMeta) error {
+			w.logger.Info(ctx, "Restoring data", "ks", table.Keyspace, "table", table.Table, "size", table.Size)
+
+			jobID, err := w.createDownloadJob(ctx, table, m, h)
+			if err != nil {
+				return errors.Wrapf(err, "create download job: %s.%s", table.Keyspace, table.Table)
+			}
+
+			if err := w.waitJob(ctx, h, jobID, pollIntervalSec); err != nil {
+				return errors.Wrapf(err, "wait job: %s.%s", table.Keyspace, table.Table)
+			}
+
+			if err := w.refreshNode(ctx, table, h, repeatInterval); err != nil {
+				return errors.Wrapf(err, "refresh node: %s.%s", table.Keyspace, table.Table)
+			}
+			return nil
+		})
+	}, logError)
+}
+
+func (w *worker) createDownloadJob(ctx context.Context, table backupspec.FilesMeta, m *backupspec.ManifestInfo, h Host) (int64, error) {
+	uploadDir := backupspec.UploadTableDir(table.Keyspace, table.Table, table.Version)
+	remoteDir := m.LocationSSTableVersionDir(table.Keyspace, table.Table, table.Version)
+	jobID, err := w.client.RcloneCopyPaths(ctx, h.Addr, scyllaclient.TransfersFromConfig, scyllaclient.NoRateLimit, uploadDir, remoteDir, table.Files)
+	if err != nil {
+		return 0, errors.Wrapf(err, "copy dir: %s", m.LocationSSTableVersionDir(table.Keyspace, table.Table, table.Version))
+	}
+	return jobID, nil
+}
+
+// Scylla operation might take a really long (and difficult to estimate) time.
+// This func exits ONLY on: success, context cancel or non-timeout related error.
+func (w *worker) refreshNode(ctx context.Context, table backupspec.FilesMeta, h Host, repeatInterval time.Duration) error {
+	running, err := w.client.LoadSSTables(ctx, h.Addr, table.Keyspace, table.Table, false, false)
+	if running || errContains(err, "timeout") {
+		w.logger.Info(ctx, "Waiting for SSTables loading to finish",
+			"host", h,
+			"error", err,
+		)
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case <-time.After(repeatInterval):
+		}
+		return w.refreshNode(ctx, table, h, repeatInterval)
+	}
+	return err
+}
+
+func errContains(err error, s string) bool {
+	if err == nil {
+		return false
+	}
+	return strings.Contains(err.Error(), s)
+}
+
+func (w *worker) waitJob(ctx context.Context, h Host, jobID int64, pollIntervalSec int) (err error) {
+	defer func() {
+		cleanCtx := context.Background()
+		// On error stop job
+		if err != nil {
+			w.stopJob(cleanCtx, jobID, h.Addr)
+		}
+		// On exit clear stats
+		w.clearJobStats(cleanCtx, jobID, h.Addr)
+	}()
+
+	for {
+		if ctx.Err() != nil {
+			return ctx.Err()
+		}
+
+		job, err := w.client.RcloneJobProgress(ctx, h.Addr, jobID, pollIntervalSec)
+		if err != nil {
+			return errors.Wrap(err, "fetch job info")
+		}
+
+		switch scyllaclient.RcloneJobStatus(job.Status) {
+		case scyllaclient.JobError:
+			return errors.Errorf("job error (%d): %s: host %s", jobID, job.Error, h.Addr)
+		case scyllaclient.JobSuccess:
+			w.logger.Info(ctx, "Job done",
+				"job_id", jobID,
+				"host", h,
+				"took", time.Time(job.CompletedAt).Sub(time.Time(job.StartedAt)),
+			)
+			return nil
+		case scyllaclient.JobRunning:
+			continue
+		case scyllaclient.JobNotFound:
+			return errors.New("job not found")
+		}
+	}
+}
+
+func (w *worker) clearJobStats(ctx context.Context, jobID int64, host string) {
+	if err := w.client.RcloneDeleteJobStats(ctx, host, jobID); err != nil {
+		w.logger.Error(ctx, "Failed to clear job stats",
+			"host", host,
+			"id", jobID,
+			"error", err,
+		)
+	}
+}
+
+func (w *worker) stopJob(ctx context.Context, jobID int64, host string) {
+	if err := w.client.RcloneJobStop(ctx, host, jobID); err != nil {
+		w.logger.Error(ctx, "Failed to stop job",
+			"host", host,
+			"id", jobID,
+			"error", err,
+		)
+	}
+}
