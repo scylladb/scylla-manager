df7b7732984d9cc66a97f73d9e6bd5c61bf52896
Michał Leszczyński <2000michal@wp.pl>
2025-02-27 14:32:17 +0100

    feat(repair): use tablet repair API
    Scylla 2025.1.0 is introducing new tablet repair API.
    SM should use it for repairing entire tablet tables
    with a single API call. Unfortunately, in 2025.1.0
    this API won't support host filtering, so we still
    need to use the old repair API for partial tablet
    table repairs.
    
    Fixes #4188

--- pkg/service/repair/plan.go
+++ pkg/service/repair/plan.go
@@ -19,10 +19,10 @@ import (
 type plan struct {
 	Keyspaces keyspacePlans
 
-	Hosts                []string
-	MaxParallel          int
-	MaxHostIntensity     map[string]Intensity
-	SmallTableOptSupport bool
+	Hosts            []string
+	MaxParallel      int
+	MaxHostIntensity map[string]Intensity
+	apiSupport       apiSupport
 	// Used for progress purposes
 	Stats map[scyllaclient.HostKeyspaceTable]tableStats
 }
@@ -43,6 +43,7 @@ type tablePlan struct {
 	RangesCnt     int
 	ReplicaSetCnt int
 	Small         bool
+	FullRepair    bool // Is an entire table is going to be repaired
 }
 
 type tableStats struct {
@@ -82,8 +83,12 @@ func newPlan(ctx context.Context, target Target, client *scyllaclient.Client) (*
 			// Update ranges and hosts
 			rangesCnt := 0
 			replicaSetCnt := 0
+			fullRepair := true
 			for _, rep := range ring.ReplicaTokens {
 				filtered := filterReplicaSet(rep.ReplicaSet, ring.HostDC, target)
+				if len(filtered) != len(rep.ReplicaSet) {
+					fullRepair = false
+				}
 				if len(filtered) == 0 {
 					continue
 				}
@@ -101,6 +106,7 @@ func newPlan(ctx context.Context, target Target, client *scyllaclient.Client) (*
 				Table:         t,
 				ReplicaSetCnt: replicaSetCnt,
 				RangesCnt:     rangesCnt,
+				FullRepair:    fullRepair,
 			})
 		}
 
@@ -124,7 +130,7 @@ func newPlan(ctx context.Context, target Target, client *scyllaclient.Client) (*
 	}
 	ks.fillSmall(target.SmallTableThreshold)
 
-	support, err := isSmallTableOptSupported(ctx, client, hosts)
+	support, err := getRepairAPISupport(ctx, client, hosts)
 	if err != nil {
 		return nil, errors.Wrap(err, "check support of small_table_optimization")
 	}
@@ -136,12 +142,12 @@ func newPlan(ctx context.Context, target Target, client *scyllaclient.Client) (*
 	}
 
 	return &plan{
-		Keyspaces:            ks,
-		Hosts:                hosts,
-		MaxParallel:          maxP,
-		MaxHostIntensity:     mhi,
-		SmallTableOptSupport: support,
-		Stats:                newStats(sizeReport, ranges),
+		Keyspaces:        ks,
+		Hosts:            hosts,
+		MaxParallel:      maxP,
+		MaxHostIntensity: mhi,
+		apiSupport:       support,
+		Stats:            newStats(sizeReport, ranges),
 	}, nil
 }
 
@@ -368,9 +374,22 @@ func newHostKsTable(host, ks, table string) scyllaclient.HostKeyspaceTable {
 	}
 }
 
-func isSmallTableOptSupported(ctx context.Context, client *scyllaclient.Client, hosts []string) (bool, error) {
-	out := atomic.Bool{}
-	out.Store(true)
+// apiSupport describes the support for optimized repair API
+// calls which are not present in all supported Scylla versions.
+type apiSupport struct {
+	// If /storage_service/repair_async/{keyspace} API supports
+	// 'small_table_optimization' query param.
+	smallTableRepair bool
+	// If /storage_service/tablets/repair API is exposed.
+	// It might not necessarily allow for host filtering.
+	tabletRepairNoHostFiltering bool
+}
+
+func getRepairAPISupport(ctx context.Context, client *scyllaclient.Client, hosts []string) (apiSupport, error) {
+	smallTableOpt := atomic.Bool{}
+	smallTableOpt.Store(true)
+	fullTabletTableOpt := atomic.Bool{}
+	fullTabletTableOpt.Store(true)
 	eg := errgroup.Group{}
 
 	for _, host := range hosts {
@@ -380,19 +399,32 @@ func isSmallTableOptSupported(ctx context.Context, client *scyllaclient.Client, 
 			if err != nil {
 				return err
 			}
+
 			res, err := ni.SupportsRepairSmallTableOptimization()
 			if err != nil {
 				return err
 			}
 			if !res {
-				out.Store(false)
+				smallTableOpt.Store(false)
 			}
+
+			res, err = ni.SupportsTabletRepairNoHostFiltering()
+			if err != nil {
+				return err
+			}
+			if !res {
+				fullTabletTableOpt.Store(false)
+			}
+
 			return nil
 		})
 	}
 
 	if err := eg.Wait(); err != nil {
-		return false, err
+		return apiSupport{}, err
 	}
-	return out.Load(), nil
+	return apiSupport{
+		smallTableRepair:            smallTableOpt.Load(),
+		tabletRepairNoHostFiltering: fullTabletTableOpt.Load(),
+	}, nil
 }
