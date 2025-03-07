9b6f77d657062de4cf3378469deae29c9816ba4e
Michal-Leszczynski <74614433+Michal-Leszczynski@users.noreply.github.com>
2025-03-07 14:06:44 +0100

    Tablet repair API (#4274)
    * feat(scyllaclient): add methods for managing tablet repair API
    
    * feat(repair): use tablet repair API
    
    Scylla 2025.1.0 is introducing new tablet repair API.
    SM should use it for repairing entire tablet tables
    with a single API call. Unfortunately, in 2025.1.0
    this API won't support host filtering, so we still
    need to use the old repair API for partial tablet
    table repairs.
    
    Fixes #4188
    
    * feat(repair): don't disable tablet load balancing from Scylla 2025.1.0
    
    Scylla 2025.1.0 is introducing new tablet repair API which does not
    require stopping tablet load balancing during the repair.
    Unfortunately, host filtering for this API will be added in 2025.1.1,
    so we can't use this API for partial tablet tables repair.
    
    As Scylla 2025.1 is aiming to utilize ~90% of disk, disabling tablet
    load balancing for an extended period of time (e.g. repair) on a
    running cluster might result in some nodes running out of disk space.
    
    Since skipping repair of some tablets does not lead to data resurrection
    (because of tombstone_gc mode repair), it's better to not stop tablet
    load balancing during repair with Scylla 2025.1.0 even when partially
    repairing tablet table with the old API.
    
    Ref #4273
    
    fix(repair): typo
    
    Co-authored-by: Yaniv Kaul <yaniv.kaul@scylladb.com>
    
    * chore(repair_test): update repair API parsing with tablet repair
    
    This way we don't need to modify currently existing tests
    so that they work with the new tablet repair API.
    
    * feat(repair_test): test tablet repair API and tablet load balancing usage
    
    * chore(restore_test): update repair API parsing with tablet repair
    
    * chore(repair_test): adjust tests for tablet repair API
    
    * feat(repair): abort tablet repair tasks on error
    
    As tablet repair tasks are handled by the Task Manager API,
    we need abort them through Task Manager API as well.
    The "/storage_service/force_terminate" is not enough,
    as the tablet repair tasks would still be rescheduled
    after they are terminated.
    
    ---------
    
    Co-authored-by: Yaniv Kaul <yaniv.kaul@scylladb.com>
    (cherry picked from commit 76b118d7d6eb366764e89cd446bbaecaeb38f2f2)

--- pkg/service/repair/service_repair_integration_test.go
+++ pkg/service/repair/service_repair_integration_test.go
@@ -987,9 +987,10 @@ func TestServiceRepairIntegration(t *testing.T) {
 	h := newRepairTestHelper(t, session, defaultConfig())
 	clusterSession := CreateSessionAndDropAllKeyspaces(t, h.Client)
 
-	createDefaultKeyspace(t, clusterSession, "test_repair", 2, 2)
-	WriteData(t, clusterSession, "test_repair", 1, "test_table_0", "test_table_1")
-	defer dropKeyspace(t, clusterSession, "test_repair")
+	const testKs = "test_repair"
+	createDefaultKeyspace(t, clusterSession, testKs, 2, 2)
+	WriteData(t, clusterSession, testKs, 1, "test_table_0", "test_table_1")
+	defer dropKeyspace(t, clusterSession, testKs)
 
 	t.Run("repair simple", func(t *testing.T) {
 		h := newRepairTestHelper(t, session, defaultConfig())
@@ -1156,7 +1157,7 @@ func TestServiceRepairIntegration(t *testing.T) {
 		defer cancel()
 
 		Print("When: run repair")
-		h.Hrt.SetInterceptor(repairMockAndBlockInterceptor(t, ctx, 2))
+		h.Hrt.SetInterceptor(repairMockAndBlockInterceptor(t, ctx, 1))
 		h.runRepair(ctx, multipleUnits(map[string]any{
 			"small_table_threshold": repairAllSmallTableThreshold,
 		}))
@@ -1436,6 +1437,11 @@ func TestServiceRepairIntegration(t *testing.T) {
 		ctx, cancel := context.WithCancel(context.Background())
 		defer cancel()
 
+		rd := scyllaclient.NewRingDescriber(ctx, h.Client)
+		if rd.IsTabletKeyspace(testKs) {
+			t.Skip("Test expects vnode keyspace: tablet tables are repaired with a single API call or they don't support resume")
+		}
+
 		Print("When: run repair")
 		holdCtx, holdCancel := context.WithCancel(ctx)
 		h.Hrt.SetInterceptor(repairMockAndBlockInterceptor(t, holdCtx, 1))
@@ -1475,18 +1481,17 @@ func TestServiceRepairIntegration(t *testing.T) {
 		defer cancel()
 
 		Print("When: run repair")
-		holdCtx, holdCancel := context.WithCancel(context.Background())
-		h.Hrt.SetInterceptor(repairMockAndBlockInterceptor(t, holdCtx, 1))
+		i, running := repairRunningInterceptor()
+		h.Hrt.SetInterceptor(i)
 		h.runRepair(ctx, multipleUnits(map[string]any{
 			"small_table_threshold": repairAllSmallTableThreshold,
 		}))
 
 		Print("Then: repair is running")
-		h.assertRunning(shortWait)
+		chanClosedWithin(t, running, shortWait)
 
 		Print("And: no network for 5s with 1s backoff")
 		h.Hrt.SetInterceptor(dialErrorInterceptor())
-		holdCancel()
 		time.AfterFunc(2*h.Client.Config().Timeout, func() {
 			h.Hrt.SetInterceptor(repairMockInterceptor(t, repairStatusDone))
 		})
@@ -1661,12 +1666,12 @@ func TestServiceRepairIntegration(t *testing.T) {
 			props["fail_fast"] = true
 
 			Print("When: run repair")
-			holdCtx, holdCancel := context.WithCancel(context.Background())
-			h.Hrt.SetInterceptor(repairMockAndBlockInterceptor(t, holdCtx, 2))
+			i, running := repairRunningInterceptor()
+			h.Hrt.SetInterceptor(i)
 			h.runRepair(ctx, props)
 
 			Print("When: repair is running")
-			h.assertRunning(longWait)
+			chanClosedWithin(t, running, shortWait)
 
 			Print("When: Scylla returns failures")
 			var killRepairCalled int32
@@ -1674,7 +1679,6 @@ func TestServiceRepairIntegration(t *testing.T) {
 				countInterceptor(&killRepairCalled, isForceTerminateRepairReq),
 				repairMockInterceptor(t, repairStatusFailed),
 			))
-			holdCancel()
 
 			Print("Then: repair finish with error")
 			h.assertError(longWait)
@@ -2110,6 +2114,111 @@ func TestServiceRepairIntegration(t *testing.T) {
 
 		Print("Then: not-batched repair is done")
 		h.assertDone(longWait)
+	})
+
+	t.Run("tablet repair API", func(t *testing.T) {
+		if ok, err := globalNodeInfo.SupportsTabletRepairNoHostFiltering(); err != nil {
+			t.Fatal(err)
+		} else if !ok {
+			t.Skip("This test is expects that tablet repair API is exposed")
+		}
+
+		h := newRepairTestHelper(t, session, defaultConfig())
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		const (
+			multiDCTabletKs  = "test_repair_multi_dc_tablet_ks"
+			singleDCTabletKS = "test_repair_single_dc_tablet_ks"
+			vnodeKs          = "test_repair_vnode_ks"
+			t1               = "test_table_1"
+			t2               = "test_table_2"
+		)
+		testCases := []struct {
+			ks         string
+			tab        []string
+			api        string
+			singleCall bool
+		}{
+			{
+				ks:         multiDCTabletKs,
+				tab:        []string{t1, t2},
+				api:        repairAsyncEndpoint,
+				singleCall: false,
+			},
+			{
+				ks:         singleDCTabletKS,
+				tab:        []string{t1, t2},
+				api:        tabletRepairEndpoint,
+				singleCall: true,
+			},
+			{
+				ks:         vnodeKs,
+				tab:        []string{t1, t2},
+				api:        repairAsyncEndpoint,
+				singleCall: false,
+			},
+		}
+
+		Print("When: prepare keyspaces")
+		createTabletKeyspace(t, clusterSession, multiDCTabletKs, 2, 2)
+		WriteData(t, clusterSession, multiDCTabletKs, 1, t1, t2)
+		defer dropKeyspace(t, clusterSession, multiDCTabletKs)
+
+		createTabletKeyspace(t, clusterSession, singleDCTabletKS, 2, 0)
+		WriteData(t, clusterSession, singleDCTabletKS, 1, t1, t2)
+		defer dropKeyspace(t, clusterSession, singleDCTabletKS)
+
+		createVnodeKeyspace(t, clusterSession, vnodeKs, 2, 2)
+		WriteData(t, clusterSession, vnodeKs, 1, t1, t2)
+		defer dropKeyspace(t, clusterSession, vnodeKs)
+
+		tabRepairAPI := make(map[string]string)
+		tabCallCnt := make(map[string]int)
+		mu := sync.Mutex{}
+		h.Hrt.SetInterceptor(httpx.RoundTripperFunc(func(req *http.Request) (*http.Response, error) {
+			if enabled, ok := newTabletLoadBalancingReq(t, req); ok {
+				if !enabled {
+					t.Error("Disabled tablet load balancing when tablet repair API is exposed")
+				}
+			}
+			if r, ok := parseRepairReq(t, req); ok {
+				mu.Lock()
+				tabCallCnt[r.fullTable()]++
+				if api, ok := tabRepairAPI[r.fullTable()]; ok {
+					if api != req.URL.Path {
+						t.Error("Mixing repair API for the same table")
+					}
+				} else {
+					tabRepairAPI[r.fullTable()] = req.URL.Path
+				}
+				mu.Unlock()
+			}
+			return nil, nil
+		}))
+
+		Print("When: run dc1 repair")
+		h.runRepair(ctx, map[string]any{
+			"keyspace":              []string{multiDCTabletKs, singleDCTabletKS, vnodeKs},
+			"dc":                    []string{"dc1"},
+			"small_table_threshold": repairAllSmallTableThreshold,
+		})
+
+		Print("Then: repair is done")
+		h.assertDone(longWait)
+
+		Print("Then: validate used repair API")
+		for _, tc := range testCases {
+			for _, tab := range tc.tab {
+				fn := tc.ks + "." + tab
+				if api := tabRepairAPI[fn]; !strings.HasPrefix(api, tc.api) {
+					t.Errorf("Table %q: expected API %q, got %q", fn, tc.api, api)
+				}
+				if cnt := tabCallCnt[fn]; cnt <= 0 || ((cnt == 1) != tc.singleCall) {
+					t.Errorf("Table %q: expected single_call=%v, got %d", fn, tc.singleCall, cnt)
+				}
+			}
+		}
 	})
 }
 
