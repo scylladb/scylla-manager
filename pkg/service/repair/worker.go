d136e460421a4c86630460e02c3d7abe22bdb79a
Michał Leszczyński <2000michal@wp.pl>
2025-03-11 15:19:34 +0100

    feat(repair): use tablet repair API
    Scylla 2025.1.0 is introducing new tablet repair API.
    SM should use it for repairing tablet tables with a single API call.
    
    Fixes #4188

--- pkg/service/repair/worker.go
+++ pkg/service/repair/worker.go
@@ -14,6 +14,8 @@ import (
 )
 
 type worker struct {
+	config Config
+	target Target
 	client *scyllaclient.Client
 	// Marks tables for which handleRunningStatus didn't have any effect.
 	// We want to limit the usage of handleRunningStatus to once per table
@@ -60,7 +62,9 @@ func (w *worker) runRepair(ctx context.Context, j job) (out error) {
 
 	var ranges []scyllaclient.TokenRange
 	switch {
-	case j.jobType == optimizeJobType:
+	case j.jobType == tabletJobType:
+		return w.fullTabletTableRepair(ctx, j.keyspace, j.table, j.master)
+	case j.jobType == smallTableJobType:
 		ranges = nil
 	case j.jobType == mergeRangesJobType:
 		ranges = []scyllaclient.TokenRange{
@@ -73,7 +77,7 @@ func (w *worker) runRepair(ctx context.Context, j job) (out error) {
 		ranges = j.ranges
 	}
 
-	jobID, err = w.client.Repair(ctx, j.keyspace, j.table, j.master, j.replicaSet, ranges, j.intensity, j.jobType == optimizeJobType)
+	jobID, err = w.client.Repair(ctx, j.keyspace, j.table, j.master, j.replicaSet, ranges, j.intensity, j.jobType == smallTableJobType)
 	if err != nil {
 		return errors.Wrap(err, "schedule repair")
 	}
@@ -161,4 +165,63 @@ func (w *worker) isTableDeleted(ctx context.Context, j job) bool {
 		return false
 	}
 	return !exists
+}
+
+func (w *worker) fullTabletTableRepair(ctx context.Context, keyspace, table, host string) error {
+	hostFilter, err := w.hostFilter(ctx)
+	if err != nil {
+		return errors.Wrap(err, "create host filter")
+	}
+
+	id, err := w.client.TabletRepair(ctx, keyspace, table, host, w.target.DC, hostFilter)
+	if err != nil {
+		return errors.Wrap(err, "schedule tablet repair task")
+	}
+
+	w.logger.Info(ctx, "Repairing entire tablet table",
+		"keyspace", keyspace,
+		"table", table,
+		"task ID", id,
+	)
+
+	for {
+		status, err := w.client.ScyllaWaitTask(ctx, host, id, int64(w.config.LongPollingTimeoutSeconds))
+		if err != nil {
+			w.scyllaAbortTask(host, id)
+			return errors.Wrap(err, "get tablet repair task status")
+		}
+
+		switch scyllaclient.ScyllaTaskState(status.State) {
+		case scyllaclient.ScyllaTaskStateDone:
+			return nil
+		case scyllaclient.ScyllaTaskStateFailed:
+			return errors.Errorf("tablet repair task finished with status %q", scyllaclient.ScyllaTaskStateFailed)
+		case scyllaclient.ScyllaTaskStateCreated, scyllaclient.ScyllaTaskStateRunning:
+			continue
+		default:
+			w.scyllaAbortTask(host, id)
+			return errors.Errorf("unexpected tablet repair task status %q", status.State)
+		}
+	}
+}
+
+func (w *worker) hostFilter(ctx context.Context) ([]string, error) {
+	if len(w.target.IgnoreHosts) == 0 {
+		return nil, nil
+	}
+	status, err := w.client.Status(ctx)
+	if err != nil {
+		return nil, errors.Wrap(err, "get status")
+	}
+	return status.Up().HostIDs(), nil
+}
+
+func (w *worker) scyllaAbortTask(host, id string) {
+	if err := w.client.ScyllaAbortTask(context.Background(), host, id); err != nil {
+		w.logger.Error(context.Background(), "Failed to abort task",
+			"host", host,
+			"id", id,
+			"error", err,
+		)
+	}
 }
