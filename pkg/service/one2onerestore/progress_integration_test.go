a5dcbbdccf5c5fb15e397359b5f9bed0a18eaed5
Vasil Averyanau <vasil.averyanau@scylladb.com>
2025-04-15 11:42:47 +0200

    test(1-1-restore): adjusts tests for 1-1-restore progress

--- pkg/service/one2onerestore/progress_integration_test.go
+++ pkg/service/one2onerestore/progress_integration_test.go
@@ -11,11 +11,9 @@ import (
 	"testing"
 
 	"github.com/scylladb/scylla-manager/backupspec"
-	"github.com/scylladb/scylla-manager/v3/pkg/scyllaclient"
 	"github.com/scylladb/scylla-manager/v3/pkg/testutils"
 	"github.com/scylladb/scylla-manager/v3/pkg/testutils/db"
 	"github.com/scylladb/scylla-manager/v3/pkg/testutils/testconfig"
-	"github.com/scylladb/scylla-manager/v3/pkg/util/timeutc"
 	"github.com/scylladb/scylla-manager/v3/pkg/util/uuid"
 )
 
@@ -65,6 +63,24 @@ func TestGetProgressIntegration(t *testing.T) {
 	if err != nil {
 		t.Fatalf("Unexpected err, prepareHostWorkload: %v", err)
 	}
+	pr, err := w.getProgress(context.Background())
+	if err != nil {
+		t.Fatalf("Unexpected err, getProgress: %v", err)
+	}
+	if !progressIsEmpty(pr) {
+		t.Fatalf("Expected empty progress, but got: %v", pr)
+	}
+
+	w.initProgress(context.Background(), workload)
+
+	pr, err = w.getProgress(context.Background())
+	if err != nil {
+		t.Fatalf("Unexpected err, getProgress: %v", err)
+	}
+	if progressIsEmpty(pr) {
+		t.Fatalf("Expected not empty progress, but got empty")
+	}
+	expectProgressStatus(t, pr, ProgressStatusNotStarted)
 
 	testutils.Print("ALTER_TGC Stage")
 	err = w.setTombstoneGCModeRepair(context.Background(), workload)
@@ -72,78 +88,90 @@ func TestGetProgressIntegration(t *testing.T) {
 		t.Fatalf("Unexpected err, setTombstoneGCModeRepair: %v", err)
 	}
 
-	pr, err := w.getProgress(context.Background(), target)
-	if err != nil {
-		t.Fatalf("Unexpected err, getProgress: %v", err)
-	}
-	if pr.Stage != StageAlterTGC {
-		t.Fatalf("Expected stage ALTER_TGC, but got %s", pr.Stage)
-	}
-	validateKeyspaces(t, pr.Keyspaces)
-
 	testutils.Print("DROP_VIEWS Stage")
 	views, err := w.dropViews(context.Background(), workload)
 	if err != nil {
 		t.Fatalf("Unexpected err, dropViews: %v", err)
 	}
 
-	pr, err = w.getProgress(context.Background(), target)
-	if err != nil {
-		t.Fatalf("Unexpected err, getProgress: %v", err)
-	}
-	if pr.Stage != StageDropViews {
-		t.Fatalf("Expected stage DROP_VIEW, but got %s", pr.Stage)
-	}
-	validateViews(t, pr.Views, "")
-
 	testutils.Print("DATA Stage")
 	if err := w.restoreTables(context.Background(), workload, target.Keyspace); err != nil {
 		t.Fatalf("Unexpected err, restoreTables: %v", err)
 	}
-	pr, err = w.getProgress(context.Background(), target)
+
+	pr, err = w.getProgress(context.Background())
 	if err != nil {
 		t.Fatalf("Unexpected err, getProgress: %v", err)
 	}
-	if pr.Stage != StageData {
-		t.Fatalf("Expected stage DATA, but got %s", pr.Stage)
+	if progressIsEmpty(pr) {
+		t.Fatalf("Expected not empty progress, but got empty")
 	}
-	validateKeyspaces(t, pr.Keyspaces)
-	validateHosts(t, pr.Hosts)
+	expectTablesStatus(t, pr.Tables, ProgressStatusDone)
+	expectViewsStatus(t, pr.Views, ProgressStatusNotStarted)
 
 	testutils.Print("RECREATE_VIEWS Stage")
 	if err := w.reCreateViews(context.Background(), views); err != nil {
 		t.Fatalf("Unexpected err, reCreateViews: %v", err)
 	}
-	pr, err = w.getProgress(context.Background(), target)
-	if err != nil {
-		t.Fatalf("Unexpected err, getProgress: %v", err)
-	}
-	if pr.Stage != StageRecreateViews {
-		t.Fatalf("Expected stage RECREATE_VIEW, but got %s", pr.Stage)
-	}
-	validateViews(t, pr.Views, scyllaclient.StatusSuccess)
 
-	testutils.Print("DONE Stage")
-	w.progressDone(context.Background(), timeutc.Now(), timeutc.Now())
-	pr, err = w.getProgress(context.Background(), target)
+	pr, err = w.getProgress(context.Background())
 	if err != nil {
 		t.Fatalf("Unexpected err, getProgress: %v", err)
 	}
-	validateGetProgress(t, pr, snapshotTag)
+	if progressIsEmpty(pr) {
+		t.Fatalf("Expected not empty progress, but got empty")
+	}
+	expectProgressStatus(t, pr, ProgressStatusDone)
 }
 
-func validateGetProgress(t *testing.T, pr Progress, tag string) {
+func progressIsEmpty(pr Progress) bool {
+	return len(pr.Tables) == 0 && len(pr.Views) == 0
+}
+
+func expectProgressStatus(t *testing.T, pr Progress, status ProgressStatus) {
 	t.Helper()
-	if pr.SnapshotTag != tag {
-		t.Fatalf("Expected snapshot tag %s, but got %s", tag, pr.SnapshotTag)
+	expectTablesStatus(t, pr.Tables, status)
+	expectViewsStatus(t, pr.Views, status)
+}
+
+func expectTablesStatus(t *testing.T, tables []TableProgress, status ProgressStatus) {
+	t.Helper()
+	for _, tp := range tables {
+		if tp.Status != status {
+			t.Fatalf("Expected table %q status, but got %q", status, tp.Status)
+		}
 	}
-	if pr.Stage != StageDone {
-		t.Fatalf("Expected stage DONE, but got %s", pr.Stage)
+}
+
+func expectViewsStatus(t *testing.T, views []ViewProgress, status ProgressStatus) {
+	t.Helper()
+	for _, vp := range views {
+		if vp.Status != status {
+			t.Fatalf("Expected view %q status, but got %q", status, vp.Status)
+		}
 	}
-	validateProgress(t, pr.progress)
-	validateKeyspaces(t, pr.Keyspaces)
-	validateHosts(t, pr.Hosts)
-	validateViews(t, pr.Views, scyllaclient.StatusSuccess)
+}
+
+func validateGetProgress(t *testing.T, pr Progress) {
+	t.Helper()
+	validateTablesProgress(t, pr.Tables)
+	validateViewsProgress(t, pr.Views)
+
+}
+
+func validateTablesProgress(t *testing.T, tables []TableProgress) {
+	t.Helper()
+	for _, tp := range tables {
+		validateProgress(t, tp.progress)
+	}
+}
+
+func validateViewsProgress(t *testing.T, views []ViewProgress) {
+	t.Helper()
+
+	for _, vp := range views {
+		validateProgress(t, vp.progress)
+	}
 }
 
 func validateProgress(t *testing.T, p progress) {
@@ -151,86 +179,13 @@ func validateProgress(t *testing.T, p progress) {
 	if p.Size != p.Restored {
 		t.Fatalf("Expected to restore %d, but restored %d", p.Size, p.Restored)
 	}
-	if p.Size != p.Downloaded {
-		t.Fatalf("Expected to download %d, but got %d", p.Size, p.Downloaded)
-	}
-	if p.Failed != 0 {
-		t.Fatalf("Expected failed to be 0, but got: %d", p.Failed)
-	}
 	if p.StartedAt == nil {
 		t.Fatalf("Expected StartedAt != nil, but got nil")
 	}
 	if p.CompletedAt == nil {
 		t.Fatalf("Expected CompletedAt != nil, but got nil")
 	}
-}
-
-func validateKeyspaces(t *testing.T, keyspaces []KeyspaceProgress) {
-	t.Helper()
-	if len(keyspaces) == 0 {
-		t.Fatalf("Expected len(keyspaces) > 0, but got 0")
-	}
-	for _, kp := range keyspaces {
-		validateProgress(t, kp.progress)
-		if kp.Keyspace == "" {
-			t.Fatalf("Expected not empty keyspace name, but got ''")
-		}
-		if len(kp.Tables) == 0 {
-			t.Fatalf("Expected %s len(tables) > 0, but got 0", kp.Keyspace)
-		}
-		for _, tp := range kp.Tables {
-			validateProgress(t, tp.progress)
-			if tp.Table == "" {
-				t.Fatalf("Expected not empty %s table name, but got ''", kp.Keyspace)
-			}
-			if tp.TombstoneGC != modeRepair {
-				t.Fatalf("Expected tombstone_gc mode of %s.%s to be repair, but got %s", kp.Keyspace, tp.Table, tp.TombstoneGC)
-			}
-		}
-	}
-}
-
-func validateHosts(t *testing.T, hosts []HostProgress) {
-	t.Helper()
-	if len(hosts) != 6 {
-		t.Fatalf("Expected len(hosts) == 6, but got %d", len(hosts))
-	}
-	for _, hp := range hosts {
-		if hp.Host == "" {
-			t.Fatalf("Expected not empty host, but got ''")
-		}
-		if hp.ShardCnt == 0 {
-			t.Fatalf("Expected %s host ShardCound != 0, but got 0", hp.Host)
-		}
-		if hp.DownloadedBytes == 0 {
-			t.Fatalf("Expected not empty %s host DownloadedBytes, but got 0", hp.Host)
-		}
-		if hp.DownloadDuration == 0 {
-			t.Fatalf("Expected not empty %s host DownloadDuration, but got 0", hp.Host)
-		}
-	}
-}
-
-func validateViews(t *testing.T, views []View, expectedBuildStatus scyllaclient.ViewBuildStatus) {
-	t.Helper()
-	if len(views) == 0 {
-		t.Fatalf("Expected len(views) > 0, but got 0")
-	}
-	for _, v := range views {
-		if v.Keyspace == "" {
-			t.Fatalf("Expected not empty view.Keyspace, but got ''")
-		}
-		if v.BaseTable == "" {
-			t.Fatalf("Expected not empty view.BaseTable, but got ''")
-		}
-		if v.View == "" {
-			t.Fatalf("Expected not empty view.View, but got ''")
-		}
-		if v.Type == "" {
-			t.Fatalf("Expected not empty view.Type, but got ''")
-		}
-		if v.BuildStatus != expectedBuildStatus {
-			t.Fatalf("Expected view.BuildStatus '%s', but got '%s'", expectedBuildStatus, v.BuildStatus)
-		}
+	if p.Status != ProgressStatusDone {
+		t.Fatalf("Expected status %q, but got %q", ProgressStatusDone, p.Status)
 	}
 }
--- pkg/service/one2onerestore/progress_test.go
+++ pkg/service/one2onerestore/progress_test.go
@@ -16,73 +16,66 @@ import (
 
 func TestAggregateProgress(t *testing.T) {
 	testCases := []struct {
-		name        string
-		rows        string
-		snapshotTag string
-		expected    Progress
+		name      string
+		tableRows string
+		viewRows  string
+		expected  Progress
 	}{
 		{
-			name:        "Only data stage",
-			rows:        "testdata/only_data.json",
-			snapshotTag: "snapshot_tag",
+			name:      "Only tables progress",
+			tableRows: "testdata/1.run_table_progress.json",
+			viewRows:  "testdata/1.run_view_progress.json",
 		},
 		{
-			name:        "Only alter tombstone_gc mode stage",
-			rows:        "testdata/only_tgc.json",
-			snapshotTag: "snapshot_tag",
+			name:      "Only views progress",
+			tableRows: "testdata/2.run_table_progress.json",
+			viewRows:  "testdata/2.run_view_progress.json",
 		},
 		{
-			name:        "Only drop views stage",
-			rows:        "testdata/only_drop_views.json",
-			snapshotTag: "snapshot_tag",
+			name:      "Tables and views progress, status not started",
+			tableRows: "testdata/3.run_table_progress.json",
+			viewRows:  "testdata/3.run_view_progress.json",
 		},
 		{
-			name:        "Data and drop views stage",
-			rows:        "testdata/data_and_drop_views.json",
-			snapshotTag: "snapshot_tag",
+			name:      "Tables and views progress, status in progress",
+			tableRows: "testdata/4.run_table_progress.json",
+			viewRows:  "testdata/4.run_view_progress.json",
 		},
 		{
-			name:        "Data and recreate views stage",
-			rows:        "testdata/data_and_recreate_views.json",
-			snapshotTag: "snapshot_tag",
+			name:      "Tables and views progress, status done",
+			tableRows: "testdata/5.run_table_progress.json",
+			viewRows:  "testdata/5.run_view_progress.json",
 		},
 		{
-			name:        "All stages",
-			rows:        "testdata/all_stages.json",
-			snapshotTag: "snapshot_tag",
-		},
-		{
-			name:        "Data stage in progress",
-			rows:        "testdata/data_stage_in_progress.json",
-			snapshotTag: "snapshot_tag",
+			name:      "Tables and views progress, status mixed",
+			tableRows: "testdata/6.run_table_progress.json",
+			viewRows:  "testdata/6.run_view_progress.json",
 		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			iter, stop := newDBTest(t, tc.rows)
+			tableIter, stop := newDBTest[RunTableProgress](t, tc.tableRows)
 			defer stop()
+			viewIter, viewStop := newDBTest[RunViewProgress](t, tc.viewRows)
+			defer viewStop()
 
-			pr := (&worker{}).aggregateProgress(iter, tc.snapshotTag)
+			pr := (&worker{}).aggregateProgress(tableIter, viewIter)
 
 			testutils.SaveGoldenJSONFileIfNeeded(t, &pr)
 			var expectedProgress Progress
 			testutils.LoadGoldenJSONFile(t, &expectedProgress)
 
-			sortKeyspacesOpt := cmpopts.SortSlices(func(a, b KeyspaceProgress) bool {
-				return a.Keyspace < b.Keyspace
+			unexportedOpts := cmp.AllowUnexported(TableProgress{}, ViewProgress{})
+			sortTables := cmpopts.SortSlices(func(a, b TableProgress) bool {
+				return a.Keyspace <= b.Keyspace && a.Table <= b.Table
 			})
-			sortTablesOpt := cmpopts.SortSlices(func(a, b TableProgress) bool {
-				return a.Table < b.Table
+			sortViews := cmpopts.SortSlices(func(a, b ViewProgress) bool {
+				return a.Keyspace <= b.Keyspace && a.Table <= b.Table
 			})
-			sortHostsOpt := cmpopts.SortSlices(func(a, b HostProgress) bool {
-				return a.Host < b.Host
-			})
-			unexportedOpts := cmp.AllowUnexported(Progress{}, KeyspaceProgress{}, TableProgress{})
 			opts := []cmp.Option{
-				sortKeyspacesOpt,
-				sortTablesOpt,
-				sortHostsOpt,
+				sortTables,
+				sortViews,
 				unexportedOpts,
 			}
 			if diff := cmp.Diff(expectedProgress, pr, opts...); diff != "" {
@@ -92,28 +85,32 @@ func TestAggregateProgress(t *testing.T) {
 	}
 }
 
-func newDBTest(t *testing.T, dataPath string) (it *dbIterTest, stop func()) {
+type runProgress interface {
+	RunTableProgress | RunViewProgress
+}
+
+func newDBTest[T runProgress](t *testing.T, dataPath string) (it *dbIterTest[T], stop func()) {
 	t.Helper()
 	data, err := os.ReadFile(dataPath)
 	if err != nil {
 		t.Fatalf("open test data file: %v", err)
 	}
-	var rows []RunProgress
+	var rows []T
 	if err := json.Unmarshal(data, &rows); err != nil {
-		t.Fatalf("unmarshal test data file: %v", err)
+		t.Fatalf("unmarshal test data file: %s: %v", dataPath, err)
 	}
 	next, stop := iter.Pull(slices.Values(rows))
-	return &dbIterTest{
+	return &dbIterTest[T]{
 		next: next,
 	}, stop
 }
 
-type dbIterTest struct {
-	next func() (RunProgress, bool)
+type dbIterTest[T runProgress] struct {
+	next func() (T, bool)
 }
 
-func (db *dbIterTest) StructScan(v any) bool {
-	pr, ok := v.(*RunProgress)
+func (db *dbIterTest[T]) StructScan(v any) bool {
+	pr, ok := v.(*T)
 	if !ok {
 		return false
 	}
