d079bd0de3dc5ba5cf71a01014302ebf869a0a69
Michał Leszczyński <2000michal@wp.pl>
2025-05-29 17:08:37 +0200

    fix(treewide): change Logf to Fatalf in api tests

--- pkg/command/info/cmd_integration_api_test.go
+++ pkg/command/info/cmd_integration_api_test.go
@@ -37,7 +37,7 @@ func TestSctoolInfoLabelsIntegrationAPITest(t *testing.T) {
 
 	defer func() {
 		if err := client.DeleteCluster(context.Background(), clusterID); err != nil {
-			t.Logf("Failed to delete cluster, err = {%v}", err)
+			t.Fatalf("Failed to delete cluster, err = {%v}", err)
 		}
 	}()
 
@@ -50,7 +50,7 @@ func TestSctoolInfoLabelsIntegrationAPITest(t *testing.T) {
 		Properties: make(map[string]interface{}),
 	})
 	if err != nil {
-		t.Logf("Failed to create task, err = {%v}", err)
+		t.Fatalf("Failed to create task, err = {%v}", err)
 	}
 
 	if err := client.UpdateTask(context.Background(), clusterID, &managerclient.Task{
