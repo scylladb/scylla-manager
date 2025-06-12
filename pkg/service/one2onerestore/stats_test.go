ea5036ce1a0c1161b14ff125571d5be6f911f1e0
Vasil Averyanau <VAveryanov8@users.noreply.github.com>
2025-06-12 13:15:24 +0200

    feat(1-1-restore): add one2onerestore_progress metric (#4423)
    This adds one2onerestore_progress metrics that will show percent of
    downloaded bytes from 0 to 100%

--- pkg/service/one2onerestore/stats_test.go
+++ pkg/service/one2onerestore/stats_test.go
@@ -9,38 +9,123 @@ import (
 
 func TestAverageBandwidthPerShard(t *testing.T) {
 	testCases := []struct {
-		name     string
-		stats    *restoreStats
-		expected string
+		name                string
+		workload            []hostWorkload
+		totalDownloadBytes  int64
+		totalDownloadTimeMS int64
+		expected            string
 	}{
 		{
 			name:     "no data",
-			stats:    &restoreStats{},
+			workload: nil,
 			expected: "unknown",
 		},
 		{
 			name: "no shards",
-			stats: &restoreStats{
-				totalDownloadTimeMS: 1000,
-				totalDownloadBytes:  1024,
+
+			workload: []hostWorkload{
+				{host: Host{ShardCount: 0}},
+				{host: Host{ShardCount: 0}},
 			},
-			expected: "1KiB/s",
+			totalDownloadBytes:  1024,
+			totalDownloadTimeMS: 1000,
+			expected:            "1KiB/s",
 		},
 		{
 			name: "shards",
-			stats: &restoreStats{
-				totalDownloadTimeMS: 1000,
-				totalDownloadBytes:  1024,
-				totalShards:         2,
+			workload: []hostWorkload{
+				{host: Host{ShardCount: 1}},
+				{host: Host{ShardCount: 1}},
 			},
-			expected: "512B/s/shard",
+			totalDownloadBytes:  1024,
+			totalDownloadTimeMS: 1000,
+			expected:            "512B/s/shard",
 		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			if got := tc.stats.averageBandwidthPerShard(); got != tc.expected {
+			stats := newRestoreStats(tc.workload)
+			stats.incrementDownloadStats(tc.totalDownloadBytes, tc.totalDownloadTimeMS)
+			if got := stats.averageBandwidthPerShard(); got != tc.expected {
 				t.Fatalf("Expected bandwidth %s, but got %s", tc.expected, got)
+			}
+		})
+	}
+}
+
+func TestProgress(t *testing.T) {
+	testCases := []struct {
+		name                string
+		workload            []hostWorkload
+		totalDownloadBytes  int64
+		totalDownloadTimeMS int64
+		expected            float64
+	}{
+		{
+			name:     "no data",
+			workload: nil,
+			expected: 100.0,
+		},
+		{
+			name: "no tables",
+			workload: []hostWorkload{
+				{host: Host{ShardCount: 0}},
+				{host: Host{ShardCount: 0}},
+			},
+			totalDownloadBytes:  1024,
+			totalDownloadTimeMS: 1000,
+			expected:            100.0,
+		},
+		{
+			name: "everything is restored",
+			workload: []hostWorkload{
+				{host: Host{ShardCount: 1}, tablesToRestore: []scyllaTableWithSize{
+					{scyllaTable: scyllaTable{keyspace: "ks1", table: "table1"}, size: 512},
+				}},
+				{host: Host{ShardCount: 1}, tablesToRestore: []scyllaTableWithSize{
+					{scyllaTable: scyllaTable{keyspace: "ks1", table: "table1"}, size: 512},
+				}},
+			},
+			totalDownloadBytes:  1024,
+			totalDownloadTimeMS: 1000,
+			expected:            100,
+		},
+		{
+			name: "half is restored",
+			workload: []hostWorkload{
+				{host: Host{ShardCount: 1}, tablesToRestore: []scyllaTableWithSize{
+					{scyllaTable: scyllaTable{keyspace: "ks1", table: "table1"}, size: 512},
+				}},
+				{host: Host{ShardCount: 1}, tablesToRestore: []scyllaTableWithSize{
+					{scyllaTable: scyllaTable{keyspace: "ks1", table: "table1"}, size: 512},
+				}},
+			},
+			totalDownloadBytes:  512,
+			totalDownloadTimeMS: 1000,
+			expected:            50,
+		},
+		{
+			name: "nothing is restored",
+			workload: []hostWorkload{
+				{host: Host{ShardCount: 1}, tablesToRestore: []scyllaTableWithSize{
+					{scyllaTable: scyllaTable{keyspace: "ks1", table: "table1"}, size: 512},
+				}},
+				{host: Host{ShardCount: 1}, tablesToRestore: []scyllaTableWithSize{
+					{scyllaTable: scyllaTable{keyspace: "ks1", table: "table1"}, size: 512},
+				}},
+			},
+			totalDownloadBytes:  0,
+			totalDownloadTimeMS: 0,
+			expected:            0,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			stats := newRestoreStats(tc.workload)
+			stats.incrementDownloadStats(tc.totalDownloadBytes, tc.totalDownloadTimeMS)
+			if got := stats.progress(); got != tc.expected {
+				t.Fatalf("Expected progress %.2f, but got %.2f", tc.expected, got)
 			}
 		})
 	}
@@ -48,9 +133,17 @@ func TestAverageBandwidthPerShard(t *testing.T) {
 
 func TestIncrementDownloadStats(t *testing.T) {
 	stats := newRestoreStats([]hostWorkload{
-		{host: Host{ShardCount: 2}},
-		{host: Host{ShardCount: 2}},
+		{host: Host{ShardCount: 2}, tablesToRestore: []scyllaTableWithSize{
+			{scyllaTable: scyllaTable{keyspace: "ks1", table: "table1"}, size: 5 * 1024},
+		}},
+		{host: Host{ShardCount: 2}, tablesToRestore: []scyllaTableWithSize{
+			{scyllaTable: scyllaTable{keyspace: "ks2", table: "table1"}, size: 5 * 1024},
+		}},
 	})
+
+	if stats.totalBytesToRestore != 10*1024 {
+		t.Fatalf("Expected totalBytesToRestore 10*1024, but got %d", stats.totalBytesToRestore)
+	}
 
 	var (
 		wg              sync.WaitGroup
@@ -71,10 +164,10 @@ func TestIncrementDownloadStats(t *testing.T) {
 	if stats.totalShards != 4 {
 		t.Fatalf("Expected totalShards 4, but got %d", stats.totalShards)
 	}
-	if stats.totalDownloadBytes != int64(numberOfWorkers*bytesPerWorker) {
-		t.Fatalf("Expected totalDownloadBytes %d, but got %d", int64(numberOfWorkers*bytesPerWorker), stats.totalDownloadBytes)
+	if stats.totalDownloadBytes.Load() != int64(numberOfWorkers*bytesPerWorker) {
+		t.Fatalf("Expected totalDownloadBytes %d, but got %d", int64(numberOfWorkers*bytesPerWorker), stats.totalDownloadBytes.Load())
 	}
-	if stats.totalDownloadTimeMS != int64(numberOfWorkers*timePerWorker) {
-		t.Fatalf("Expected totalDownloadTimeMS %d, but got %d", int64(numberOfWorkers*timePerWorker), stats.totalDownloadTimeMS)
+	if stats.totalDownloadTimeMS.Load() != int64(numberOfWorkers*timePerWorker) {
+		t.Fatalf("Expected totalDownloadTimeMS %d, but got %d", int64(numberOfWorkers*timePerWorker), stats.totalDownloadTimeMS.Load())
 	}
 }
