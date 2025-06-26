6942f236260d1c39b1019e40db6105eb18240fce
Vasil Averyanau <vasil.averyanau@scylladb.com>
2025-06-26 12:09:12 +0200

    feat(1-1-restore): adds support for views (#4257)
    This adds support for restoring views in 1-1-restore.
    Views should be dropped before copy data stage and then re-created to
    avoid data inconsistency between base table and view.
    
    Refs: #4252

--- pkg/service/one2onerestore/worker.go
+++ pkg/service/one2onerestore/worker.go
@@ -4,12 +4,17 @@ package one2onerestore
 
 import (
 	"context"
+	"strings"
+	"time"
 
 	"github.com/pkg/errors"
 	"github.com/scylladb/go-log"
 	"github.com/scylladb/gocqlx/v2"
 	"github.com/scylladb/scylla-manager/backupspec"
 	"github.com/scylladb/scylla-manager/v3/pkg/scyllaclient"
+	"github.com/scylladb/scylla-manager/v3/pkg/util/parallel"
+	"github.com/scylladb/scylla-manager/v3/pkg/util/retry"
+	"go.uber.org/multierr"
 )
 
 type worker struct {
@@ -19,6 +24,24 @@ type worker struct {
 	clusterSession gocqlx.Session
 
 	logger log.Logger
+}
+
+// restore is an actual 1-1-restore stages.
+func (w *worker) restore(ctx context.Context, workload []hostWorkload, target Target) (err error) {
+	views, err := w.dropViews(ctx, workload, target.Keyspace)
+	if err != nil {
+		return errors.Wrap(err, "drop views")
+	}
+	defer func() {
+		if rErr := w.reCreateViews(ctx, views); rErr != nil {
+			err = multierr.Combine(
+				err,
+				errors.Wrap(rErr, "recreate views"),
+			)
+		}
+	}()
+
+	return w.restoreTables(ctx, workload, target.Keyspace)
 }
 
 // getAllSnapshotManifestsAndTargetHosts gets backup(source) cluster node represented by manifests and target cluster nodes.
@@ -78,4 +101,56 @@ func nodesToHosts(nodes scyllaclient.NodeStatusInfoSlice) []Host {
 		})
 	}
 	return hosts
+}
+
+// prepareHostWorkload is a helper function that creates a hostWorkload structure convenient for use in later 1-1-restore stages.
+// This avoids the need to repeat operations like node mapping and fetching manifest content.
+func (w *worker) prepareHostWorkload(ctx context.Context, manifests []*backupspec.ManifestInfo, hosts []Host, nodeMappings []nodeMapping) ([]hostWorkload, error) {
+	targetBySourceHostID, err := mapTargetHostToSource(hosts, nodeMappings)
+	if err != nil {
+		return nil, errors.Wrap(err, "invalid node mapping")
+	}
+
+	result := make([]hostWorkload, len(manifests))
+	return result, parallel.Run(len(manifests), len(manifests), func(i int) error {
+		m := manifests[i]
+		h := targetBySourceHostID[m.NodeID]
+
+		mc, err := w.getManifestContent(ctx, h.Addr, m)
+		if err != nil {
+			return errors.Wrap(err, "manifest content")
+		}
+		result[i] = hostWorkload{
+			host:            h,
+			manifestInfo:    m,
+			manifestContent: mc,
+		}
+
+		return nil
+	}, parallel.NopNotify)
+}
+
+// alterSchemaRetryWrapper is useful when executing many statements altering schema,
+// as it might take more time for Scylla to process them one after another.
+// This wrapper exits on: success, context cancel, op returned non-timeout error or after maxTotalTime has passed.
+func alterSchemaRetryWrapper(ctx context.Context, op func() error, notify func(err error, wait time.Duration)) error {
+	const (
+		minWait      = 5 * time.Second
+		maxWait      = 1 * time.Minute
+		maxTotalTime = 15 * time.Minute
+		multiplier   = 2
+		jitter       = 0.2
+	)
+	backoff := retry.NewExponentialBackoff(minWait, maxTotalTime, maxWait, multiplier, jitter)
+
+	wrappedOp := func() error {
+		err := op()
+		if err == nil || strings.Contains(err.Error(), "timeout") {
+			return err
+		}
+		// All non-timeout errors shouldn't be retried
+		return retry.Permanent(err)
+	}
+
+	return retry.WithNotify(ctx, wrappedOp, backoff, notify)
 }
