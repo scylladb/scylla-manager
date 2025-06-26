6243b2bb0042bd9fd1e57cf204f9c358fe507935
Vasil Averyanau <vasil.averyanau@scylladb.com>
2025-06-26 12:10:22 +0200

    feat(1-1-restore): reduces --keyspace param features to keyspace level filtering only (#4256)
    This reduces functionality of `--keyspace` parameter to support only:
     - keyspace level filtering. Table level filtering (system.table_name) is forbidden.
     - only include filtering. Excluding(!) is forbidden.
     - wildcards (*) are forbidden. The only exception is when the only filter that is provided is a single character (*),
    meaning that all keyspaces should be included into restore process.
    This is done by allowing to filter on keyspaces that are already exists in the cluster.
    
    Refs: #4253

--- pkg/service/one2onerestore/model_test.go
+++ pkg/service/one2onerestore/model_test.go
@@ -121,3 +121,72 @@ func TestValidateNodesMapping(t *testing.T) {
 		})
 	}
 }
+
+func TestValidateKeyspaceFilter(t *testing.T) {
+	testCases := []struct {
+		name           string
+		keyspaceFilter []string
+		keyspaces      []string
+		expectedErr    string
+	}{
+		{
+			name:           "Only keyspace level filtering",
+			keyspaceFilter: []string{"hello", "system"},
+			keyspaces:      []string{"system", "hello", "world"},
+		},
+		{
+			name:           "Include all * is supported (default)",
+			keyspaceFilter: []string{"*"},
+			keyspaces:      []string{"hello", "world"},
+			expectedErr:    "",
+		},
+		{
+			name:           "Wildcard patterns are not supported",
+			keyspaceFilter: []string{"hello*"},
+			keyspaces:      []string{"hello", "helloworld"},
+			expectedErr:    "only existing keyspaces can be provided, but got: hello*",
+		},
+		{
+			name:           "Wildcard patterns are not supported2",
+			keyspaceFilter: []string{"*", "hi"},
+			keyspaces:      []string{"hi", "hey"},
+			expectedErr:    "only existing keyspaces can be provided, but got: *",
+		},
+		{
+			name:           "Table level is not allowed",
+			keyspaceFilter: []string{"hello", "system.table"},
+			keyspaces:      []string{"hello", "system"},
+			expectedErr:    "only existing keyspaces can be provided, but got: system.table",
+		},
+		{
+			name:           "Exclude filters are not supported",
+			keyspaceFilter: []string{"hello", "!world"},
+			keyspaces:      []string{"hello", "world"},
+			expectedErr:    "only existing keyspaces can be provided, but got: !world",
+		},
+		{
+			name:           "Alternator keyspaces are supported",
+			keyspaceFilter: []string{"alternator_Tab_le-With1.da_sh2-aNd.d33ot.-"},
+			keyspaces:      []string{"hello", "alternator_Tab_le-With1.da_sh2-aNd.d33ot.-"},
+		},
+		{
+			name:           "Empty(nil) filters",
+			keyspaceFilter: nil,
+		},
+	}
+
+	for _, tc := range testCases {
+		errMsg := func(err error) string {
+			if err == nil {
+				return ""
+			}
+			return err.Error()
+		}
+		t.Run(tc.name, func(t *testing.T) {
+			err := validateKeyspaceFilter(tc.keyspaceFilter, tc.keyspaces)
+			if errMsg(err) != tc.expectedErr {
+				t.Fatalf("Expected err %q, but got %q", tc.expectedErr, errMsg(err))
+			}
+		})
+	}
+}
