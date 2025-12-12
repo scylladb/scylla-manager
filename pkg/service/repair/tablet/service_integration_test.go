19ee1d65208d14c0bd554aa191cd89e79382dc7b
Michał Leszczyński <2000michal@wp.pl>
2025-12-12 10:23:13 +0100

    tmp

--- pkg/service/repair/tablet/service_integration_test.go
+++ pkg/service/repair/tablet/service_integration_test.go
@@ -45,9 +45,9 @@ type testHelper struct {
 func newTestHelper(t *testing.T) *testHelper {
 	t.Helper()
 
-	logger := log.NewDevelopmentWithLevel(zapcore.InfoLevel)
+	logger := log.NewDevelopmentWithLevel(zapcore.DebugLevel)
 	hrt := NewHackableRoundTripper(scyllaclient.DefaultTransport())
-	client := newTestClient(t, hrt, log.NopLogger)
+	client := newTestClient(t, hrt, log.NewDevelopment())
 	smSession := CreateScyllaManagerDBSession(t)
 
 	clusterSession := CreateManagedClusterSession(t, true, client, "", "")
@@ -140,240 +140,248 @@ type fullTab struct {
 }
 
 func TestTabletRepairIntegration(t *testing.T) {
-	h := newTestHelper(t)
+	f := func(t *testing.T) {
+		h := newTestHelper(t)
 
-	ni, err := h.Client.AnyNodeInfo(context.Background())
-	if err != nil {
-		t.Fatal(err)
-	}
-	if !ni.EnableTablets {
-		t.Skip("Tablet repair requires tablets to be enabled")
-	}
-
-	if ok, err := ni.SupportsTabletRepair(); err != nil {
-		t.Fatal(err)
-	} else if !ok {
-		t.Skip("Tablet repair requires tablet repair API support")
-	}
-
-	incrementalRepairSupport, err := ni.SupportsIncrementalRepair()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	const (
-		cqlKs   = "cql_ks"
-		cqlTab1 = "tab_1"
-		cqlTab2 = "tab_2"
-		cqlTab3 = "tab_3"
-		altTab1 = "tab_1" + AlternatorProblematicTableChars
-		altTab2 = "tab_2" + AlternatorProblematicTableChars
-		altTab3 = "tab_3" + AlternatorProblematicTableChars
-	)
-
-	// We should enhance table setup with special tables (mv, si, gsi, lsi),
-	// but this requires #4660 to be fixed first.
-
-	// CQL setup
-	WriteData(t, h.clusterSession, cqlKs, 1, cqlTab1, cqlTab2, cqlTab3)
-	// Create CDC table (if supported on tablets)
-	if ok, err := version.CheckConstraint(ni.ScyllaVersion, ">= 2025.4"); err != nil {
-		t.Fatal(err)
-	} else if ok {
-		ExecStmt(t, h.clusterSession, fmt.Sprintf("UPDATE %q.%q SET data = null WHERE id = 0 IF data != null", cqlKs, cqlTab3))
-	}
-
-	// Alternator setup.
-	// Note that alternator tables on tablets are not supported in all scylla versions
-	// which support tablet repair. Because of that, they might or might not be included
-	// in the tablet repair task.
-	CreateAlternatorTable(t, h.alternatorClient, 0, 0, altTab1, altTab2, altTab3)
-
-	// Get all tables from scylla so that we don't need to handle alternator and cdc table names manually
-	allTables := make(map[fullTab]struct{})
-	kss, err := h.Client.FilteredKeyspaces(context.Background(), scyllaclient.KeyspaceTypeNonLocal, scyllaclient.ReplicationTablet)
-	if err != nil {
-		t.Fatal(err)
-	}
-	for _, ks := range kss {
-		tabs, err := h.Client.Tables(context.Background(), ks)
+		ni, err := h.Client.AnyNodeInfo(context.Background())
 		if err != nil {
 			t.Fatal(err)
 		}
-		for _, tab := range tabs {
-			allTables[fullTab{
-				ks:  ks,
-				tab: tab,
-			}] = struct{}{}
+		if !ni.EnableTablets {
+			t.Skip("Tablet repair requires tablets to be enabled")
 		}
-	}
 
-	// helper function which runs tablet repair and verifies that:
-	// - it targeted all tables
-	// - it made a single repair call per table
-	// - it used incremental mode if supported
-	smokeTest := func(t *testing.T, testCtx context.Context) {
-		repairedTables := map[fullTab]struct{}{}
-		mu := sync.Mutex{}
+		if ok, err := ni.SupportsTabletRepair(); err != nil {
+			t.Fatal(err)
+		} else if !ok {
+			t.Skip("Tablet repair requires tablet repair API support")
+		}
 
-		h.Hrt.SetInterceptor(httpx.RoundTripperFunc(func(req *http.Request) (*http.Response, error) {
-			if !isTabletRepairReq(req) {
-				return nil, nil
-			}
-			mu.Lock()
-			defer mu.Unlock()
-			// Check single call per single table
-			trr := parseTabletRepairReq(t, req)
-			ft := trr.fullTab()
-			if _, ok := repairedTables[ft]; ok {
-				t.Errorf("Multiple tablet repair calls for a single table %s", ft)
-			}
-			repairedTables[ft] = struct{}{}
-			// Check incremental mode
-			if incrementalRepairSupport && trr.incrementalMode != scyllaclient.IncrementalModeIncremental {
-				t.Errorf("Expected incremental mode, got %q", trr.incrementalMode)
-			}
-			if !incrementalRepairSupport && trr.incrementalMode != "" {
-				t.Errorf("Expected no incremental mode, got %q", trr.incrementalMode)
-			}
-			return nil, nil
-		}))
-
-		h.RunID = uuid.NewTime()
-		err := h.service.Run(testCtx, h.ClusterID, h.TaskID, h.RunID, json.RawMessage{})
+		incrementalRepairSupport, err := ni.SupportsIncrementalRepair()
 		if err != nil {
 			t.Fatal(err)
 		}
-		// Check that all tables were repaired
-		if !maps.Equal(allTables, repairedTables) {
-			t.Fatalf("Expected repaired tables to be:\n%v\n, got:\n%v\n", allTables, repairedTables)
+
+		const (
+			cqlKs   = "cql_ks"
+			cqlTab1 = "tab_1"
+			cqlTab2 = "tab_2"
+			cqlTab3 = "tab_3"
+			altTab1 = "tab_1" + AlternatorProblematicTableChars
+			altTab2 = "tab_2" + AlternatorProblematicTableChars
+			altTab3 = "tab_3" + AlternatorProblematicTableChars
+		)
+
+		// We should enhance table setup with special tables (mv, si, gsi, lsi),
+		// but this requires #4660 to be fixed first.
+
+		// CQL setup
+		WriteData(t, h.clusterSession, cqlKs, 1, cqlTab1, cqlTab2, cqlTab3)
+		// Create CDC table (if supported on tablets)
+		if ok, err := version.CheckConstraint(ni.ScyllaVersion, ">= 2025.4"); err != nil {
+			t.Fatal(err)
+		} else if ok {
+			ExecStmt(t, h.clusterSession, fmt.Sprintf("UPDATE %q.%q SET data = null WHERE id = 0 IF data != null", cqlKs, cqlTab3))
 		}
-	}
 
-	t.Run("Smoke", func(t *testing.T) {
-		testCtx, testCancel := context.WithTimeout(context.Background(), 5*time.Minute)
-		defer testCancel()
-		smokeTest(t, testCtx)
-	})
+		// Alternator setup.
+		// Note that alternator tables on tablets are not supported in all scylla versions
+		// which support tablet repair. Because of that, they might or might not be included
+		// in the tablet repair task.
+		CreateAlternatorTable(t, h.alternatorClient, 0, 0, altTab1, altTab2, altTab3)
 
-	// helper function which runs tablet repair and hangs it after N tables were repaired.
-	// It returns:
-	// - repairCtxCancel - cancels ctx of hanged repair
-	// - freeRepair - unhangs hanged repair
-	// - waitRepair - waits for the unhanged repair to finish
-	hangAfter := func(t *testing.T, testCtx context.Context, after int) (repairCtxCancel, freeRepair func(), waitRepair func() error) {
-		// Since we intercept tablet repair sched, we need to start counting from -1
-		doneTabCnt := -1
-		testWait := make(chan struct{})
-		hangRepair := make(chan struct{})
-		mu := sync.Mutex{}
-
-		h.Hrt.SetInterceptor(httpx.RoundTripperFunc(func(req *http.Request) (*http.Response, error) {
-			if !isTabletRepairReq(req) {
-				return nil, nil
-			}
-			mu.Lock()
-			doneTabCnt++
-			curr := doneTabCnt
-			mu.Unlock()
-			// Hang after N table is repaired
-			if curr == after {
-				close(testWait)
-				select {
-				case <-hangRepair:
-				case <-testCtx.Done():
-					return nil, testCtx.Err()
-				}
-			}
-			return nil, nil
-		}))
-		// Start repair
-		wg, _ := errgroup.WithContext(testCtx)
-		repairCtx, repairCtxCancel := context.WithCancel(testCtx)
-		wg.Go(func() error {
-			h.RunID = uuid.NewTime()
-			return h.service.Run(repairCtx, h.ClusterID, h.TaskID, h.RunID, json.RawMessage{})
-		})
-		// Wait for repair to hang
-		select {
-		case <-testWait:
-		case <-testCtx.Done():
-			t.Error(testCtx.Err())
-		}
-		return repairCtxCancel, func() { close(hangRepair) }, wg.Wait
-	}
-
-	t.Run("Pause and resume", func(t *testing.T) {
-		testCtx, testCancel := context.WithTimeout(context.Background(), 5*time.Minute)
-		defer testCancel()
-		// Hang running repair
-		const interruptAfter = 2
-		repairCtxCancel, freeRepair, waitRepair := hangAfter(t, testCtx, interruptAfter)
-		// Verify progress of running repair
-		pr, err := h.service.GetProgress(testCtx, h.ClusterID, h.TaskID, h.RunID)
+		// Get all tables from scylla so that we don't need to handle alternator and cdc table names manually
+		allTables := make(map[fullTab]struct{})
+		kss, err := h.Client.FilteredKeyspaces(context.Background(), scyllaclient.KeyspaceTypeNonLocal, scyllaclient.ReplicationTablet)
 		if err != nil {
 			t.Fatal(err)
 		}
-		var completedTabCnt int
-		for _, tab := range pr.Tables {
-			if tab.CompletedAt != nil && !tab.CompletedAt.IsZero() && tab.Error == "" {
-				completedTabCnt++
-			}
-		}
-		if completedTabCnt != interruptAfter {
-			t.Fatalf("Expected %d completed tables, got %d from progress %+v", interruptAfter, completedTabCnt, pr)
-		}
-		// Pause repair and let it finish
-		repairCtxCancel()
-		freeRepair()
-		if err := waitRepair(); !errors.Is(err, context.Canceled) {
-			t.Fatalf("Expected context canceled error, got %v", err)
-		}
-		// Resume repair and verify that it ran from scratch
-		smokeTest(t, testCtx)
-	})
-
-	t.Run("Transient node down", func(t *testing.T) {
-		testCtx, testCancel := context.WithTimeout(context.Background(), 5*time.Minute)
-		defer testCancel()
-		// Hang running repair
-		const interruptAfter = 1
-		_, freeRepair, waitRepair := hangAfter(t, testCtx, interruptAfter)
-		// Take node down when repair is running
-		downNode := ManagedClusterHost()
-		ni, err := h.Client.NodeInfo(testCtx, downNode)
-		if err != nil {
-			t.Fatal(err)
-		}
-		h.StopNode(downNode)
-		// Unhang repair
-		freeRepair()
-		// Let repair run with node down for a while
-		time.Sleep(15 * time.Second)
-		// Bring node up
-		h.StartNode(downNode, ni)
-		// Verify that repair finished successfully
-		if err := waitRepair(); err != nil {
-			t.Fatal(err)
-		}
-	})
-
-	t.Run("Ongoing scylla tablet repair tasks", func(t *testing.T) {
-		testCtx, testCancel := context.WithTimeout(context.Background(), 5*time.Minute)
-		defer testCancel()
-
-		h.Hrt.SetInterceptor(nil)
-		// Start lots of scylla tablet repair tasks
-		for ft := range allTables {
-			_, _ = h.Client.TabletRepair(testCtx, ft.ks, ft.tab, "", nil, nil, scyllaclient.IncrementalModeDisabled)
-		}
-		// Make sure that they are visible on all nodes
-		for _, host := range ManagedClusterHosts() {
-			if err := h.Client.RaftReadBarrier(testCtx, host, ""); err != nil {
+		for _, ks := range kss {
+			tabs, err := h.Client.Tables(context.Background(), ks)
+			if err != nil {
 				t.Fatal(err)
 			}
+			for _, tab := range tabs {
+				allTables[fullTab{
+					ks:  ks,
+					tab: tab,
+				}] = struct{}{}
+			}
 		}
-		// Verify that those scylla tablet repair tasks won't break SM tablet repair task
-		smokeTest(t, testCtx)
-	})
+
+		// helper function which runs tablet repair and verifies that:
+		// - it targeted all tables
+		// - it made a single repair call per table
+		// - it used incremental mode if supported
+		smokeTest := func(t *testing.T, testCtx context.Context) {
+			repairedTables := map[fullTab]struct{}{}
+			mu := sync.Mutex{}
+
+			h.Hrt.SetInterceptor(httpx.RoundTripperFunc(func(req *http.Request) (*http.Response, error) {
+				if !isTabletRepairReq(req) {
+					return nil, nil
+				}
+				mu.Lock()
+				defer mu.Unlock()
+				// Check single call per single table
+				trr := parseTabletRepairReq(t, req)
+				ft := trr.fullTab()
+				if _, ok := repairedTables[ft]; ok {
+					t.Errorf("Multiple tablet repair calls for a single table %s", ft)
+				}
+				repairedTables[ft] = struct{}{}
+				// Check incremental mode
+				if incrementalRepairSupport && trr.incrementalMode != scyllaclient.IncrementalModeIncremental {
+					t.Errorf("Expected incremental mode, got %q", trr.incrementalMode)
+				}
+				if !incrementalRepairSupport && trr.incrementalMode != "" {
+					t.Errorf("Expected no incremental mode, got %q", trr.incrementalMode)
+				}
+				return nil, nil
+			}))
+
+			h.RunID = uuid.NewTime()
+			err := h.service.Run(testCtx, h.ClusterID, h.TaskID, h.RunID, json.RawMessage{})
+			if err != nil {
+				t.Fatal(err)
+			}
+			// Check that all tables were repaired
+			if !maps.Equal(allTables, repairedTables) {
+				t.Fatalf("Expected repaired tables to be:\n%v\n, got:\n%v\n", allTables, repairedTables)
+			}
+		}
+
+		t.Run("Smoke", func(t *testing.T) {
+			testCtx, testCancel := context.WithTimeout(context.Background(), 5*time.Minute)
+			defer testCancel()
+			smokeTest(t, testCtx)
+		})
+
+		// helper function which runs tablet repair and hangs it after N tables were repaired.
+		// It returns:
+		// - repairCtxCancel - cancels ctx of hanged repair
+		// - freeRepair - unhangs hanged repair
+		// - waitRepair - waits for the unhanged repair to finish
+		hangAfter := func(t *testing.T, testCtx context.Context, after int) (repairCtxCancel, freeRepair func(), waitRepair func() error) {
+			// Since we intercept tablet repair sched, we need to start counting from -1
+			doneTabCnt := -1
+			testWait := make(chan struct{})
+			hangRepair := make(chan struct{})
+			mu := sync.Mutex{}
+
+			h.Hrt.SetInterceptor(httpx.RoundTripperFunc(func(req *http.Request) (*http.Response, error) {
+				if !isTabletRepairReq(req) {
+					return nil, nil
+				}
+				mu.Lock()
+				doneTabCnt++
+				curr := doneTabCnt
+				mu.Unlock()
+				// Hang after N table is repaired
+				if curr == after {
+					close(testWait)
+					select {
+					case <-hangRepair:
+					case <-testCtx.Done():
+						return nil, testCtx.Err()
+					}
+				}
+				return nil, nil
+			}))
+			// Start repair
+			wg, _ := errgroup.WithContext(testCtx)
+			repairCtx, repairCtxCancel := context.WithCancel(testCtx)
+			wg.Go(func() error {
+				h.RunID = uuid.NewTime()
+				return h.service.Run(repairCtx, h.ClusterID, h.TaskID, h.RunID, json.RawMessage{})
+			})
+			// Wait for repair to hang
+			select {
+			case <-testWait:
+			case <-testCtx.Done():
+				t.Error(testCtx.Err())
+			}
+			return repairCtxCancel, func() { close(hangRepair) }, wg.Wait
+		}
+
+		t.Run("Pause and resume", func(t *testing.T) {
+			testCtx, testCancel := context.WithTimeout(context.Background(), 5*time.Minute)
+			defer testCancel()
+			// Hang running repair
+			const interruptAfter = 2
+			repairCtxCancel, freeRepair, waitRepair := hangAfter(t, testCtx, interruptAfter)
+			// Verify progress of running repair
+			pr, err := h.service.GetProgress(testCtx, h.ClusterID, h.TaskID, h.RunID)
+			if err != nil {
+				t.Fatal(err)
+			}
+			var completedTabCnt int
+			for _, tab := range pr.Tables {
+				if tab.CompletedAt != nil && !tab.CompletedAt.IsZero() && tab.Error == "" {
+					completedTabCnt++
+				}
+			}
+			if completedTabCnt != interruptAfter {
+				t.Fatalf("Expected %d completed tables, got %d from progress %+v", interruptAfter, completedTabCnt, pr)
+			}
+			// Pause repair and let it finish
+			repairCtxCancel()
+			freeRepair()
+			if err := waitRepair(); !errors.Is(err, context.Canceled) {
+				t.Fatalf("Expected context canceled error, got %v", err)
+			}
+			// Resume repair and verify that it ran from scratch
+			smokeTest(t, testCtx)
+		})
+
+		t.Run("Transient node down", func(t *testing.T) {
+			testCtx, testCancel := context.WithTimeout(context.Background(), 5*time.Minute)
+			defer testCancel()
+			// Hang running repair
+			const interruptAfter = 1
+			_, freeRepair, waitRepair := hangAfter(t, testCtx, interruptAfter)
+			// Take node down when repair is running
+			downNode := ManagedClusterHost()
+			ni, err := h.Client.NodeInfo(testCtx, downNode)
+			if err != nil {
+				t.Fatal(err)
+			}
+			h.StopNode(downNode)
+			// Unhang repair
+			freeRepair()
+			// Let repair run with node down for a while
+			time.Sleep(15 * time.Second)
+			// Bring node up
+			h.StartNode(downNode, ni)
+			// Verify that repair finished successfully
+			if err := waitRepair(); err != nil {
+				t.Fatal(err)
+			}
+		})
+
+		t.Run("Ongoing scylla tablet repair tasks", func(t *testing.T) {
+			testCtx, testCancel := context.WithTimeout(context.Background(), 5*time.Minute)
+			defer testCancel()
+
+			h.Hrt.SetInterceptor(nil)
+			// Start lots of scylla tablet repair tasks
+			for ft := range allTables {
+				_, _ = h.Client.TabletRepair(testCtx, ft.ks, ft.tab, "", nil, nil, scyllaclient.IncrementalModeDisabled)
+			}
+			// Make sure that they are visible on all nodes
+			for _, host := range ManagedClusterHosts() {
+				if err := h.Client.RaftReadBarrier(testCtx, host, ""); err != nil {
+					t.Fatal(err)
+				}
+			}
+			// Verify that those scylla tablet repair tasks won't break SM tablet repair task
+			smokeTest(t, testCtx)
+		})
+	}
+
+	for i := range 100 {
+		if !t.Run(fmt.Sprintf("Loop %d", i), f) {
+			return
+		}
+	}
 }
