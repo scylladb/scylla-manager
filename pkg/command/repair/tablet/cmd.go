9c163f6b4bbbc9c034355bb4eae5754504904123
Michał Leszczyński <2000michal@wp.pl>
2025-12-16 16:02:23 +0100

    feat(command/tablet): add tablet repair commands
    This commit adds `sctool repair tablet` command.
    It also updates progress commands to work with tablet repair.
    
    Fixes #4644

---
+++ pkg/command/repair/tablet/cmd.go
@@ -0,0 +1,115 @@
+// Copyright (C) 2025 ScyllaDB
+
+package tablet
+
+import (
+	_ "embed"
+	"errors"
+	"fmt"
+
+	"github.com/scylladb/scylla-manager/v3/pkg/command/flag"
+	"github.com/scylladb/scylla-manager/v3/pkg/managerclient"
+	"github.com/spf13/cobra"
+	"gopkg.in/yaml.v2"
+)
+
+//go:embed res.yaml
+var res []byte
+
+//go:embed update-res.yaml
+var updateRes []byte
+
+type command struct {
+	flag.TaskBase
+
+	client *managerclient.Client
+
+	cluster string
+}
+
+func NewCommand(client *managerclient.Client) *cobra.Command {
+	cmd := newCommand(client, false)
+	updateCmd := newCommand(client, true)
+	cmd.AddCommand(&updateCmd.Command)
+
+	return &cmd.Command
+}
+
+func newCommand(client *managerclient.Client, update bool) *command {
+	var (
+		cmd = &command{
+			client: client,
+		}
+		r []byte
+	)
+	if update {
+		cmd.TaskBase = flag.NewUpdateTaskBase()
+		r = updateRes
+	} else {
+		cmd.TaskBase = flag.MakeTaskBase()
+		r = res
+	}
+	if err := yaml.Unmarshal(r, &cmd.Command); err != nil {
+		panic(err)
+	}
+	cmd.init()
+	cmd.RunE = func(_ *cobra.Command, args []string) error {
+		return cmd.run(args)
+	}
+	return cmd
+}
+
+func (cmd *command) init() {
+	cmd.Init()
+
+	defer flag.MustSetUsages(&cmd.Command, res, "cluster")
+	w := flag.Wrap(cmd.Flags())
+	w.Cluster(&cmd.cluster)
+}
+
+func (cmd *command) run(args []string) error {
+	var (
+		task *managerclient.Task
+		ok   bool
+	)
+
+	if cmd.Update() {
+		a := managerclient.TabletRepairTask
+		if len(args) > 0 {
+			a = args[0]
+		}
+		taskType, taskID, err := cmd.client.TaskSplit(cmd.Context(), cmd.cluster, a)
+		if err != nil {
+			return err
+		}
+		if taskType != managerclient.TabletRepairTask {
+			return fmt.Errorf("can't handle %s task", taskType)
+		}
+
+		task, err = cmd.client.GetTask(cmd.Context(), cmd.cluster, taskType, taskID)
+		if err != nil {
+			return err
+		}
+		ok = cmd.UpdateTask(task)
+	} else {
+		task = cmd.CreateTask(managerclient.TabletRepairTask)
+	}
+
+	switch {
+	case task.ID == "":
+		id, err := cmd.client.CreateTask(cmd.Context(), cmd.cluster, task)
+		if err != nil {
+			return err
+		}
+		task.ID = id.String()
+	case ok:
+		if err := cmd.client.UpdateTask(cmd.Context(), cmd.cluster, task); err != nil {
+			return err
+		}
+	default:
+		return errors.New("nothing to do")
+	}
+
+	_, err := fmt.Fprintln(cmd.OutOrStdout(), managerclient.TaskID(task))
+	return err
+}
