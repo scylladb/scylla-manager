43544d1a3970f6a568f9836ae0e43bda0904d6a4
Michał Leszczyński <2000michal@wp.pl>
2025-12-16 19:20:47 +0100

    feat(command/localsnapshot): add backup delete local snapshot command
    This commit adds `sctool backup delete local-snapshot` command.
    It also runs `make docs`.
    
    Fixes #4648

---
+++ pkg/command/backup/backupdelete/localsnapshot/cmd.go
@@ -0,0 +1,48 @@
+// Copyright (C) 2025 ScyllaDB
+
+package localsnapshot
+
+import (
+	_ "embed"
+
+	"github.com/scylladb/scylla-manager/v3/pkg/command/flag"
+	"github.com/scylladb/scylla-manager/v3/pkg/managerclient"
+	"github.com/spf13/cobra"
+	"gopkg.in/yaml.v2"
+)
+
+//go:embed res.yaml
+var res []byte
+
+type command struct {
+	cobra.Command
+
+	client *managerclient.Client
+
+	cluster string
+}
+
+func NewCommand(client *managerclient.Client) *cobra.Command {
+	cmd := &command{
+		client: client,
+	}
+	if err := yaml.Unmarshal(res, &cmd.Command); err != nil {
+		panic(err)
+	}
+	cmd.init()
+	cmd.RunE = func(_ *cobra.Command, _ []string) error {
+		return cmd.run()
+	}
+	return &cmd.Command
+}
+
+func (cmd *command) init() {
+	defer flag.MustSetUsages(&cmd.Command, res, "cluster")
+
+	w := flag.Wrap(cmd.Flags())
+	w.Cluster(&cmd.cluster)
+}
+
+func (cmd *command) run() error {
+	return cmd.client.DeleteLocalSnapshots(cmd.Context(), cmd.cluster)
+}
