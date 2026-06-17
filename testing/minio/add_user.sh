537ca612c56d5476bfd6520f1b0077308d0e8f2f
Michał Leszczyński <2000michal@wp.pl>
2026-06-17 12:18:35 +0200

    chore(testing): bump minio to its final version
    This commit bumps minio version and updates its
    setup, as some previously used commands are no
    longer supported. This minio version is the last
    one published on their docker account, and it looks
    like they don't plan on publishing newer docker
    images. It should still be possible to create such
    images on our own, as the code is still open source.
    
    Fixes https://scylladb.atlassian.net/browse/CLOUD-2162

--- testing/minio/add_user.sh
+++ testing/minio/add_user.sh
@@ -11,7 +11,7 @@ function mc() {
   docker run -i --rm --network=host -v $(pwd)/minio/mc:/root/.mc/ -v $(pwd)/minio/user-policy.json:/user-policy.json  minio/mc:"$MINIO_MC_VERSION" "$@"
 }
 
-mc --insecure config host add minio-test "$MINIO_ENDPOINT" "$MINIO_ROOT_USER" "$MINIO_ROOT_PASSWORD"
-mc --insecure admin policy add minio-test user /user-policy.json
-mc --insecure admin user add minio-test "$MINIO_USER_ACCESS_KEY" "$MINIO_USER_SECRET_KEY"
-mc --insecure admin policy set minio-test user user="$MINIO_USER_ACCESS_KEY"
+mc --insecure alias set minio-test "$MINIO_ENDPOINT" "$MINIO_ROOT_USER" "$MINIO_ROOT_PASSWORD"
+mc --insecure admin policy create minio-test user /user-policy.json || mc --insecure admin policy update minio-test user /user-policy.json
+mc --insecure admin user add minio-test "$MINIO_USER_ACCESS_KEY" "$MINIO_USER_SECRET_KEY" || true
+mc --insecure admin policy attach minio-test user --user "$MINIO_USER_ACCESS_KEY"
