769650c09ce3d99b63c910eb6f3b15edf2b2d393
Michał Leszczyński <2000michal@wp.pl>
2026-07-21 16:27:09 +0200

    feat(rclone/operations): adjust permission check to event based hold
    This commit changes CheckPermissions so that it validates expected
    event based hold functionalities. It mostly validates permissions
    related to setting event based hold, as the gcs mock used in our
    test env does not currently support CRUD event based hold operations,
    and such validation would fail in our CI. Similar to how the gcs mock
    does not support overriding and cleaning unlocked retention mode,
    as in such case it keeps on returning the unlocked retention mode.

--- pkg/rclone/operations/operations.go
+++ pkg/rclone/operations/operations.go
@@ -63,9 +63,9 @@ func (e OperationError) StatusCode() int {
 // listing, getting, creating, and deleting objects.
 // The remote parameter specifies a subdirectory within l
 // where the test directory is created.
-// Locked and overrideLock controls whether retention lock
-// permissions should be verified.
-func CheckPermissions(ctx context.Context, l fs.Fs, remote string, locked, overrideLock bool) error {
+// Params retentionMode, overrideLock and eventBasedHold control whether
+// retention lock and event based hold related permissions should be verified.
+func CheckPermissions(ctx context.Context, l fs.Fs, remote, retentionMode string, overrideLock, eventBasedHold bool) error {
 	// Disable retries for calls in permissions check.
 	ctx = pacer.WithRetries(ctx, 1)
 
@@ -111,12 +111,12 @@ func CheckPermissions(ctx context.Context, l fs.Fs, remote string, locked, overr
 		}
 	}
 
-	// Create, cat and remove remote file.
+	// Create and cat remote file.
+	o, err := l.NewObject(ctx, filepath.Join(testDirName, testFileName))
+	if err != nil {
+		return errors.Wrap(err, "init remote temp file object")
+	}
 	{
-		o, err := l.NewObject(ctx, filepath.Join(testDirName, testFileName))
-		if err != nil {
-			return errors.Wrap(err, "init remote temp file object")
-		}
 		r, err := o.Open(ctx)
 		if err != nil {
 			return asOperationError("open", l, err)
@@ -125,15 +125,28 @@ func CheckPermissions(ctx context.Context, l fs.Fs, remote string, locked, overr
 		if err := stderr.Join(readErr, r.Close()); err != nil {
 			return asOperationError("read", l, err)
 		}
-		if err := o.Remove(ctx); err != nil {
-			return asOperationError("remove", l, err)
-		}
 	}
 
+	checkDelete := true
 	// Retention lock check.
-	if locked {
-		if err := checkRetentionLock(ctx, l, f, filepath.Join(testDirName, testFileName), overrideLock); err != nil {
+	if retentionMode != "" {
+		if err := checkRetentionLock(ctx, l, o, overrideLock); err != nil {
 			return err
+		}
+		checkDelete = false
+	}
+	// Event based hold check.
+	if eventBasedHold {
+		if err := checkEventBasedHold(ctx, l, o); err != nil {
+			return err
+		}
+		checkDelete = false
+	}
+
+	// Remove the file if eligible
+	if checkDelete {
+		if err := o.Remove(ctx); err != nil {
+			return asOperationError("remove", l, err)
 		}
 	}
 
@@ -142,45 +155,91 @@ func CheckPermissions(ctx context.Context, l fs.Fs, remote string, locked, overr
 		// As we already verified all permissions needed by SM to perform
 		// a successful backup, we can just log an error here to allow
 		// backup to proceed in case of unexpected and not critical error.
-		fs.Errorf(l, "failed to remove test directory %q: %v", testDirName, err)
+		if fs.GetConfig(context.TODO()).LogLevel >= fs.LogLevelWarning {
+			fs.LogPrintf(fs.LogLevelWarning, l, "failed to remove test directory %q: %v", testDirName, err)
+		}
 	}
 
 	return nil
 }
 
-// checkRetentionLock verifies retention lock and optional override lock
-// permissions by re-copying the test file and applying retention policies.
-func checkRetentionLock(ctx context.Context, l, localFs fs.Fs, remote string, overrideLock bool) error {
-	rs, ok := l.(fs.ObjectRetentionSetter)
+// checkRetentionLock verifies retention lock and optional override unlocked permissions on object.
+func checkRetentionLock(ctx context.Context, f fs.Fs, o fs.Object, overrideUnlocked bool) error {
+	// Set retention lock (unlocked, now+1m).
+	rs, ok := f.(fs.ObjectRetentionSetter)
 	if !ok {
-		return asOperationError("retention-lock", l, errors.Errorf("backend %q does not support retention lock", l.Name()))
+		return asOperationError("set-retention-lock", f, errors.Errorf("backend %q does not support retention lock", f.Name()))
 	}
-
-	// Re-copy the test file.
-	if err := copyTestFile(ctx, l, localFs); err != nil {
-		return err
-	}
-
-	// Apply retention lock (unlocked, now+1m).
-	// Time is rounded as retention periods are calculated according to
-	// snapshot tags, which have a second level precision.
 	info := fs.ObjectRetentionInfo{
-		RetainUntil: timeutc.Now().Round(time.Second).Add(time.Minute),
+		RetainUntil: timeutc.Now().Add(time.Minute),
 		Mode:        fs.RetentionModeUnlocked,
 	}
+	if err := rs.SetObjectRetention(ctx, o.Remote(), info, false); err != nil {
+		return asOperationError("set-retention-lock", f, err)
+	}
 
-	if err := rs.SetObjectRetention(ctx, remote, info, false); err != nil {
-		return asOperationError("retention-lock", l, err)
+	// Check applied retention lock.
+	o, err := f.NewObject(ctx, o.Remote())
+	if err != nil {
+		return asOperationError("get-retention-lock", f, errors.Wrap(err, "reload object"))
+	}
+	rl, ok := o.(fs.ObjectRetentionInfoer)
+	if !ok {
+		return asOperationError("get-retention-lock", f, errors.New("object does not support retention lock info"))
+	}
+	info, err = rl.ObjectRetentionInfo(ctx)
+	if err != nil {
+		return asOperationError("get-retention-lock", f, err)
+	}
+	if info.Mode != fs.RetentionModeUnlocked {
+		return asOperationError("get-retention-lock", f, errors.Errorf("expected retention lock %q, got %q", fs.RetentionModeUnlocked, info.Mode))
 	}
 
 	// Override retention lock (locked).
-	info.Mode = fs.RetentionModeLocked
-	if overrideLock {
-		if err := rs.SetObjectRetention(ctx, remote, info, true); err != nil {
-			return asOperationError("override-lock", l, err)
+	if overrideUnlocked {
+		info.Mode = fs.RetentionModeLocked
+		if err := rs.SetObjectRetention(ctx, o.Remote(), info, true); err != nil {
+			return asOperationError("override-unlocked", f, err)
 		}
 	}
 
+	return nil
+}
+
+// checkEventBasedHold verifies event based hold permissions on object.
+func checkEventBasedHold(ctx context.Context, f fs.Fs, o fs.Object) error {
+	// Check default event based hold.
+	es, ok := f.(fs.EventBasedHoldSetter)
+	if !ok {
+		return asOperationError("event-based-hold", f, errors.Errorf("backend %q does not support event-based hold", f.Name()))
+	}
+	eh, ok := o.(fs.EventBasedHolder)
+	if !ok {
+		return asOperationError("get-event-based-hold", f, errors.New("object does not support event-based hold"))
+	}
+	hold, err := eh.EventBasedHold(ctx)
+	if err != nil {
+		return asOperationError("get-event-based-hold", f, err)
+	}
+	if !hold {
+		// SM can perform --retention-lock-mode=event-based-hold backup even without
+		// default event based hold, as SM would just apply the holds itself in such case.
+		// This is undesirable in most cases, as it results in additional per-object req.
+		// In such case, warn with ad-hoc fs.Warnf implementation based on fs.Errorf.
+		if fs.GetConfig(context.TODO()).LogLevel >= fs.LogLevelWarning {
+			fs.LogPrintf(fs.LogLevelWarning, o, "Object doesn't have event based hold automatically applied")
+		}
+		// Set missing event based hold so that we can test its removal.
+		if err := es.SetEventBasedHold(ctx, o.Remote(), true); err != nil {
+			return asOperationError("set-event-based-hold", f, err)
+		}
+	}
+	// Remove the event based hold.
+	// With default bucket retention policy, this starts retention
+	// on the object, so permission check cleanup has to remove it later.
+	if err := es.SetEventBasedHold(ctx, o.Remote(), false); err != nil {
+		return asOperationError("set-event-based-hold", f, err)
+	}
 	return nil
 }
 
