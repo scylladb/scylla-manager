ae7ff164f5c638f432a429d40c796881ab891d53
Michał Leszczyński <2000michal@wp.pl>
2026-08-05 11:26:46 +0200

    feat(backup): release holds from vanished dirs
    This commit reworks event based hold backup part which
    is responsible for handling SM manifests holds.
    Previously, we just listed all manifests from with current
    task ID and removed holds from those with older snapshot tags.
    
    Now we also handle vanished dirs - sstable dirs in backup location
    which had holds applied during previous task execution, but for
    one reason or another, were not part of the current task execution
    and were missed during deduplication stage, which is the main point
    for handling sstable holds.
    
    To do that, we first list all older manifests which still have hold
    applied. Then, we parse them to learn about all dirs that were a part
    of their snapshot. Then we compare those dirs with the current ones,
    find the vanished ones, and release all hold still placed in such dirs.
    All of that happens parallel for location and up to MaxManifestInMemory
    per location.
    
    Fixes https://scylladb.atlassian.net/browse/CLOUD-3223

--- pkg/service/backup/worker_event_based_hold.go
+++ pkg/service/backup/worker_event_based_hold.go
@@ -5,11 +5,14 @@ package backup
 import (
 	"context"
 	stdErr "errors"
+	"net/http"
 	"path"
+	"sync"
 
 	"github.com/pkg/errors"
 	"github.com/scylladb/scylla-manager/backupspec"
 	"github.com/scylladb/scylla-manager/v3/pkg/scyllaclient"
+	"github.com/scylladb/scylla-manager/v3/pkg/util/uuid"
 	"golang.org/x/sync/errgroup"
 )
 
@@ -27,18 +30,20 @@ func (w *worker) eventBasedHoldSnapshot(ctx context.Context, hosts []hostInfo, t
 			return errors.Wrap(err, "handle schema files holds")
 		}
 	}
-
+	// We branch parallelism on location level first (instead of jumping to the host level right away),
+	// because we need to list/download/parse all currently held manifests - even from not currently
+	// operating hosts.
 	eg, ctx := errgroup.WithContext(ctx)
-	eg.SetLimit(MaxManifestInMemory)
-	for i := range hosts {
+	locHosts := groupHostsByLocation(hosts)
+	// MaxManifestInMemory is cluster wide limit, so we need divide it across
+	// all locations. This is not a hard limit, so we can round it up.
+	perLocManifestLimit := (MaxManifestInMemory + len(locHosts) - 1) / len(locHosts)
+	perLocManifestLimit = max(perLocManifestLimit, 1)
+	for loc, hs := range locHosts {
 		eg.Go(func() error {
-			if err := w.holdManifestFiles(ctx, &hosts[i]); err != nil {
-				return errors.Wrapf(err, "handle host %s manifest holds", hosts[i].IP)
-			}
-			return nil
+			return errors.Wrapf(w.holdLocationManifests(ctx, perLocManifestLimit, loc, hs), "location %s", loc)
 		})
 	}
-
 	return eg.Wait()
 }
 
@@ -92,43 +97,255 @@ func (w *worker) holdSchemaFiles(ctx context.Context, hosts []hostInfo) error {
 	return nil
 }
 
-// holdManifestFiles handles event based holds on SM manifests for given host.
-func (w *worker) holdManifestFiles(ctx context.Context, h *hostInfo) error {
-	w.Logger.Info(ctx, "Reconciling manifest files holds", "host", h.IP)
+// holdLocationManifests handles holds of SM manifests and vanished sstable dirs (see vanishedSSTableDirs).
+// We first remove holds from all vanished sstable dirs and only after that we
+// proceed with previous snapshot manifests hold removal. This order is important
+// because we identify unprocessed previous snapshot manifests by the fact that
+// they still have hold applied.
+// Hosts are expected to share the same single loc.
+// Manifest limit refers to the amount of manifests which can be stored
+// in memory at a single time.
+func (w *worker) holdLocationManifests(ctx context.Context, manifestLimit int, loc backupspec.Location, hosts []hostInfo) error {
+	manifests, err := listRemoteManifests(ctx, w.Client, hosts[0].IP, loc, w.ClusterID)
+	if err != nil {
+		return errors.Wrap(err, "list manifests")
+	}
+	current, oldHeld := groupManifestsByTagAndHold(manifests, w.TaskID, w.SnapshotTag)
+	w.Logger.Info(ctx, "Found manifests to process",
+		"location", loc,
+		"current manifests", len(current),
+		"old held manifests", len(oldHeld),
+	)
 
-	manifestPath := backupspec.RemoteManifestFile(w.ClusterID, w.TaskID, w.SnapshotTag, h.DC, h.ID)
-	remoteManifestPath := h.Location.RemotePath(manifestPath)
-	remoteManifestDir := path.Dir(remoteManifestPath)
+	// Even though not having default event based holds set in bucket configuration
+	// is fine for sstables and schema files (it results in one additional per object request),
+	// we rely on correct manifest hold handling to reason about protected snapshots
+	// and vanished sstable dirs. Because of that, we should apply missing manifests holds.
+	var currentNotHeld []string
+	for _, m := range current {
+		if !m.EventBasedHold {
+			currentNotHeld = append(currentNotHeld, m.Path())
+		}
+	}
+	if len(currentNotHeld) > 0 {
+		if err := w.holdAndWait(ctx, hosts[0].IP, loc.RemotePath(""), currentNotHeld, true, "", ""); err != nil {
+			return errors.Wrap(err, "set missing hold on current manifests")
+		}
+	}
 
-	// Initialize holdHandler
+	// Look for vanished sstable dirs and clean holds from them
+	if len(oldHeld) > 0 {
+		vanished, err := w.vanishedSSTableDirs(ctx, manifestLimit, hosts, current, oldHeld)
+		if err != nil {
+			return errors.Wrap(err, "find vanished dirs")
+		}
+		if err := w.releaseAllHoldsAllDirs(ctx, hosts, vanished); err != nil {
+			return errors.Wrap(err, "release vanished dirs holds")
+		}
+	}
+
+	// Finally, clean holds from old manifests
+	if err := w.releaseManifestsHold(ctx, hosts[0].IP, oldHeld); err != nil {
+		return errors.Wrap(err, "release old manifest holds")
+	}
+	return nil
+}
+
+// sstableDir describes sstable version dir.
+type sstableDir struct {
+	Keyspace string
+	Table    string
+	NodeID   string
+	Path     string // Relative to location root.
+}
+
+// vanishedSSTableDirs returns sstable version dirs (identified by nodeID, keyspace, table, table version)
+// which used to be a part of previous snapshot, but are not a part of the current one.
+// This can happen when node is replaced or table is dropped.
+// Such dirs are discovered by reading contents of previous snapshot manifests
+// and comparing their sstable dirs with the ones from the current snapshot.
+// Hosts, current and oldHeld manifests are expected to share the same single location.
+// Hosts work in parallel up to specified limit.
+func (w *worker) vanishedSSTableDirs(ctx context.Context, limit int, hosts []hostInfo, current, oldHeld []remoteManifestInfo) ([]sstableDir, error) {
+	if len(current)+len(oldHeld) == 0 {
+		return nil, nil
+	}
+
+	type parseManifestJob struct {
+		mi      *backupspec.ManifestInfo
+		current bool
+	}
+	jobCh := make(chan parseManifestJob, len(current)+len(oldHeld))
+	for _, m := range current {
+		jobCh <- parseManifestJob{mi: m.ManifestInfo, current: true}
+	}
+	for _, m := range oldHeld {
+		jobCh <- parseManifestJob{mi: m.ManifestInfo}
+	}
+	close(jobCh)
+
+	var (
+		currentDirs = make(map[sstableDir]struct{})
+		oldDirs     = make(map[sstableDir]struct{})
+		mu          sync.Mutex
+	)
+	eg, egCtx := errgroup.WithContext(ctx)
+	eg.SetLimit(limit)
+	for i := range hosts {
+		eg.Go(func() error {
+			for job := range jobCh {
+				if egCtx.Err() != nil {
+					return egCtx.Err()
+				}
+				dirs, err := w.manifestSSTableDirs(egCtx, hosts[i].IP, job.mi)
+				if err != nil {
+					return errors.Wrapf(err, "%s: get sstable dirs of manifest %s", hosts[i].IP, job.mi.Path())
+				}
+				mu.Lock()
+				if job.current {
+					for _, d := range dirs {
+						currentDirs[d] = struct{}{}
+					}
+				} else {
+					for _, d := range dirs {
+						oldDirs[d] = struct{}{}
+					}
+				}
+				mu.Unlock()
+			}
+			return nil
+		})
+	}
+	if err := eg.Wait(); err != nil {
+		return nil, err
+	}
+	var vanished []sstableDir
+	for d := range oldDirs {
+		if _, ok := currentDirs[d]; !ok {
+			vanished = append(vanished, d)
+		}
+	}
+	return vanished, nil
+}
+
+// manifestSSTableDirs downloads manifest and returns its sstable dirs.
+func (w *worker) manifestSSTableDirs(ctx context.Context, host string, m *backupspec.ManifestInfo) (dirs []sstableDir, err error) {
+	r, err := w.Client.RcloneOpen(ctx, host, m.Location.RemotePath(m.Path()))
+	if err != nil {
+		return nil, errors.Wrap(err, "download manifest")
+	}
+	defer func() {
+		err = stdErr.Join(err, r.Close())
+	}()
+
+	var mc backupspec.ManifestContentWithIndex
+	if err := mc.Read(r); err != nil {
+		return nil, errors.Wrap(err, "read manifest")
+	}
+
+	err = mc.ForEachIndexIter(nil, func(fm backupspec.FilesMeta) {
+		dirs = append(dirs, sstableDir{
+			Keyspace: fm.Keyspace,
+			Table:    fm.Table,
+			NodeID:   m.NodeID,
+			Path:     m.SSTableVersionDir(fm.Keyspace, fm.Table, fm.Version),
+		})
+	})
+	if err != nil {
+		return nil, errors.Wrap(err, "iterate over manifest index")
+	}
+	return dirs, nil
+}
+
+// releaseAllHoldsAllDirs removes event based holds from all
+// objects in all given dirs in parallel.
+// Hosts and dirs are expected to share the same single location.
+func (w *worker) releaseAllHoldsAllDirs(ctx context.Context, hosts []hostInfo, dirs []sstableDir) error {
+	if len(dirs) == 0 {
+		return nil
+	}
+
+	jobCh := make(chan sstableDir, len(dirs))
+	for _, dir := range dirs {
+		jobCh <- dir
+	}
+	close(jobCh)
+
+	eg, egCtx := errgroup.WithContext(ctx)
+	for i := range hosts {
+		eg.Go(func() error {
+			for dir := range jobCh {
+				if egCtx.Err() != nil {
+					return egCtx.Err()
+				}
+				if err := w.releaseAllDirHolds(egCtx, hosts[i], dir); err != nil {
+					return errors.Wrapf(err, "dir %s", dir.Path)
+				}
+			}
+			return nil
+		})
+	}
+	return eg.Wait()
+}
+
+// releaseAllDirHolds removes event based holds from all objects in given dir.
+func (w *worker) releaseAllDirHolds(ctx context.Context, h hostInfo, dir sstableDir) error {
+	w.Logger.Info(ctx, "Removing all holds from dir",
+		"host", h.IP,
+		"node", dir.NodeID,
+		"keyspace", dir.Keyspace,
+		"table", dir.Table,
+		"dir", dir.Path,
+	)
+
+	remoteDir := h.Location.RemotePath(dir.Path)
 	apply := func(ctx context.Context, paths []string, hold bool) error {
-		return w.holdAndWait(ctx, h.IP, remoteManifestDir, paths, hold, "", "")
+		return w.holdAndWait(ctx, h.IP, remoteDir, paths, hold, dir.Keyspace, dir.Table)
 	}
 	holdHandler := newEventBasedHoldHandler(apply, eventBasedHoldBatchSize)
-	// Feed local files - even though manifests have already been uploaded,
-	// we still treat them as local for the purposes of applying/removing the hold.
-	// For consistency, we also treat shadowed temporary manifest as local,
-	// so that its hold cycle follows the regular manifest cycle.
-	holdHandler.addLocal(path.Base(remoteManifestPath))
-	holdHandler.addLocal(backupspec.TempFile(path.Base(remoteManifestPath)))
+	// No local files - all objects should have holds removed
 	holdHandler.finalizeLocal()
-	// Feed remote files - limit to manifests coming from the same task ID,
-	// so that we reduce interference between multiple backup tasks.
 	opts := &scyllaclient.RcloneListDirOpts{
 		FilesOnly:          true,
 		ShowEventBasedHold: true,
 	}
-	listErr := w.Client.RcloneListDirIter(ctx, h.IP, remoteManifestDir, opts, func(item *scyllaclient.RcloneListDirItem) {
-		var mi backupspec.ManifestInfo
-		err := mi.ParsePath(path.Join(path.Dir(manifestPath), item.Name))
-		if err == nil && mi.TaskID == w.TaskID {
-			holdHandler.addRemote(ctx, item.Name, item.EventBasedHold)
-		}
+	listErr := w.Client.RcloneListDirIter(ctx, h.IP, remoteDir, opts, func(item *scyllaclient.RcloneListDirItem) {
+		holdHandler.addRemote(ctx, item.Path, item.EventBasedHold)
 	})
 	return stdErr.Join(
-		errors.Wrap(listErr, "list manifests holds"),
-		errors.Wrap(holdHandler.finalize(ctx), "finalize remote manifests holds"),
+		errors.Wrap(listErr, "list dir"),
+		errors.Wrap(holdHandler.finalize(ctx), "finalize holds removal"),
 	)
+}
+
+// releaseManifestsHold releases holds for given manifests.
+// Since regular manifests can shadow temporary ones which still have hold,
+// it tries to release the hold from the shadowed temporary manifests first.
+// Host and all manifests are expected to share the same single location.
+func (w *worker) releaseManifestsHold(ctx context.Context, host string, manifests []remoteManifestInfo) error {
+	if len(manifests) == 0 {
+		return nil
+	}
+	paths := make([]string, 0, len(manifests))
+	for _, m := range manifests {
+		if !m.EventBasedHold {
+			continue
+		}
+		paths = append(paths, m.Path())
+		// Try to release hold from shadowed tmp manifest first.
+		// Those calls are not batched with remaining ones, as
+		// we want to tolerate errors in case tmp manifest doesn't exist.
+		if !m.Temporary {
+			tmpM := *m.ManifestInfo
+			tmpM.Temporary = true
+			remoteTmpPath := tmpM.Location.RemotePath(tmpM.Path())
+			err := w.Client.RcloneEventBasedHold(ctx, host, remoteTmpPath, false)
+			if err != nil && scyllaclient.StatusCodeOf(err) != http.StatusNotFound {
+				return err
+			}
+		}
+	}
+	return w.holdAndWait(ctx, host, manifests[0].Location.RemotePath(""), paths, false, "", "")
 }
 
 func (w *worker) holdAndWait(ctx context.Context, host, remoteDir string, paths []string, hold bool, keyspace, table string) error {
@@ -137,6 +354,41 @@ func (w *worker) holdAndWait(ctx context.Context, host, remoteDir string, paths 
 		return errors.Wrap(err, "schedule event based hold job")
 	}
 	return w.waitRetentionJob(ctx, host, jobID, w.retentionLockJobCB(host, jobID, keyspace, table))
+}
+
+// groupManifestsByTagAndHold returns manifests being a part of the current snapshot
+// and manifests from other snapshots still having hold applied.
+// Shadowed manifests and manifests coming with different task IDs are ignored.
+func groupManifestsByTagAndHold(manifests []remoteManifestInfo, taskID uuid.UUID, snapshotTag string) (current, oldHeld []remoteManifestInfo) {
+	all := map[*backupspec.ManifestInfo]remoteManifestInfo{}
+	for _, m := range manifests {
+		all[m.ManifestInfo] = m
+	}
+	for _, m := range filterShadowedTemporaryManifests(manifestInfos(manifests)) {
+		if m.TaskID != taskID {
+			continue
+		}
+		switch {
+		case m.SnapshotTag == snapshotTag:
+			current = append(current, all[m])
+		case all[m].EventBasedHold:
+			oldHeld = append(oldHeld, all[m])
+		}
+	}
+	return current, oldHeld
+}
+
+// groupHostsByLocation groups hosts by their location.
+// The DC part of the location is ignored, so that the same
+// physical location is processed only once.
+func groupHostsByLocation(hosts []hostInfo) map[backupspec.Location][]hostInfo {
+	m := make(map[backupspec.Location][]hostInfo)
+	for i := range hosts {
+		loc := hosts[i].Location
+		loc.DC = ""
+		m[loc] = append(m[loc], hosts[i])
+	}
+	return m
 }
 
 // eventBasedHoldBatchSize defines how many files are batched together
