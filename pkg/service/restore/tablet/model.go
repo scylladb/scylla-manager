7ec91594752fa360f9ccf9808fd6b47c496f54ee
Michał Leszczyński <2000michal@wp.pl>
2026-09-08 14:55:38 +0200

    feat(restore/tablet): add run progress model
    This commit adds the RunProgress struct mapping to the
    restore_run_progress_tablet table. It also adds DB getter
    for the current progress state as well as a way to copy
    previous run progress to resume it.

---
+++ pkg/service/restore/tablet/model.go
@@ -0,0 +1,43 @@
+// Copyright (C) 2026 ScyllaDB
+
+package tablet
+
+import (
+	"time"
+
+	"github.com/scylladb/scylla-manager/v3/pkg/util/uuid"
+)
+
+// RunProgress is tablet aware restore progress representation in SM DB.
+type RunProgress struct {
+	// Partition key
+	ClusterID uuid.UUID
+	TaskID    uuid.UUID
+	RunID     uuid.UUID
+	// Clustering key
+	Keyspace string `db:"keyspace_name"`
+	Table    string `db:"table_name"`
+
+	// Host on which tablet aware restore task was scheduled.
+	// Task status can be queried only on this host.
+	Host         string
+	ScyllaTaskID string `db:"scylla_task_id"`
+	// StartedAt is set to the scylla task timestamp
+	// with SM side fallback if it's not available.
+	StartedAt time.Time
+	// CompletedAt is set to the scylla task timestamp
+	// with SM side fallback if it's not available.
+	// CompletedAt is set only when scylla task reached terminal state (done or failed).
+	CompletedAt time.Time
+	// Scylla reports progress in restored and total sstables
+	RestoredSSTables int64 `db:"restored_sstables"`
+	TotalSSTables    int64 `db:"total_sstables"`
+	// Size of the restored table taken from manifest
+	Size  int64
+	Error string
+}
+
+// isSuccess reports whether the table was fully restored without an error.
+func (pr RunProgress) isSuccess() bool {
+	return !pr.CompletedAt.IsZero() && pr.Error == ""
+}
